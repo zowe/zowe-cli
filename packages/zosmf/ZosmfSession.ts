64d7aafe3f9165b51a00d1205184bc96777be3d0
boech02 <Christopher.Boehm@ca.com>
2018-10-17 12:35:48 -0400

    add utility to create session from arguments
    Signed-off-by: boech02 <Christopher.Boehm@ca.com>

--- packages/zosmf/ZosmfSession.ts
+++ packages/zosmf/ZosmfSession.ts
@@ -10,6 +10,7 @@
 */
 
 import { ICommandOptionDefinition, IProfile, Logger, Session } from "@brightside/imperative";
+import { ICommandArguments } from "@brightside/imperative/lib/cmd/src/doc/args/ICommandArguments";
 
 /**
  * Utility Methods for Brightside
@@ -18,33 +19,43 @@ import { ICommandOptionDefinition, IProfile, Logger, Session } from "@brightside
 export class ZosmfSession {
 
     public static ZOSMF_CONNECTION_OPTION_GROUP = "Zosmf Connection Options";
+
+    /**
+     * Option used in profile creation and commands for hostname for z/OSMF
+     */
+    public static ZOSMF_OPTION_HOST: ICommandOptionDefinition = {
+        name: "host",
+        aliases: ["H"],
+        description: "The z/OSMF server host name.",
+        type: "string",
+        required: true,
+        group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
+    };
+
+    /**
+     * Option used in profile creation and commands for port for z/OSMF
+     */
+    public static ZOSMF_OPTION_PORT: ICommandOptionDefinition = {
+        name: "port",
+        aliases: ["P"],
+        description: "The z/OSMF server port.",
+        type: "number",
+        defaultValue: 443,
+        group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
+    };
     /**
      * Options related to connecting to z/OSMF
      * These options can be filled in if the user creates a profile
      */
     public static ZOSMF_CONNECTION_OPTIONS: ICommandOptionDefinition[] = [
-        {
-            name: "host",
-            aliases: ["H"],
-            description: "The z/OSMF server host name.",
-            type: "string",
-            required: true,
-            group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
-        },
-        {
-            name: "port",
-            aliases: ["P"],
-            description: "The z/OSMF server port.",
-            type: "number",
-            defaultValue: 443,
-            group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
-        },
+        ZosmfSession.ZOSMF_OPTION_HOST,
+        ZosmfSession.ZOSMF_OPTION_PORT,
         {
             name: "user",
             aliases: ["u"],
             description: "Mainframe (z/OSMF) user name, which can be the same as your TSO login.",
             type: "string",
-            implies: ["password"],
+            implies: ["pass"],
             group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
         },
         {
@@ -52,7 +63,6 @@ export class ZosmfSession {
             aliases: ["p"],
             description: "Mainframe (z/OSMF) password, which can be the same as your TSO password.",
             type: "string",
-            implies: ["user"],
             group: ZosmfSession.ZOSMF_CONNECTION_OPTION_GROUP
         },
         {
@@ -82,6 +92,25 @@ export class ZosmfSession {
             password: profile.pass,
             base64EncodedAuth: profile.auth,
             rejectUnauthorized: profile.rejectUnauthorized,
+        });
+    }
+
+    /**
+     * Given command line arguments, create a REST Client Session.
+     * @static
+     * @param {IProfile} args - The arguments specified by the user
+     * @returns {Session} - A session for usage in the z/OSMF REST Client
+     */
+    public static createBasicZosmfSessionFromArguments(args: ICommandArguments): Session {
+        this.log.debug("Creating a z/OSMF session from arguments");
+        return new Session({
+            type: "basic",
+            hostname: args.host,
+            port: args.port,
+            user: args.user,
+            password: args.pass,
+            base64EncodedAuth: args.auth,
+            rejectUnauthorized: args.rejectUnauthorized,
         });
     }
 
