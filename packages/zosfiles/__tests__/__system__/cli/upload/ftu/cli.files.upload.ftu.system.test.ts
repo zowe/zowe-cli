f4542d0c8ae0ffaa2d83ae904ffe72f11c86700a
Swauger, William E <William.Swauger@ca.com>
2018-10-24 12:16:01 -0400

    Added system tests for profile precedence

--- packages/zosfiles/__tests__/__system__/cli/upload/ftu/cli.files.upload.ftu.system.test.ts
+++ packages/zosfiles/__tests__/__system__/cli/upload/ftu/cli.files.upload.ftu.system.test.ts
@@ -9,7 +9,7 @@
 *                                                                                 *
 */
 
-import { Imperative, Session } from "@brightside/imperative";
+import { Imperative, IO, Session } from "@brightside/imperative";
 import * as path from "path";
 import { runCliScript, getUniqueDatasetName, stripNewLines } from "../../../../../../../__tests__/__src__/TestUtils";
 import { TestEnvironment } from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
@@ -18,24 +18,26 @@ import { TestProperties } from "../../../../../../../__tests__/__src__/propertie
 import { ITestSystemSchema } from "../../../../../../../__tests__/__src__/properties/ITestSystemSchema";
 import { Create, CreateDataSetTypeEnum } from "../../../../../src/api/methods/create";
 import { Delete } from "../../../../../src/api/methods/delete";
-import { ZosFilesConstants } from "../../../../../index";
+import { Get, ZosFilesConstants } from "../../../../../index";
 import { ZosmfRestClient } from "../../../../../../rest";
 
 let REAL_SESSION: Session;
-let testEnvironment: ITestEnvironment;
+let TEST_ENVIRONMENT: ITestEnvironment;
+let TEST_ENVIRONMENT_NO_PROF: ITestEnvironment;
 let systemProps: TestProperties;
 let defaultSystem: ITestSystemSchema;
 let dsname: string;
 let ussname: string;
 
 describe("Upload uss file", () => {
+
     beforeAll(async () => {
-        testEnvironment = await TestEnvironment.setUp({
+        TEST_ENVIRONMENT = await TestEnvironment.setUp({
             tempProfileTypes: ["zosmf"],
             testName: "upload_uss_file"
         });
 
-        systemProps = new TestProperties(testEnvironment.systemTestProperties);
+        systemProps = new TestProperties(TEST_ENVIRONMENT.systemTestProperties);
         defaultSystem = systemProps.getDefaultSystem();
 
         REAL_SESSION = new Session({
@@ -51,6 +53,74 @@ describe("Upload uss file", () => {
         ussname = dsname.replace(/\./g, "");
         ussname = `${defaultSystem.unix.testdir}/${ussname}`;
         Imperative.console.info("Using ussfile:" + ussname);
+    });
+
+    afterAll(async () => {
+        await TestEnvironment.cleanUp(TEST_ENVIRONMENT);
+    });
+
+    describe("Without profile", () => {
+        let sysProps;
+        let defaultSys: ITestSystemSchema;
+
+        // Create the unique test environment
+        beforeAll(async () => {
+            TEST_ENVIRONMENT_NO_PROF = await TestEnvironment.setUp({
+                testName: "zos_files_upload_file_to_uss_without_profile"
+            });
+
+            sysProps = new TestProperties(TEST_ENVIRONMENT_NO_PROF.systemTestProperties);
+            defaultSys = sysProps.getDefaultSystem();
+            //
+            // REAL_SESSION = new Session({
+            //     user: defaultSys.zosmf.user,
+            //     password: defaultSys.zosmf.pass,
+            //     hostname: defaultSys.zosmf.host,
+            //     port: defaultSys.zosmf.port,
+            //     type: "basic",
+            //     rejectUnauthorized: defaultSys.zosmf.rejectUnauthorized
+            // });
+            //
+            // dsname = getUniqueDatasetName(defaultSys.zosmf.user);
+            // ussname = dsname.replace(/\./g, "");
+            // ussname = `${defaultSys.unix.testdir}/${ussname}`;
+            // Imperative.console.info("Using ussfile:" + ussname);
+        });
+
+        beforeEach(async () => {
+            try {
+                await Create.dataSet(REAL_SESSION, CreateDataSetTypeEnum.DATA_SET_PARTITIONED, dsname);
+            } catch (err) {
+                throw err;
+            }
+        });
+
+        afterEach(async () => {
+            try {
+                await Delete.dataSet(REAL_SESSION, dsname);
+            } catch (err) {
+                throw err;
+            }
+        });
+
+        it("should upload USS file from local file", async () => {
+            const shellScript = path.join(__dirname, "__scripts__", "command", "command_fully_qualified.sh");
+            const localFileName = path.join(__dirname, "__data__", "command_upload_ftu.txt");
+            const response = runCliScript(shellScript,
+                TEST_ENVIRONMENT_NO_PROF,
+                [localFileName,
+                    ussname.substring(1),
+                    defaultSys.zosmf.host,
+                    defaultSys.zosmf.port,
+                    defaultSys.zosmf.user,
+                    defaultSys.zosmf.pass
+                ]);
+            expect(response.stderr.toString()).toBe("");
+            expect(response.status).toBe(0);
+            expect(response.stdout.toString()).toContain("Data set uploaded successfully.");
+            const content = await Get.dataSet(REAL_SESSION, dsname + "(member)");
+            expect(content.toString().trim()).toEqual(IO.readFileSync(localFileName).toString().trim());
+        });
     });
 
     describe ("Success scenarios", () => {
@@ -70,7 +140,7 @@ describe("Upload uss file", () => {
 
         it("should display upload uss file help", async () => {
             const shellScript = path.join(__dirname, "__scripts__", "command_upload_ftu_help.sh");
-            const response = runCliScript(shellScript, testEnvironment);
+            const response = runCliScript(shellScript, TEST_ENVIRONMENT);
             expect(response.stderr.toString()).toBe("");
             expect(response.status).toBe(0);
             const helpText = response.stdout.toString();
@@ -89,7 +159,7 @@ describe("Upload uss file", () => {
         it("should upload USS file from local file", async () => {
             const shellScript = path.join(__dirname, "__scripts__", "command", "command_upload_ftu.sh");
             const localFileName = path.join(__dirname, "__data__", "command_upload_ftu.txt");
-            const response = runCliScript(shellScript, testEnvironment, [localFileName, ussname.substring(1)]);
+            const response = runCliScript(shellScript, TEST_ENVIRONMENT, [localFileName, ussname.substring(1)]);
             expect(response.stderr.toString()).toBe("");
             expect(response.status).toBe(0);
             expect(response.stdout.toString()).toContain("USS file uploaded successfully");
@@ -100,7 +170,7 @@ describe("Upload uss file", () => {
     describe("Expected failures", () => {
         it("should fail due to missing uss file name", async () => {
             const shellScript = path.join(__dirname, "__scripts__", "command", "command_upload_ftu.sh");
-            const response = runCliScript(shellScript, testEnvironment, [""]);
+            const response = runCliScript(shellScript, TEST_ENVIRONMENT, [""]);
             expect(response.status).toBe(1);
             expect(response.stderr.toString()).toContain("Missing Positional Option");
             expect(response.stderr.toString()).toContain("USSFileName");
@@ -108,7 +178,7 @@ describe("Upload uss file", () => {
 
         it("should fail when local file does not exist", async () => {
             const shellScript = path.join(__dirname, "__scripts__", "command", "command_upload_ftu.sh");
-            const response = runCliScript(shellScript, testEnvironment, ["localFileThatDoesNotExist", ussname]);
+            const response = runCliScript(shellScript, TEST_ENVIRONMENT, ["localFileThatDoesNotExist", ussname]);
             expect(stripNewLines(response.stderr.toString())).toContain("no such file or directory, lstat");
             expect(stripNewLines(response.stderr.toString())).toContain("localFileThatDoesNotExist");
         });
