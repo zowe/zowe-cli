3a120384ea1676793e9d41eb05421440f55dcfd2
Wright, Christopher R <Christopher.Wright@ca.com>
2018-11-06 13:19:20 -0500

    Allow proper path setup on windows/linux
    Signed-off-by: Wright, Christopher R <Christopher.Wright@ca.com>

--- __tests__/__src__/environment/TestEnvironment.ts
+++ __tests__/__src__/environment/TestEnvironment.ts
@@ -60,7 +60,11 @@ export class TestEnvironment {
         // scripts in the test environment
         const env: { [key: string]: string } = {};
         env[Constants.HOME_ENV_KEY] = testDirectory;
-        env.PATH = nodePath.resolve(__dirname, "../../__resources__/application_instances") + ";" + process.env.PATH;
+
+        // Ensure correct path separator for windows or linux like systems.
+        const separator = process.platform === "win32" ? ";" : ":";
+
+        env.PATH = `${nodePath.resolve(__dirname, "../../__resources__/application_instances")}${separator}${process.env.PATH}`;
 
         const result: ITestEnvironment = {
             workingDir: testDirectory,
