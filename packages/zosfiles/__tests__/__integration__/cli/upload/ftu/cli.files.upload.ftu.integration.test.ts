1a93ffbbfce74bce5304b980d26568844d1eb0c0
boech02 <Christopher.Boehm@ca.com>
2018-11-07 15:23:29 -0500

    correct location of script file command_upload_ftu_help.sh
    Signed-off-by: boech02 <Christopher.Boehm@ca.com>

--- packages/zosfiles/__tests__/__integration__/cli/upload/ftu/cli.files.upload.ftu.integration.test.ts
+++ packages/zosfiles/__tests__/__integration__/cli/upload/ftu/cli.files.upload.ftu.integration.test.ts
@@ -43,7 +43,7 @@ describe("Upload uss file", () => {
             const shellScript = path.join(__dirname, "__scripts__", "command", "command_upload_ftu.sh");
             const response = runCliScript(shellScript, TEST_ENVIRONMENT, [""]);
             expect(response.status).toBe(1);
-            expect(response.stderr.toString()).toContain("Missing Positional Option");
+            expect(response.stderr.toString()).toContain("Missing Positional Argument");
             expect(response.stderr.toString()).toContain("USSFileName");
         });
 
