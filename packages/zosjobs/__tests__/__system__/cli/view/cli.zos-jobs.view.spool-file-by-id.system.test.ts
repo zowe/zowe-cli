fa759335cb2d9347f46013da4a5fc232f9f59404
boech02 <Christopher.Boehm@ca.com>
2018-11-09 12:39:18 -0500

    remove obsolete snapshot files
    Signed-off-by: boech02 <Christopher.Boehm@ca.com>

--- packages/zosjobs/__tests__/__system__/cli/view/cli.zos-jobs.view.spool-file-by-id.system.test.ts
+++ packages/zosjobs/__tests__/__system__/cli/view/cli.zos-jobs.view.spool-file-by-id.system.test.ts
@@ -35,7 +35,8 @@ describe("zos-jobs view spool-file-by-id command", () => {
     // Create the unique test environment
     beforeAll(async () => {
         TEST_ENVIRONMENT = await TestEnvironment.setUp({
-            testName: "zos_jobs_view_spool_file_by_id_command"
+            testName: "zos_jobs_view_spool_file_by_id_command",
+            tempProfileTypes: ["zosmf"]
         });
 
         IEFBR14_JOB = TEST_ENVIRONMENT.systemTestProperties.zosjobs.iefbr14Member;
