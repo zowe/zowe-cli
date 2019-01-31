881ca09cd5fc1b78e675f34e02fcb7259119c9ed
nurra01 <nurra01@ca.com>
2019-01-31 17:08:44 +0100

    rename DirToUssDir to DirToUSSDir and remove quotes from options
    Signed-off-by: nurra01 <nurra01@ca.com>

--- packages/zosfiles/src/cli/upload/dtu/DirToUSSDir.definition.ts
+++ packages/zosfiles/src/cli/upload/dtu/DirToUSSDir.definition.ts
@@ -58,15 +58,15 @@ export const DirToUSSDirDefinition: ICommandDefinition = {
         },
         {
             description: strings.EXAMPLES.EX2,
-            options: `"local_dir" "/a/ibmuser/my_dir" "--recursive"`
+            options: `"local_dir" "/a/ibmuser/my_dir" --recursive`
         },
         {
             description: strings.EXAMPLES.EX3,
-            options: `"local_dir" "/a/ibmuser/my_dir" "--binary-files" "myFile1.exe,myFile2.exe,myFile3.exe"`
+            options: `"local_dir" "/a/ibmuser/my_dir" --binary-files "myFile1.exe,myFile2.exe,myFile3.exe"`
         },
         {
             description: strings.EXAMPLES.EX4,
-            options: `"local_dir" "/a/ibmuser/my_dir" "--binary" "--ascii-files" "myFile1.txt,myFile2.txt,myFile3.txt"`
+            options: `"local_dir" "/a/ibmuser/my_dir" --binary --ascii-files "myFile1.txt,myFile2.txt,myFile3.txt"`
         }
     ]
 };
