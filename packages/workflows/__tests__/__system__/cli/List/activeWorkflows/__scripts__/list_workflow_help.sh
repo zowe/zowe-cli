f49e534f0a771c84521c4d3524dd2f5ca44d4c06
Uzunov <stanko.uzunov@broadcom.com>
2019-02-19 10:47:02 +0100

    Edit help scripts to match command script and update snaps
    Signed-off-by: Uzunov <stanko.uzunov@broadcom.com>

--- packages/workflows/__tests__/__system__/cli/List/activeWorkflows/__scripts__/list_workflow_help.sh
+++ packages/workflows/__tests__/__system__/cli/List/activeWorkflows/__scripts__/list_workflow_help.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "================Z/OS WORKFLOWS LIST WORKFLOWS HELP==============="
+echo "================Z/OS WORKFLOWS LIST ACTIVE WORKFLOWS HELP==============="
 zowe zos-workflows list acw --help
 if [ $? -gt 0 ]
 then
     exit $?
 fi
 
-echo "================Z/OS WORKFLOWS LIST WORKFLOWS HELP RFJ==========="
+echo "================Z/OS WORKFLOWS LIST ACTIVE WORKFLOWS HELP RFJ==========="
 zowe zos-workflows list acw --help --rfj
 exit $?
