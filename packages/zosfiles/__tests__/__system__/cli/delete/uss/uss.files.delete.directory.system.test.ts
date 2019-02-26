b1ec63de3c4784c7a3b81996c1c33c47ea233246
Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>
2019-02-26 14:04:14 +0000

    Initial upload of delete uss function and tests
    Signed-off-by: Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>

---
+++ packages/zosfiles/__tests__/__system__/cli/delete/uss/uss.files.delete.directory.system.test.ts
@@ -0,0 +1,152 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { Imperative, Session } from "@brightside/imperative";
+import { Create } from "../../../../..";
+import { inspect } from "util";
+import { runCliScript } from "../../../../../../../__tests__/__src__/TestUtils";
+import { TestEnvironment } from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
+import { ITestEnvironment } from "../../../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
+import { TestProperties } from "../../../../../../../__tests__/__src__/properties/TestProperties";
+import { ITestSystemSchema } from "../../../../../../../__tests__/__src__/properties/ITestSystemSchema";
+import { appendFile } from "fs";
+
+let REAL_SESSION: Session;
+// Test Environment populated in the beforeAll();
+let TEST_ENVIRONMENT: ITestEnvironment;
+let TEST_ENVIRONMENT_NO_PROF: ITestEnvironment;
+let systemProps: TestProperties;
+let defaultSystem: ITestSystemSchema;
+let ussname: string;
+let user: string;
+
+describe("Delete Directory", () => {
+
+    // Create the unique test environment
+    beforeAll(async () => {
+        TEST_ENVIRONMENT = await TestEnvironment.setUp({
+            tempProfileTypes: ["zosmf"],
+            testName: "zos_delete_uss_directory"
+        });
+
+        systemProps = new TestProperties(TEST_ENVIRONMENT.systemTestProperties);
+        defaultSystem = systemProps.getDefaultSystem();
+
+        REAL_SESSION = TestEnvironment.createZosmfSession(TEST_ENVIRONMENT);
+        user = defaultSystem.zosmf.user.trim();
+        ussname = `${defaultSystem.zosmf.user.trim()}.aTestUssDirectory`;
+        ussname = ussname.replace(/\./g, "");
+        ussname = `${defaultSystem.unix.testdir}/${ussname}`;
+    });
+
+    afterAll(async () => {
+        await TestEnvironment.cleanUp(TEST_ENVIRONMENT);
+    });
+
+    describe("Success scenario without profiles", () => {
+        let sysProps;
+        let defaultSys: ITestSystemSchema;
+
+        // Create the unique test environment
+        beforeEach(async () => {
+            TEST_ENVIRONMENT_NO_PROF = await TestEnvironment.setUp({
+                testName: "zos_files_delete_uss_directory_without_profile"
+            });
+
+            sysProps = new TestProperties(TEST_ENVIRONMENT_NO_PROF.systemTestProperties);
+            defaultSys = sysProps.getDefaultSystem();
+            let error;
+            let response;
+            try {
+                response = await Create.uss(REAL_SESSION, ussname, "directory");
+            } catch (err) {
+                error = err;
+                Imperative.console.info("Error: " + inspect(error));
+            }
+            try {
+                response = await Create.uss(REAL_SESSION, `${ussname}/appendFile.txt`, "directory");
+            } catch (err) {
+                error = err;
+                Imperative.console.info("Error: " + inspect(error));
+            }
+        });
+
+        afterAll(async () => {
+            await TestEnvironment.cleanUp(TEST_ENVIRONMENT_NO_PROF);
+        });
+
+        it("should delete a uss directory, with fully specified command", async () => {
+            const ZOWE_OPT_BASE_PATH = "ZOWE_OPT_BASE_PATH";
+
+            // if API Mediation layer is being used (basePath has a value) then
+            // set an ENVIRONMENT variable to be used by zowe.
+            if (defaultSys.zosmf.basePath != null) {
+                TEST_ENVIRONMENT_NO_PROF.env[ZOWE_OPT_BASE_PATH] = defaultSys.zosmf.basePath;
+            }
+
+            const response = runCliScript(__dirname + "/__scripts__/command/command_delete_directory_qualified.sh",
+                TEST_ENVIRONMENT_NO_PROF, [ussname, "--for-sure",
+                    defaultSys.zosmf.host,
+                    defaultSys.zosmf.port,
+                    defaultSys.zosmf.user,
+                    defaultSys.zosmf.pass]);
+            expect(response.stderr.toString()).toBe("");
+            expect(response.status).toBe(0);
+            expect(response.stdout.toString()).toMatchSnapshot();
+        });
+    });
+
+    describe("Success scenarios", () => {
+        let sysProps;
+        let defaultSys: ITestSystemSchema;
+
+        // Create the unique test environment
+        beforeEach(async () => {
+            TEST_ENVIRONMENT_NO_PROF = await TestEnvironment.setUp({
+                testName: "zos_files_delete_uss_directory_without_profile"
+            });
+
+            sysProps = new TestProperties(TEST_ENVIRONMENT_NO_PROF.systemTestProperties);
+            defaultSys = sysProps.getDefaultSystem();
+            let error;
+            let response;
+
+            try {
+                response = await Create.uss(REAL_SESSION, ussname, "directory");
+            } catch (err) {
+                error = err;
+                Imperative.console.info("Error: " + inspect(error));
+            }
+        });
+
+        afterAll(async () => {
+            await TestEnvironment.cleanUp(TEST_ENVIRONMENT_NO_PROF);
+        });
+
+        it("should delete a directory", async () => {
+            const response = runCliScript(__dirname + "/__scripts__/command/command_delete_directory.sh",
+                TEST_ENVIRONMENT, [ussname, "--for-sure", "--r"]);
+            expect(response.stderr.toString()).toBe("");
+            expect(response.status).toBe(0);
+            expect(response.stdout.toString()).toMatchSnapshot();
+        });
+    });
+
+    describe("Expected failures", () => {
+        it("should fail deleting a directory that does not exist", async () => {
+            const response = runCliScript(__dirname + "/__scripts__/command/command_delete_directory.sh",
+                TEST_ENVIRONMENT, [ussname, "--for-sure"]);
+            expect(response.status).toBe(1);
+            expect(response.stderr.toString()).toContain("No such file or directory.");
+            expect(response.stdout.toString()).toMatchSnapshot();
+        });
+    });
+});
