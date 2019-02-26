43bccbc19f1a28d88d43241936687b30ad013ac5
Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>
2019-02-26 14:04:14 +0000

    Initial upload of delete uss function and tests
    Signed-off-by: Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>

---
+++ packages/zosfiles/src/cli/delete/uss/Uss.handler.ts
@@ -0,0 +1,23 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { AbstractSession, IHandlerParameters } from "@brightside/imperative";
+import { Delete, IZosFilesResponse } from "../../../api";
+import { ZosFilesBaseHandler } from "../../ZosFilesBase.handler";
+
+/**
+ * Handler to delete a USS file.
+ */
+export default class UssHandler extends ZosFilesBaseHandler {
+    public async processWithSession(commandParameters: IHandlerParameters, session: AbstractSession): Promise<IZosFilesResponse> {
+        return Delete.ussFile(session, commandParameters.arguments.fileName, commandParameters.arguments.recursive);
+    }
+}
