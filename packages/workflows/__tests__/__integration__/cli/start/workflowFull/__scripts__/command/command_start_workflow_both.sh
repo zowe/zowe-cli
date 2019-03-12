27e9783e2b7e9d8f0ac8fc2e42a687f3d8759b6d
Uzunov <stanko.uzunov@broadcom.com>
2019-03-12 14:57:11 +0100

    Add integration tests, scripts and update snaps
    Signed-off-by: Uzunov <stanko.uzunov@broadcom.com>

---
+++ packages/workflows/__tests__/__integration__/cli/start/workflowFull/__scripts__/command/command_start_workflow_both.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wk=$1
+wn=$2
+set -e
+
+echo "================Z/OS WORKFLOWS START WORKFLOW BOTH ==============="
+bright zos-workflows start workflow-full --workflow-key $wk --workflow-name $wn
+if [ $? -gt 0 ]
+then
+    exit $?
+fi
