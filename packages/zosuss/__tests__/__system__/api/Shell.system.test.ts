f84f9970d978823837c75087c55bb3d77e22df14
Jeonghyuk Park <jeonghyukpark@jeonghyuks-mbp.hursley.uk.ibm.com>
2019-03-12 09:41:25 +0000

    Modify error handling for empty privateKey, Add api system test Signed-off-by: Jeonghyuk Park <jeonghyukpark@jeonghyuks-mbp.hursley.uk.ibm.com>
    Signed-off-by: cy <cy.cao@163.com>

--- packages/zosuss/__tests__/__system__/api/Shell.system.test.ts
+++ packages/zosuss/__tests__/__system__/api/Shell.system.test.ts
@@ -12,21 +12,62 @@
 import { Shell, SshSession } from "../../../index";
 import { ITestEnvironment } from "../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
 import { TestEnvironment } from "../../../../../__tests__/__src__/environment/TestEnvironment";
+import { ClientChannel } from "ssh2";
+import { TestProperties } from "../../../../../__tests__/__src__/properties/TestProperties";
+import { ITestSystemSchema } from "../../../../../__tests__/__src__/properties/ITestSystemSchema";
 
-let testEnvironment: ITestEnvironment;
+let TEST_ENVIRONMENT: ITestEnvironment;
 let SSH_SESSION: SshSession;
+let systemProps: TestProperties;
+let defaultSystem: ITestSystemSchema;
 
-describe("Shell", () => {
+describe("zowe uss issue ssh api call test", () => {
 
     beforeAll(async () => {
-        testEnvironment = await TestEnvironment.setUp({
-            testName: "shell_execute_command"
+        TEST_ENVIRONMENT = await TestEnvironment.setUp({
+            testName: "issue_ssh_system_api",
+            tempProfileTypes: ["ssh"]
         });
-        SSH_SESSION = TestEnvironment.createSshSession(testEnvironment);
+        SSH_SESSION = TestEnvironment.createSshSession(TEST_ENVIRONMENT);
+        systemProps = new TestProperties(TEST_ENVIRONMENT.systemTestProperties);
+        defaultSystem = systemProps.getDefaultSystem();
     });
 
-    it ("should execute uname command on the remote system by ssh and return operating system name", async () => {
-        // TODO: make a meaningful test e.g. check output from stdout
-        // await Shell.executeSsh(SSH_SESSION, "uname");
+    afterAll(async () => {
+        await TestEnvironment.cleanUp(TEST_ENVIRONMENT);
     });
+
+    it ("should execute uname command on the remote system by ssh and return operating system name", (done) => {
+        const command = "uname";
+        Shell.executeSsh(SSH_SESSION, command, (stream: ClientChannel) => {
+            let stdoutData: string;
+            stream.on("data", (data: string) => {
+                if (!data.includes("exit")) {
+                    stdoutData += data;
+                }
+            });
+            stream.on("close", () => {
+                expect(stdoutData).toMatch("OS/390");
+                done();
+            });
+        });
+    });
+
+    it ("should resolve cwd option", (done) => {
+        const command = "pwd";
+        const cwd =  `${defaultSystem.unix.testdir}/`;
+        Shell.executeSshCwd(SSH_SESSION, command, cwd, (stream: ClientChannel) => {
+            let stdoutData: string;
+            stream.on("data", (data: string) => {
+                if (!data.includes("exit")) {
+                    stdoutData += data;
+                }
+            });
+            stream.on("close", () => {
+                expect(stdoutData).toMatch(new RegExp("\\" + cwd + "\\s"));
+                done();
+            });
+        });
+    });
+
 });
