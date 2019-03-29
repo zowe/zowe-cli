8e5039651aec95d10863b97f39577eead40f19b1
Uzunov <stanko.uzunov@broadcom.com>
2019-03-29 10:48:20 +0100

    Fix return message by archive-workflow-by-wfname handler
    Signed-off-by: Uzunov <stanko.uzunov@broadcom.com>

--- packages/workflows/src/cli/archive/Archive.handler.ts
+++ packages/workflows/src/cli/archive/Archive.handler.ts
@@ -77,7 +77,7 @@ export default class ArchiveHandler extends ZosmfBaseHandler {
                     throw error;
                 }
                 params.response.data.setObj(resp);
-                params.response.console.log("Workflow archived with workflow-name " + resp.workflowKey);
+                params.response.console.log("Workflow archived with workflow-name " + this.arguments.workflowName);
                 break;
 
             default:
