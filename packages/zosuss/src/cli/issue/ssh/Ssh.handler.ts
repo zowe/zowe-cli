0db583944123b5c1b3e4230980b200ea55107dd6
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-04-04 14:10:33 +0100

    Use response.console.log for output
    Signed-off-by: Matthew Wilson <matthew.wilson@uk.ibm.com>
    Signed-off-by: cy <cy.cao@163.com>

--- packages/zosuss/src/cli/issue/ssh/Ssh.handler.ts
+++ packages/zosuss/src/cli/issue/ssh/Ssh.handler.ts
@@ -9,7 +9,7 @@
 *
 */
 
-import { IHandlerParameters } from "@brightside/imperative";
+import { IHandlerParameters, IHandlerResponseConsoleApi } from "@zowe/imperative";
 import { Shell } from "../../../api/Shell";
 import { ClientChannel } from "ssh2";
 import { SshBaseHandler } from "../../../SshBaseHandler";
@@ -22,16 +22,19 @@ import { SshBaseHandler } from "../../../SshBaseHandler";
  */
 export default class Handler extends SshBaseHandler {
 
+    private parameters: IHandlerParameters;
 
     public async processCmd(commandParameters: IHandlerParameters) {
+        this.parameters = commandParameters;
         if (commandParameters.arguments.cwd) {
-            await Shell.executeSshCwd(this.mSession, commandParameters.arguments.command, commandParameters.arguments.cwd, this.stdoutHandler);
+            await Shell.executeSshCwd(this.mSession, commandParameters.arguments.command, commandParameters.arguments.cwd,
+                this.handleStdout.bind(this));
         } else {
-            await Shell.executeSsh(this.mSession, commandParameters.arguments.command, this.stdoutHandler);
+            await Shell.executeSsh(this.mSession, commandParameters.arguments.command, this.handleStdout.bind(this));
         }
     }
 
-    private stdoutHandler(data: string) {
-        process.stdout.write(data);
+    public handleStdout(data: string) {
+        this.parameters.response.console.log(data);
     }
 }
