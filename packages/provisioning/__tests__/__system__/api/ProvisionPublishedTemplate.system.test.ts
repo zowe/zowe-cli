598d4a6a70cb8abd9ed2acf94a7261d0e0a97a83
Rauan Nurbatyrov <rauan.nurbatyrov@broadcom.com>
2019-04-23 09:13:58 +0200

    refactor ProvisionPublishedTemplate api system tests

--- packages/provisioning/__tests__/__system__/api/ProvisionPublishedTemplate.system.test.ts
+++ packages/provisioning/__tests__/__system__/api/ProvisionPublishedTemplate.system.test.ts
@@ -15,23 +15,21 @@ import { TestEnvironment } from "../../../../../__tests__/__src__/environment/Te
 import { ITestSystemSchema } from "../../../../../__tests__/__src__/properties/ITestSystemSchema";
 import { ITestEnvironment } from "../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
 import {
-    DeleteInstance,
-    IProvisionedInstances,
-    IProvisionOptionals,
     IProvisionTemplateResponse,
-    ListInstanceInfo,
-    ListRegistryInstances,
     noAccountInfo,
     noSessionProvisioning,
     noTemplateName,
     nozOSMFVersion,
-    PerformAction,
     ProvisioningConstants,
     ProvisionPublishedTemplate,
-} from "../../../../provisioning";
+} from "../../../";
+import { ProvisioningTestUtils } from "../../__resources__/api/ProvisioningTestUtils";
 
-const MAX_TIMEOUT_NUMBER: number = 3600000;
-const SLEEP_TIME: number = 10000;
+const MAX_TIMEOUT_TIME: number = 3600000;
+
+// If you have a published template which requires account-info, change it here
+// in most cases account-info is optional
+const ACCOUNT_NUMBER: string = "1111111111";
 
 let testEnvironment: ITestEnvironment;
 let systemProps: TestProperties;
@@ -41,17 +39,6 @@ let REAL_SESSION: Session;
 
 let templateName: string;
 let instanceName: string;
-let accountNumber: string;
-
-const inputProperties: IProvisionOptionals = {
-    "input-variables": null,
-    "domain-name": null,
-    "tenant-name": null,
-    "user-data-id": null,
-    "account-info": null,
-    "user-data": null,
-    "systems-nicknames": null
-};
 
 const OBJECT_URI: string = `${ProvisioningConstants.RESOURCE}/${ProvisioningConstants.ZOSMF_VERSION}/${ProvisioningConstants.INSTANCES_RESOURCE}/`;
 
@@ -67,55 +54,6 @@ function expectZosmfResponseFailed(response: IProvisionTemplateResponse, error: 
     expect(error.details.msg).toContain(msg);
 }
 
-function sleep(time: number) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-async function cleanUpActiveInstances() {
-    let registryInstances: IProvisionedInstances;
-    let error: ImperativeError;
-    try {
-        registryInstances = await ListRegistryInstances.listRegistryCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION);
-        for (const instance of registryInstances["scr-list"]) {
-            let instanceState: string;
-            let instanceId: string;
-            if (instance["external-name"].includes(instanceName)) {
-                instanceId = instance["object-id"];
-                if (instance.state === "being-provisioned") {
-                    instanceId = instance["object-id"];
-                    instanceState = instance.state;
-                    while (instanceState === "being-provisioned") {
-                        await sleep(SLEEP_TIME);
-                        instanceState = (await ListInstanceInfo.listInstanceCommon(REAL_SESSION,
-                            ProvisioningConstants.ZOSMF_VERSION, instanceId)).state;
-                        Imperative.console.info(`Instance state is ${instanceState}`);
-                    }
-                    Imperative.console.info(`Instance state is ${instanceState}`);
-                    await PerformAction.doProvisioningActionCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                        instanceId, "deprovision");
-                    instanceState = "being-deprovisioned";
-                    while (instanceState === "being-deprovisioned") {
-                        await sleep(SLEEP_TIME);
-                        instanceState = (await ListInstanceInfo.listInstanceCommon(REAL_SESSION,
-                            ProvisioningConstants.ZOSMF_VERSION, instanceId)).state;
-                        Imperative.console.info(`Instance state is ${instanceState}`);
-                    }
-                    Imperative.console.info(`Instance state is ${instanceState}`);
-                    await DeleteInstance.deleteDeprovisionedInstance(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION, instanceId);
-
-                } else {
-                    await PerformAction.doProvisioningActionCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                        instanceId, "deprovision");
-                }
-            }
-        }
-
-    } catch (thrownError) {
-        error = thrownError;
-        Imperative.console.info(`Error ${error}`);
-    }
-}
-
 describe("ProvisionPublishedTemplate (system)", () => {
     beforeAll(async () => {
         testEnvironment = await TestEnvironment.setUp({
@@ -127,38 +65,40 @@ describe("ProvisionPublishedTemplate (system)", () => {
 
         templateName = testEnvironment.systemTestProperties.provisioning.templateName;
         instanceName = testEnvironment.systemTestProperties.provisioning.instanceName;
-        accountNumber = defaultSystem.tso.account;
     });
 
     describe("ProvisionPublishedTemplate.provisionTemplateCommon", () => {
         afterAll(async () => {
             await TestEnvironment.cleanUp(testEnvironment);
-            await cleanUpActiveInstances();
-        }, MAX_TIMEOUT_NUMBER);
+        });
 
         it("should succeed with all correct parameters", async () => {
             let response: IProvisionTemplateResponse;
+            let instanceID: string;
             let error: ImperativeError;
             let OBJECT_URI_RESPONSE: string;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                    templateName, accountNumber);
+                    templateName, ACCOUNT_NUMBER);
+                instanceID = response["registry-info"]["object-id"];
                 Imperative.console.info(`Response ${response}`);
-                OBJECT_URI_RESPONSE = OBJECT_URI + response["registry-info"]["object-id"];
+                OBJECT_URI_RESPONSE = OBJECT_URI + instanceID;
+                // Delete the provisioned instance
+                await ProvisioningTestUtils.removeProvisionedInstance(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION, instanceID);
             } catch (thrownError) {
                 error = thrownError;
                 Imperative.console.info(`Error ${error}`);
             }
             expectZosmfResponseSucceeded(response, error);
             expect(response["registry-info"]["object-uri"]).toEqual(OBJECT_URI_RESPONSE);
-        }, MAX_TIMEOUT_NUMBER);
+        }, MAX_TIMEOUT_TIME);
 
         it("should throw an error if the session parameter is undefined", async () => {
             let response: IProvisionTemplateResponse;
             let error: ImperativeError;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(undefined, ProvisioningConstants.ZOSMF_VERSION,
-                    templateName, accountNumber);
+                    templateName, null);
                 Imperative.console.info(`Response ${response}`);
             } catch (thrownError) {
                 error = thrownError;
@@ -172,7 +112,7 @@ describe("ProvisionPublishedTemplate (system)", () => {
             let error: ImperativeError;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(REAL_SESSION, undefined,
-                    templateName, accountNumber);
+                    templateName, null);
                 Imperative.console.info(`Response ${response}`);
             } catch (thrownError) {
                 error = thrownError;
@@ -186,7 +126,7 @@ describe("ProvisionPublishedTemplate (system)", () => {
             let error: ImperativeError;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(REAL_SESSION, "",
-                    templateName, accountNumber);
+                    templateName, null);
                 Imperative.console.info(`Response ${response}`);
             } catch (thrownError) {
                 error = thrownError;
@@ -200,7 +140,7 @@ describe("ProvisionPublishedTemplate (system)", () => {
             let error: ImperativeError;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                    undefined, accountNumber);
+                    undefined, null);
                 Imperative.console.info(`Response ${response}`);
             } catch (thrownError) {
                 error = thrownError;
@@ -214,7 +154,7 @@ describe("ProvisionPublishedTemplate (system)", () => {
             let error: ImperativeError;
             try {
                 response = await ProvisionPublishedTemplate.provisionTemplateCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                    "", accountNumber);
+                    "", null);
                 Imperative.console.info(`Response ${response}`);
             } catch (thrownError) {
                 error = thrownError;
@@ -256,8 +196,7 @@ describe("ProvisionPublishedTemplate (system)", () => {
     describe("ProvisionPublishedTemplate.provisionTemplate", () => {
         afterAll(async () => {
             await TestEnvironment.cleanUp(testEnvironment);
-            await cleanUpActiveInstances();
-        }, MAX_TIMEOUT_NUMBER);
+        });
 
         it("should throw an error if the session parameter is undefined", async () => {
             let response: IProvisionTemplateResponse;
@@ -324,23 +263,26 @@ describe("ProvisionPublishedTemplate (system)", () => {
             expectZosmfResponseFailed(response, error, noTemplateName.message);
         });
 
-        it("should succeed with passed optional parameters", async () => {
+        it("should succeed with no optional parameters", async () => {
             let response: IProvisionTemplateResponse;
+            let instanceID: string;
             let error: ImperativeError;
             let OBJECT_URI_RESPONSE: string;
             try {
-                inputProperties["account-info"] = accountNumber;
                 response = await ProvisionPublishedTemplate.provisionTemplate(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-                    templateName, inputProperties);
+                    templateName, null);
+                instanceID = response["registry-info"]["object-id"];
                 Imperative.console.info(`Response ${response}`);
-                OBJECT_URI_RESPONSE = OBJECT_URI + response["registry-info"]["object-id"];
+                OBJECT_URI_RESPONSE = OBJECT_URI + instanceID;
+                // Delete the provisioned instance
+                await ProvisioningTestUtils.removeProvisionedInstance(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION, instanceID);
             } catch (thrownError) {
                 error = thrownError;
                 Imperative.console.info(`Error ${error}`);
             }
             expectZosmfResponseSucceeded(response, error);
             expect(response["registry-info"]["object-uri"]).toEqual(OBJECT_URI_RESPONSE);
-        }, MAX_TIMEOUT_NUMBER);
+        }, MAX_TIMEOUT_TIME);
     });
 });
 
