f46347e2b21f15f00cbd59bda166f65b119220a1
Rauan Nurbatyrov <rauan.nurbatyrov@broadcom.com>
2019-04-29 08:30:57 +0200

    add tests for perform command and remove them from delete suites
    Signed-off-by: cy <cy.cao@163.com>

--- packages/provisioning/__tests__/__system__/cli/perform/cli.provisioning.perform.action.system.test.ts
+++ packages/provisioning/__tests__/__system__/cli/perform/cli.provisioning.perform.action.system.test.ts
@@ -15,13 +15,15 @@ import { ITestEnvironment } from "../../../../../../__tests__/__src__/environmen
 import { TestEnvironment } from "../../../../../../__tests__/__src__/environment/TestEnvironment";
 import { ITestSystemSchema } from "../../../../../../__tests__/__src__/properties/ITestSystemSchema";
 import { TestProperties } from "../../../../../../__tests__/__src__/properties/TestProperties";
-import { PerformAction, ProvisioningConstants } from "../../../../";
+import { ProvisioningConstants } from "../../../../";
+import * as fs from "fs";
 import { ProvisioningTestUtils } from "../../../__resources__/utils/ProvisioningTestUtils";
 
 // Test Environment populated in the beforeAll();
 let TEST_ENVIRONMENT: ITestEnvironment;
+let SYSTEM_PROPS: TestProperties;
 let REAL_SESSION: Session;
-let defaultSystem: ITestPropertiesSchema;
+let defaultSystem: ITestSystemSchema;
 let templateName: string;
 let instance;
 let instanceID: string;
@@ -32,38 +34,53 @@ describe("provisioning delete instance", () => {
     // Create the unique test environment
     beforeAll(async () => {
         TEST_ENVIRONMENT = await TestEnvironment.setUp({
-            testName: "provisioning_delete_instance",
+            testName: "provisioning_perform_action",
             tempProfileTypes: ["zosmf", "tso"]
         });
 
-        defaultSystem = TEST_ENVIRONMENT.systemTestProperties;
+        SYSTEM_PROPS = new TestProperties(TEST_ENVIRONMENT.systemTestProperties);
+        defaultSystem = SYSTEM_PROPS.getDefaultSystem();
         templateName = templateName = TEST_ENVIRONMENT.systemTestProperties.provisioning.templateName;
         REAL_SESSION = TestEnvironment.createZosmfSession(TEST_ENVIRONMENT);
 
+        // Provision an instance to use it later
         instance = await ProvisioningTestUtils.getProvisionedInstance(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION, templateName);
-        Imperative.console.info(`Provisioned instance: ${instance["external-name"]}`);
         instanceID = instance["object-id"];
+        instanceName = instance["external-name"];
+        Imperative.console.info(`Provisioned instance: ${instanceName}`);
+    }, ProvisioningTestUtils.MAX_TIMEOUT_TIME);
 
-        // Deprovision the instance
-        instance = await PerformAction.doProvisioningActionCommon(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
-            instanceID, ProvisioningTestUtils.ACTION_DEPROV);
-        Imperative.console.info(`Deprovision of the instance started, action-id: ${instance["action-id"]}`);
+    afterAll(async () => {
+        await TestEnvironment.cleanUp(TEST_ENVIRONMENT);
+
         // Wait until instance state is 'deprovisioned'
         instance = await ProvisioningTestUtils.waitInstanceState(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION,
             instanceID, ProvisioningTestUtils.STATE_DEPROV);
         instanceName = instance["external-name"];
         Imperative.console.info(`Deprovisioned instance: ${instanceName}`);
+
+        // Delete deprovisioned instance
+        await ProvisioningTestUtils.removeRegistryInstance(REAL_SESSION, ProvisioningConstants.ZOSMF_VERSION, instanceID);
     }, ProvisioningTestUtils.MAX_TIMEOUT_TIME);
 
-    afterAll(async () => {
-        await TestEnvironment.cleanUp(TEST_ENVIRONMENT);
-    });
-
-    it("should successfully issue the command", async () => {
-        const response = runCliScript(__dirname + "/__scripts__/instance/delete_instance_success.sh", TEST_ENVIRONMENT,
+    // system test for perform action
+    it("should successfully perform checkStatus action", async () => {
+        const regex = fs.readFileSync(__dirname + "/../perform/__regex__/perform_action_response.regex").toString();
+        const response = runCliScript(__dirname + "/__scripts__/instance/perform_checkStatus_action_success.sh", TEST_ENVIRONMENT,
             [instanceName]);
         expect(response.stderr.toString()).toBe("");
         expect(response.status).toBe(0);
-        expect(response.stdout).toMatchSnapshot();
+        expect(new RegExp(regex, "g").test(response.stdout.toString())).toBe(true);
+    }, ProvisioningTestUtils.MAX_CLI_TIMEOUT);
+
+    // system test for perform action
+    it("should successfully perform deprovision action", async () => {
+        const regex = fs.readFileSync(__dirname + "/../perform/__regex__/perform_action_response.regex").toString();
+        const response = runCliScript(__dirname + "/__scripts__/instance/perform_deprovision_action_success.sh", TEST_ENVIRONMENT,
+            [instanceName]);
+        expect(response.stderr.toString()).toBe("");
+        expect(response.status).toBe(0);
+        expect(new RegExp(regex, "g").test(response.stdout.toString())).toBe(true);
     }, ProvisioningTestUtils.MAX_CLI_TIMEOUT);
 });
+
