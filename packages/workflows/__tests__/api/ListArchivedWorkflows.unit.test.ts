62abadbaad7b86b0b0990f3b0b3d5e1130f00cef
melvl01 <melvl01@ca.com>
2019-05-22 12:43:39 +0200

    Port changes to master
    Signed-off-by: melvl01 <melvl01@ca.com>

--- packages/workflows/__tests__/api/ListArchivedWorkflows.unit.test.ts
+++ packages/workflows/__tests__/api/ListArchivedWorkflows.unit.test.ts
@@ -10,24 +10,12 @@
 */
 
 import { ZosmfRestClient } from "../../../rest";
-import { Session, ImperativeError, Imperative } from "@zowe/imperative";
+import { Session, ImperativeError, Imperative, Headers } from "@zowe/imperative";
 import { ListArchivedWorkflows } from "../../src/api/ListArchivedWorkflows";
-import { WorkflowConstants, wrongString, noSession, noWorkflowName } from "../../src/api/WorkflowConstants";
+import { WorkflowConstants, noSession } from "../../src/api/WorkflowConstants";
 import { IWorkflowsInfo } from "../../src/api/doc/IWorkflowsInfo";
-import { IArchivedWorkflows } from "../../src/api/doc/IArchivedWorkflows";
-
-const system = "SYS1";
-const category = "Provisioning";
-const statusName = "complete";
-const owner = "owner1";
-const vendor = "IBM";
-const workflowName = "workflow one";
-const badString = "Ba?d";
-const badString1 = "Ba&d";
 
 const START_RESOURCE_QUERY: string = `${WorkflowConstants.RESOURCE}/${WorkflowConstants.ZOSMF_VERSION}/${WorkflowConstants.ARCH_WORKFLOW_RESOURCE}`;
-const PRETEND_URL = START_RESOURCE_QUERY + `?workflowName=${encodeURIComponent(workflowName)}&category=${category}`
-                                         + `&system=${system}&owner=${owner}&vendor=${vendor}&statusName=${statusName}`;
 
 const PRETEND_ZOSMF_RESPONSE: IWorkflowsInfo = {
     workflowKey: "73c81ef4-eccc-47ce-8f08-8a5c97e753f1",
@@ -42,20 +30,6 @@ const PRETEND_ZOSMF_RESPONSE: IWorkflowsInfo = {
     system: "SYS1"
 };
 
-const PRETEND_ZOSMF_RESPONSE_MULTIPLE_WF: IArchivedWorkflows = {
-    archivedWorkflows: [
-        {
-            workflowKey: "73c81ef4-eccc-47ce-8f08-8a5c97e753f1",
-            workflowName: "workflow1",
-            archivedInstanceURI: "\/zosmf\/workflow\/rest\/1.0\/archivedworkflows\/73c81ef4-eccc-47ce-8f08-8a5c97e753f1"
-        },
-        {
-            workflowKey: "73c81ef4-eccc-47ce-8f08-8a5c97e753f2",
-            workflowName: "workflow2",
-            archivedInstanceURI: "\/zosmf\/workflow\/rest\/1.0\/archivedworkflows\/73c81ef4-eccc-47ce-8f08-8a5c97e753f2"
-        }
-    ]
-};
 
 const PRETEND_SESSION = new Session({
     user: "usr",
@@ -79,32 +53,6 @@ function expectZosmfResponseFailed(response: any, error: ImperativeError, msg: s
 }
 
 describe("List archived workflows", () => {
-    // List archived workflow that match all optional parameters
-    it("Successful call with all optional parameters.", async () => {
-
-        (ZosmfRestClient.getExpectJSON as any) = jest.fn<string>(() => {
-            return new Promise((resolve) => {
-                process.nextTick(() => {
-                    resolve(PRETEND_ZOSMF_RESPONSE);
-                });
-            });
-        });
-
-        let error: ImperativeError;
-        let response: any;
-        try {
-            response = await ListArchivedWorkflows.listArchivedWorkflows(PRETEND_SESSION, undefined,
-                                                                         workflowName, category, system, owner, vendor, statusName );
-            Imperative.console.info(`Response ${response}`);
-        } catch (thrownError) {
-            error = thrownError;
-            Imperative.console.info(`Error ${error}`);
-        }
-        expect((ZosmfRestClient.getExpectJSON as any)).toHaveBeenCalledTimes(1);
-        expect((ZosmfRestClient.getExpectJSON as any)).toHaveBeenCalledWith(PRETEND_SESSION, PRETEND_URL);
-        expectZosmfResponseSucceeded(response, error);
-        expect(response).toEqual(PRETEND_ZOSMF_RESPONSE);
-    });
     // List all archived workflows - without any optional parameters
     it("Successful call without any optional parameters.", async () => {
 
@@ -126,25 +74,12 @@ describe("List archived workflows", () => {
             Imperative.console.info(`Error ${error}`);
         }
         expect((ZosmfRestClient.getExpectJSON as any)).toHaveBeenCalledTimes(1);
-        expect((ZosmfRestClient.getExpectJSON as any)).toHaveBeenCalledWith(PRETEND_SESSION, START_RESOURCE_QUERY);
+        expect((ZosmfRestClient.getExpectJSON as any)).toHaveBeenCalledWith(PRETEND_SESSION, START_RESOURCE_QUERY, [Headers.APPLICATION_JSON]);
         expectZosmfResponseSucceeded(response, error);
         expect(response).toEqual(PRETEND_ZOSMF_RESPONSE);
     });
 
     describe("Fail scenarios", () => {
-        it("Throws an error with incorrect parameter format.", async () => {
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.listArchivedWorkflows(PRETEND_SESSION, undefined,
-                                                                             badString, badString1, badString, badString, badString);
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-                error = thrownError;
-                Imperative.console.info(`Error ${error}`);
-            }
-            expectZosmfResponseFailed(response, error, wrongString.message);
-        });
         it("Throws an error with undefined session.", async () => {
             let error: ImperativeError;
             let response: any;
@@ -156,97 +91,6 @@ describe("List archived workflows", () => {
               Imperative.console.info(`Error ${error}`);
             }
             expectZosmfResponseFailed(response, error, noSession.message);
-        });
-    });
-});
-describe("Get workflow key by name", () => {
-    describe("Success scenarios", () => {
-        it("Returns wf key if only one wf with requested name was found", async () => {
-            const apiResponse = {archivedWorkflows: [PRETEND_ZOSMF_RESPONSE]};
-            (ZosmfRestClient.getExpectJSON as any) = jest.fn<string>(() => {
-                return new Promise((resolve) => {
-                    process.nextTick(() => {
-                        resolve(apiResponse);
-                    });
-                });
-            });
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.getWfKey(PRETEND_SESSION, workflowName);
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-              error = thrownError;
-              Imperative.console.info(`Error ${error}`);
-            }
-            expect(response).toBe(apiResponse.archivedWorkflows[0].workflowKey);
-            expect(error).toBe(undefined);
-        });
-
-        it("Returns null if no workflow with requested name was found", async () => {
-            (ZosmfRestClient.getExpectJSON as any) = jest.fn<string>(() => {
-                return new Promise((resolve) => {
-                    process.nextTick(() => {
-                        resolve({archivedWorkflows: []});
-                    });
-                });
-            });
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.getWfKey(PRETEND_SESSION, workflowName);
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-              error = thrownError;
-              Imperative.console.info(`Error ${error}`);
-            }
-            expect(response).toBe(null);
-            expect(error).toBe(undefined);
-        });
-    });
-    describe("Fail scenarios", () => {
-        it("Throws an error with undefined session.", async () => {
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.getWfKey(undefined, workflowName);
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-              error = thrownError;
-              Imperative.console.info(`Error ${error}`);
-            }
-            expectZosmfResponseFailed(response, error, noSession.message);
-        });
-        it("Throws an error with undefined wf key.", async () => {
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.getWfKey(PRETEND_SESSION, undefined);
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-              error = thrownError;
-              Imperative.console.info(`Error ${error}`);
-            }
-            expectZosmfResponseFailed(response, error, noWorkflowName.message);
-        });
-        it("Throws an error if multiple wf were found.", async () => {
-            (ZosmfRestClient.getExpectJSON as any) = jest.fn<string>(() => {
-                return new Promise((resolve) => {
-                    process.nextTick(() => {
-                        resolve(PRETEND_ZOSMF_RESPONSE_MULTIPLE_WF);
-                    });
-                });
-            });
-            let error: ImperativeError;
-            let response: any;
-            try {
-                response = await ListArchivedWorkflows.getWfKey(PRETEND_SESSION, "workflow.*");
-                Imperative.console.info(`Response ${response}`);
-            } catch (thrownError) {
-              error = thrownError;
-              Imperative.console.info(`Error ${error}`);
-            }
-            expectZosmfResponseFailed(response, error, "More than one workflows found with name");
         });
     });
 });
--- packages/workflows/src/api/ListArchivedWorkflows.ts
+++ packages/workflows/src/api/ListArchivedWorkflows.ts
@@ -11,93 +11,18 @@
 
 import { ZosmfRestClient } from "../../../rest";
 import { WorkflowValidator } from "./WorkflowValidator";
-import { AbstractSession, ImperativeError } from "@zowe/imperative";
-import { WorkflowConstants, nozOSMFVersion, wrongString, noWorkflowName } from "./WorkflowConstants";
+import { AbstractSession, Headers } from "@zowe/imperative";
+import { WorkflowConstants, nozOSMFVersion } from "./WorkflowConstants";
 import { IArchivedWorkflows } from "./doc/IArchivedWorkflows";
 
-/**
- * Get list of archived workflows from registry.
- * @export
- * @class ListArchivedWorkflows
- */
+
 export class ListArchivedWorkflows {
-    /**
-     * This operation returns list of archived workflows.
-     * Parameters are optional,request can include one or more parameters to filter the results.
-     * @param {AbstractSession} session - z/OSMF connection info
-     * @param {string} zOSMFVersion - the URI path that identifies the version of the provisioning service.
-     * @param {string} workflowName - the URI path with optional parameter for listing filtered workflows.
-     * @param {string} category - the URI path with optional parameter for listing filtered workflows.
-     * @param {string} system - the URI path with optional parameter for listing filtered workflows.
-     * @param {string} owner - the URI path with optional parameter for listing filtered workflows.
-     * @param {string} vendor - the URI path with optional parameter for listing filtered workflows.
-     * @param {string} statusName - the URI path with optional parameter for listing filtered workflows.
-     * @returns {string} z/OSMF response object
-     * @memberof ListArchivedWorkflows
-     */
-    public static async listArchivedWorkflows(session: AbstractSession, zOSMFVersion = WorkflowConstants.ZOSMF_VERSION,
-                                              workflowName?: string, category?: string, system?: string,
-                                              owner?: string, vendor?: string, statusName?: string ): Promise<IArchivedWorkflows> {
+   public static async listArchivedWorkflows(session: AbstractSession,
+                                             zOSMFVersion = WorkflowConstants.ZOSMF_VERSION): Promise<IArchivedWorkflows> {
         WorkflowValidator.validateSession(session);
         WorkflowValidator.validateNotEmptyString(zOSMFVersion, nozOSMFVersion.message);
-        const resourcesQuery: string = ListArchivedWorkflows.getResourcesQuery(zOSMFVersion,
-            [
-                {key: WorkflowConstants.workflowName, value : workflowName ? encodeURIComponent(workflowName) : null},
-                {key: WorkflowConstants.category, value : category},
-                {key: WorkflowConstants.system, value : system},
-                {key: WorkflowConstants.owner, value : owner},
-                {key: WorkflowConstants.vendor, value : vendor},
-                {key: WorkflowConstants.statusName, value : statusName},
-            ]
-        );
-        return ZosmfRestClient.getExpectJSON(session, resourcesQuery);
-    }
+        const resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.ARCH_WORKFLOW_RESOURCE}`;
 
-    /**
-     * This operation Builds URI path from provided parameters.
-     * @param {string} zOSMFVersion - the URI path that identifies the version of the provisioning service.
-     * @param {string} params - The array with URI path with filters for listing filtered workflows.
-     * @returns {string} URI path for the REST call.
-     * @memberof LisArchivedWorkflows
-     */
-    public static getResourcesQuery(zOSMFVersion: string, params: Array <{key: string, value: string}>): string {
-        let query: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.ARCH_WORKFLOW_RESOURCE}`;
-        let sign = "?";
-        params.forEach((element) => {
-            if (element.value) {
-                // Validate if parameter value does not contains ? or &
-                WorkflowValidator.validateParameter(element.value, wrongString.message);
-                query += sign + `${element.key}=${element.value}`;
-                sign = "&";
-            }
-        });
-        return query;
-    }
-
-    /**
-     * This operation is used to return an archived worflow-key by given workflow name.
-     * @param {AbstractSession} session - z/OSMF connection info
-     * @param {string} workflowName - workflow name by which to list workflows
-     * @param {string} zOSMFVersion - identifies the version of the provisioning service.
-     * @returns {Promise<string> | null} - Promise with string containing wf key, or null if none was found
-     * @throws {ImperativeError}
-     * @memberof ListArchivedWorkflows
-     */
-    public static async getWfKey(session: AbstractSession, workflowName: string,
-                                 zOSMFVersion = WorkflowConstants.ZOSMF_VERSION): Promise<string> | null{
-
-        WorkflowValidator.validateSession(session);
-        WorkflowValidator.validateNotEmptyString(zOSMFVersion, nozOSMFVersion.message);
-        WorkflowValidator.validateNotEmptyString(workflowName, noWorkflowName.message);
-
-        const result: IArchivedWorkflows = await this.listArchivedWorkflows(session, zOSMFVersion, workflowName);
-
-        // Check if there was more than one workflows found
-        if (result.archivedWorkflows.length > 1){
-            throw new ImperativeError({
-                msg: `More than one workflows found with name ` + workflowName,
-            });
-        }
-        return result.archivedWorkflows.length !== 0 ? result.archivedWorkflows[0].workflowKey : null;
+        return ZosmfRestClient.getExpectJSON (session, resourcesQuery, [Headers.APPLICATION_JSON]);
     }
 }
