7eca6cf14b270dec644f4dd7993943e5b434f0a3
Timothy Johnson <timothy.johnson@broadcom.com>
2019-06-10 10:46:36 -0400

    Add CLI integration tests for delete command
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    Signed-off-by: cy <cy.cao@163.com>

--- packages/zosfiles/__tests__/__integration__/cli/delete/zfs/__scripts__/delete_zfs_help.sh
+++ packages/zosfiles/__tests__/__integration__/cli/delete/zfs/__scripts__/delete_zfs_help.sh
@@ -2,12 +2,12 @@
 set -e
 
 echo "================Z/OS FILES DELETE HELP==============="
-zowe zos-files delete vsam --help
+zowe zos-files delete zfs --help
 if [ $? -gt 0 ]
 then
     exit $?
 fi
 
 echo "================Z/OS FILES DELETE HELP WITH RFJ==========="
-zowe zos-files delete vsam --help --rfj
+zowe zos-files delete zfs --help --rfj
 exit $?
