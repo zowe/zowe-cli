5c5369cd77ce01bccadb452ed8dd0e047adb0a04
Andrew Smithson <smithson@uk.ibm.com>
2019-06-30 07:34:17 +0000

    Allow JCL to be submitted from stdin
    Signed-off-by: Andrew Smithson <smithson@uk.ibm.com>
    Signed-off-by: cy <cy.cao@163.com>

---
+++ packages/zosjobs/src/cli/submit/stdin/stdin.definition.ts
@@ -0,0 +1,45 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ICommandDefinition, ICommandOptionDefinition } from "@zowe/imperative";
+
+export const StdinDefinition: ICommandDefinition = {
+    name: "stdin",
+    aliases: ["in"],
+    type: "command",
+    summary: "Submit a job read from standard in",
+    description: "Submit a job (JCL) passed to the command via the stdin stream. " +
+        "The command presents errors verbatim from the z/OSMF Jobs REST endpoints. " +
+        "For more information about z/OSMF Jobs API errors, see the z/OSMF Jobs API REST documentation.",
+    handler: __dirname + "/../Submit.shared.handler",
+    options: ([
+        {
+            name: "view-all-spool-content", aliases: ["vasc"],
+            description: "View all spool content for specified job ID",
+            type: "boolean"
+        },
+        {
+            name: "directory", aliases: ["d"],
+            description: "The local directory you would like to download the output for the job to. "
+                + "Creates a subdirectory using the jobID as the name and files are titled based on DD names.",
+            type: "string"
+        },
+        {
+            name: "extension", aliases: ["e"],
+            description: "A file extension to save the job output with",
+            type: "string"
+        },
+    ] as ICommandOptionDefinition[]),
+    profile: {
+        optional: ["zosmf"]
+    },
+    outputFormatOptions: true,
+};
