8f8168f43ffbef5dabae183fa0d7087175202bba
Andrew Smithson <smithson@uk.ibm.com>
2019-07-05 13:29:54 +0000

    Add tests for new submit option
    Signed-off-by: Andrew Smithson <smithson@uk.ibm.com>
    Signed-off-by: cy <cy.cao@163.com>

--- packages/zosjobs/__tests__/__integration__/cli/submit/stdin/cli.zos-jobs.submit.stdin.test.ts
+++ packages/zosjobs/__tests__/__integration__/cli/submit/stdin/cli.zos-jobs.submit.stdin.test.ts
@@ -1,14 +1,3 @@
-/*
-* This program and the accompanying materials are made available under the terms of the
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Copyright Contributors to the Zowe Project.
-*
-*/
-
 import { ITestEnvironment } from "../../../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
 import { TestEnvironment } from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
 import { runCliScript } from "../../../../../../../__tests__/__src__/TestUtils";
