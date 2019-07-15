4def5236ed8de2935d5dc77ce13e56643880d0ea
Timothy Johnson <timothy.johnson@broadcom.com>
2019-07-15 14:43:49 -0400

    Add config options to web help build script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- web-help/build-web-help.ts
+++ web-help/build-web-help.ts
@@ -2,7 +2,15 @@ import * as fs from "fs";
 import * as path from "path";
 import { CommandResponse, WebHelpGenerator } from "@zowe/imperative";
 
+interface IConfig {
+    cliPackage: null | string,
+    excludeGroups: string[],
+};
+
 (async () => {
+    const jsonText: string = fs.readFileSync(__dirname + "/config.json").toString();
+    const config: IConfig = JSON.parse(require("strip-json-comments")(jsonText));
+
     const outDir: string = __dirname + "/dist";
     if (fs.existsSync(outDir)) {
         require("rimraf").sync(outDir + "/*");
@@ -10,13 +18,14 @@ import { CommandResponse, WebHelpGenerator } from "@zowe/imperative";
         fs.mkdirSync(outDir);
     }
 
-    let cliPackageDir: string = "../";
+    let cliPackageDir: string = path.join(__dirname, "..");
     let imperativeImportPath: string = "@zowe/imperative";
     let imperativeRequirePath: string = "../packages/imperative";
-    if (process.argv.length > 2) {
-        cliPackageDir = process.argv[2];
-        imperativeImportPath = path.join(__dirname, cliPackageDir, "../imperative");
-        imperativeRequirePath = path.join(__dirname, cliPackageDir, "lib/imperative");
+
+    if (config.cliPackage) {
+        cliPackageDir = path.join(__dirname, "node_modules", config.cliPackage);
+        imperativeImportPath = path.join(cliPackageDir, "../imperative");
+        imperativeRequirePath = path.join(cliPackageDir, "lib/imperative");
     }
 
     // Get all command definitions
@@ -27,10 +36,11 @@ import { CommandResponse, WebHelpGenerator } from "@zowe/imperative";
     myConfig.loadedConfig.commandModuleGlobs = ["**/!(__tests__)/cli/*.definition!(.d).*s"];
     // Need to set this for the internal caller location so that the commandModuleGlobs finds the commands
     const oldFilename: string = process.mainModule.filename;
-    process.mainModule.filename = path.join(__dirname, cliPackageDir, "package.json");
+    process.mainModule.filename = path.join(cliPackageDir, "package.json");
     // Initialize Imperative for commands to document
     await imperativeModule.Imperative.init(myConfig.loadedConfig);
     process.mainModule.filename = oldFilename;
+    console.log(`Initialized Imperative for ${myConfig.callerPackageJson.name} ${myConfig.callerPackageJson.version}`);
 
     // Ensure required config values are defined
     if (myConfig.rootCommandName === undefined) {
@@ -40,8 +50,13 @@ import { CommandResponse, WebHelpGenerator } from "@zowe/imperative";
         myConfig.loadedConfig.webHelpLogoImgPath = __dirname + "/logo.png";
     }
 
+    // Exclude undesired command groups
+    const cmdDefinitions: any = imperativeModule.Imperative.fullCommandTree;
+    cmdDefinitions.children = cmdDefinitions.children
+        .filter((group: any) => config.excludeGroups.indexOf(group.name) === -1);
+
     // Build command help pages
-    const helpGenerator = new WebHelpGenerator(imperativeModule.Imperative.fullCommandTree, myConfig, outDir);
+    const helpGenerator = new WebHelpGenerator(cmdDefinitions, myConfig, outDir);
     helpGenerator.sanitizeHomeDir = true;
     helpGenerator.buildHelp(new CommandResponse({ silent: false }));
 })();
