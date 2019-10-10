532166dd7a192537bfba55560cf27ef44a373bd8
ChrisBoehmCA <christopher.boehm@broadcom.com>
2019-10-10 14:18:02 -0400

    fix lint
    Signed-off-by: ChrisBoehmCA <christopher.boehm@broadcom.com>

--- packages/zosjobs/__tests__/__integration__/cli/submit/stdin/cli.zos-jobs.submit.stdin.integration.test.ts
+++ packages/zosjobs/__tests__/__integration__/cli/submit/stdin/cli.zos-jobs.submit.stdin.integration.test.ts
@@ -9,9 +9,9 @@
 *
 */
 
-import {ITestEnvironment} from "../../../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
-import {TestEnvironment} from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
-import {runCliScript} from "../../../../../../../__tests__/__src__/TestUtils";
+import { ITestEnvironment } from "../../../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
+import { TestEnvironment } from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
+import { runCliScript } from "../../../../../../../__tests__/__src__/TestUtils";
 
 process.env.FORCE_COLOR = "0";
 
