e94fa3316f435e3b25b4894c911fdfe07ca715db
Pranay Sodani <pranay.sodaniibm.com@pranays-mbp.hursley.uk.ibm.com>
2019-11-04 11:49:11 +0000

    Added in zOSMF failure unit test
    Signed-off-by: Pranay Sodani <pranay.sodaniibm.com@pranays-mbp.hursley.uk.ibm.com>

--- packages/zosfiles/__tests__/api/methods/hMigrate/HMigrate.unit.test.ts
+++ packages/zosfiles/__tests__/api/methods/hMigrate/HMigrate.unit.test.ts
@@ -9,7 +9,7 @@
 *
 */
 
-import { Session } from "@zowe/imperative";
+import { Session, ImperativeError } from "@zowe/imperative";
 import { posix } from "path";
 import { HMigrate, ZosFilesConstants, ZosFilesMessages } from "../../../..";
 
@@ -97,6 +97,44 @@ describe("hMigrate data set", () => {
                 expectedHeaders,
                 expectedPayload
             );
+        });
+    });
+    describe("Failure Scenarios", () => {
+        const dataSetName: string = "EFGH";
+        const errorMessage = "Dummy error message";
+
+        it("should fail if the zOSMF REST client fails", async () => {
+            putExpectStringSpy.mockImplementation(() => {
+                throw new ImperativeError({msg: errorMessage});
+            });
+            const expectedPayload = {
+                "request": "hmigrate",
+            };
+            const expectedEndpoint = posix.join(
+                ZosFilesConstants.RESOURCE,
+                ZosFilesConstants.RES_DS_FILES,
+                dataSetName
+            );
+            const expectedHeaders = [
+                { "Content-Type": "application/json" },
+                { "Content-Length": JSON.stringify(expectedPayload).length.toString() },
+            ];
+
+            let error;
+            try {
+                await HMigrate.dataSet(dummySession, dataSetName);
+            } catch (err) {
+                error = err.message;
+            }
+
+            expect(putExpectStringSpy).toHaveBeenCalledTimes(1);
+            expect(putExpectStringSpy).toHaveBeenLastCalledWith(
+                dummySession,
+                expectedEndpoint,
+                expectedHeaders,
+                expectedPayload
+            );
+            expect(error).toContain(errorMessage);
         });
     });
 });
