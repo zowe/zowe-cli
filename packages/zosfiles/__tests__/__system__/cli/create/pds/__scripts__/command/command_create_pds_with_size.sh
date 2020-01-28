a1ae4d6c4500d5f23fe1e6fe7d91a772e47682d7
Andrew W. Harn <andrew.harn@broadcom.com>
2020-01-28 16:58:01 +0000

    Print attributes on the size tests
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/zosfiles/__tests__/__system__/cli/create/pds/__scripts__/command/command_create_pds_with_size.sh
+++ packages/zosfiles/__tests__/__system__/cli/create/pds/__scripts__/command/command_create_pds_with_size.sh
@@ -3,7 +3,7 @@ hlq=$1
 set -e
 
 echo "================Z/OS FILES CREATE PDS==============="
-zowe zos-files create data-set-partitioned "$1.test.data.set.pds.size" --size 2CYL
+zowe zos-files create data-set-partitioned "$1.test.data.set.pds.size" --size 2CYL --pa
 if [ $? -gt 0 ]
 then
     exit $?
