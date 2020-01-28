fdb849bff7ad4fe37e1d49de29df69a4c7a8825f
Andrew W. Harn <andrew.harn@broadcom.com>
2020-01-28 17:07:34 +0000

    Run the right test.
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/zosfiles/__tests__/__system__/cli/create/binaryPds/__scripts__/command/command_create_binary_pds_with_primary_secondary.sh
+++ packages/zosfiles/__tests__/__system__/cli/create/binaryPds/__scripts__/command/command_create_binary_pds_with_primary_secondary.sh
@@ -3,7 +3,7 @@ hlq1=$1
 set -e
 
 echo "================Z/OS FILES CREATE DATA-SET-BINARY==============="
-zowe zos-files create data-set-partitioned "$1.test.data.set.binary.second" --ps 20 --ss 20 --pa
+zowe zos-files create data-set-binary "$1.test.data.set.binary.second" --ps 20 --ss 5 --pa
 if [ $? -gt 0 ]
 then
     exit $?
