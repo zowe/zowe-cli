fca4d682aec280b65588a4a64bb04c673ab5d54f
Gene Johnston <eugene.johnston@broadcom.com>
2020-04-02 11:47:17 -0400

    Changes from updateLicense.
    Signed-off-by: Gene Johnston <eugene.johnston@broadcom.com>

--- packages/zosmf/src/api/Login.ts
+++ packages/zosmf/src/api/Login.ts
@@ -1,4 +1,3 @@
-
 /*
 * This program and the accompanying materials are made available under the terms of the
 * Eclipse Public License v2.0 which accompanies this distribution, and is available at
@@ -9,6 +8,7 @@
 * Copyright Contributors to the Zowe Project.
 *
 */
+
 import { AbstractSession, ImperativeExpect, Logger, ImperativeError, RestConstants } from "@zowe/imperative";
 import { ZosmfRestClient } from "../../../rest";
 import { LoginConstants } from "./LoginConstants";
