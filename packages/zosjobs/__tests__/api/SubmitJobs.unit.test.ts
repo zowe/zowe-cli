cfc6a21b4a6c411ec220913b3abd751f8d8702ef
Gene Johnston <eugene.johnston@broadcom.com>
2020-04-02 20:12:59 -0400

    Fix tests broken by relocating ZosmfRestClient.ts
    Signed-off-by: Gene Johnston <eugene.johnston@broadcom.com>

--- packages/zosjobs/__tests__/api/SubmitJobs.unit.test.ts
+++ packages/zosjobs/__tests__/api/SubmitJobs.unit.test.ts
@@ -15,7 +15,7 @@ import { IJob, MonitorJobs, SubmitJobs } from "../../";
 import { ZosmfRestClient } from "../../../rest/src/api/ZosmfRestClient";
 import { ImperativeError } from "@zowe/imperative";
 
-jest.mock("../../../rest/src/ZosmfRestClient");
+jest.mock("../../../rest/src/api/ZosmfRestClient");
 jest.mock("../../src/api/MonitorJobs");
 
 const fakeSession: any = {};
