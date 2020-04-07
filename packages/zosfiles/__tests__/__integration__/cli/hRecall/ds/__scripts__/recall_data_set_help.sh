38979f509be86fe49b9175e962897d264054ffdf
Pranay Sodani <pranay_7@Pranays-MacBook-Pro.local>
2020-04-07 15:56:30 +0100

    Changed alias for recall and migrate
    Signed-off-by: Pranay Sodani <pranay_7@Pranays-MacBook-Pro.local>

--- packages/zosfiles/__tests__/__integration__/cli/hRecall/ds/__scripts__/recall_data_set_help.sh
+++ packages/zosfiles/__tests__/__integration__/cli/hRecall/ds/__scripts__/recall_data_set_help.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "================Z/OS FILES RECALL DATA-SET-SEQUENTIAL HELP==============="
-zowe zos-files hRecall data-set --help
+zowe zos-files recall data-set --help
 if [ $? -gt 0 ]
 then
     exit $?
--- packages/zosfiles/src/cli/hMigrate/HMigrate.definition.ts
+++ packages/zosfiles/src/cli/hMigrate/HMigrate.definition.ts
@@ -22,8 +22,8 @@ const { DESCRIPTION } = (require("../-strings-/en").default as typeof i18nTyping
  * @type {ICommandDefinition}
  */
 export const HMigrateDefinition: ICommandDefinition = {
-    name: "hMigrate",
-    aliases: ["hmigr"],
+    name: "migrate",
+    aliases: ["hmigr", "hMigrate"],
     type: "group",
     description: DESCRIPTION,
     children: [
