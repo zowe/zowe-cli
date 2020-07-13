b9a1ffb9483bd799e55b203b908a998fd028dcbd
Andrew W. Harn <andrew.harn@broadcom.com>
2020-07-13 15:14:32 -0400

    Add zosmf API examples
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

---
+++ docs/Examples/zosmf/CheckStatus.example.ts
@@ -0,0 +1,47 @@
+import { CheckStatus, IZosmfInfoResponse } from "../../../packages/zosmf";
+import { Session, ISession, SessConstants } from "@zowe/imperative";
+import { exit } from "process";
+
+// Connection Options
+const hostname: string = "yourhost.yourdomain.net";
+const port: number = 443;
+const user: string = "ZOWEUSER";
+const password: string = "ZOWEPASS";
+const protocol: SessConstants.HTTP_PROTOCOL_CHOICES = "https";
+const basePath: string = undefined;
+const type: SessConstants.AUTH_TYPE_CHOICES = "basic";
+const tokenType: string = undefined;
+const tokenValue: string = undefined;
+const rejectUnauthorized: boolean = false;
+
+// Session Options
+const sessionConfig: ISession = {
+    hostname,
+    port,
+    user,
+    password,
+    protocol,
+    basePath,
+    type,
+    tokenType,
+    tokenValue,
+    rejectUnauthorized
+}
+
+const session = new Session(sessionConfig);
+
+async function main() {
+    let response: IZosmfInfoResponse;
+    try {
+        response = await CheckStatus.getZosmfInfo(session);
+        // tslint:disable-next-line: no-console
+        console.log(response);
+        exit(0);
+    } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.error(err);
+        exit(1);
+    }
+}
+
+main();
