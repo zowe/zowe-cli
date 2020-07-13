9400833a3325d62ccef807fa600b3fb41a4013f3
Pranay154 <pran25@hotmail.co.uk>
2020-07-13 13:09:58 +0100

    Updated scripts
    Signed-off-by: Pranay154 <pran25@hotmail.co.uk>

--- packages/zosfiles/__tests__/__integration__/cli/hMigrate/ds/__scripts__/migrate_data_set_help_rfj.sh
+++ packages/zosfiles/__tests__/__integration__/cli/hMigrate/ds/__scripts__/migrate_data_set_help_rfj.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "================Z/OS FILES MIGRATE DATA-SET-SEQUENTIAL HELP==============="
-zowe zos-files hMigrate data-set --help --rfj
+zowe zos-files migrate data-set --help --rfj
 if [ $? -gt 0 ]
 then
     exit $?
--- packages/zosfiles/__tests__/__system__/cli/hRecall/ds/__scripts__/command/command_recall_data_set_wait.sh
+++ packages/zosfiles/__tests__/__system__/cli/hRecall/ds/__scripts__/command/command_recall_data_set_wait.sh
@@ -3,7 +3,7 @@ dataSetName=$1
 set -e
 
 echo "================Z/OS FILES RECALL DATA SET==============="
-zowe zos-files hRecall data-set "$dataSetName" --wait
+zowe zos-files recall data-set "$dataSetName" --wait
 if [ $? -gt 0 ]
 then
     exit $?
