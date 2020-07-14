4efba2948558fd20e5844d98f72d9f086d464422
Andrew W. Harn <andrew.harn@broadcom.com>
2020-07-14 09:48:11 -0400

    Add zosconsole examples
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

---
+++ docs/Examples/zosconsole/CollectCommand.example.ts
@@ -0,0 +1,52 @@
+import { CollectCommand, ICollectParms, IConsoleResponse } from "../../../packages/zosconsole";
+import { Session, ISession, SessConstants } from "@zowe/imperative";
+import { exit } from "process";
+
+// Connection Options
+const hostname: string = "yourhost.yourdomain.net";
+const port: number = 443;
+const user: string = "ZOWEUSER";
+const password: string = "ZOWEPASS";
+const protocol: SessConstants.HTTP_PROTOCOL_CHOICES = "https";
+const basePath: string = undefined;
+const type: SessConstants.AUTH_TYPE_CHOICES = "basic";
+const tokenType: string = undefined;
+const tokenValue: string = undefined;
+const rejectUnauthorized: boolean = false;
+
+// Console Options
+const parms: ICollectParms = {
+    commandResponseKey: "KEY",
+    waitToCollect: undefined,
+    followUpAttempts: undefined
+}
+const sessionConfig: ISession = {
+    hostname,
+    port,
+    user,
+    password,
+    protocol,
+    basePath,
+    type,
+    tokenType,
+    tokenValue,
+    rejectUnauthorized
+}
+
+const session = new Session(sessionConfig);
+
+async function main() {
+    let response: IConsoleResponse;
+    try {
+        response = await CollectCommand.collect(session, parms);
+        // tslint:disable-next-line: no-console
+        console.log(response);
+        exit(0);
+    } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.error(err);
+        exit(1);
+    }
+}
+
+main();
