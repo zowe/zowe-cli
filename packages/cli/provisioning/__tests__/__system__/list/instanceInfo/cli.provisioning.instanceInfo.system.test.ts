794bfcdadbd1112b8441e8199652cfd9994c3c30
Andrew <u1858490@unimail.hud.ac.uk>
2020-08-17 16:54:36 +0100

    Fix provisioning cli system test import errors
    Signed-off-by: Andrew <u1858490@unimail.hud.ac.uk>

--- packages/cli/provisioning/__tests__/__system__/list/instanceInfo/cli.provisioning.instanceInfo.system.test.ts
+++ packages/cli/provisioning/__tests__/__system__/list/instanceInfo/cli.provisioning.instanceInfo.system.test.ts
@@ -1,13 +1,13 @@
 /*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Copyright Contributors to the Zowe Project.
- *
- */
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
 
 import { ITestEnvironment } from "../../../../../../../__tests__/__src__/environment/doc/response/ITestEnvironment";
 import { TestEnvironment } from "../../../../../../../__tests__/__src__/environment/TestEnvironment";
@@ -17,7 +17,7 @@ import { Imperative, Session } from "@zowe/imperative";
 import {
     IProvisionedInstance,
     ListRegistryInstances,
-    ProvisioningConstants,
+    ProvisioningConstants
 } from "../../../../../../../packages/provisioning";
 import { ProvisioningTestUtils } from "../../../../../../../packages/provisioning/__tests__/__resources__/utils/ProvisioningTestUtils";
 import { ITestZosmfSchema } from "../../../../../../../__tests__/__src__/properties/ITestZosmfSchema";
@@ -34,7 +34,7 @@ describe("provisioning list instance-info", () => {
     beforeAll(async () => {
         TEST_ENVIRONMENT = await TestEnvironment.setUp({
             testName: "provisioning_list_instance_info",
-            tempProfileTypes: ["zosmf"],
+            tempProfileTypes: ["zosmf"]
         });
         REAL_SESSION = TestEnvironment.createZosmfSession(TEST_ENVIRONMENT);
         templateName =
@@ -78,7 +78,7 @@ describe("provisioning list instance-info", () => {
         // Create a separate test environment for no profiles
         beforeAll(async () => {
             TEST_ENVIRONMENT_NO_PROF = await TestEnvironment.setUp({
-                testName: "provisioning_list_instance_info_no_profile",
+                testName: "provisioning_list_instance_info_no_profile"
             });
             zOSMF = TEST_ENVIRONMENT_NO_PROF.systemTestProperties.zosmf;
         });
@@ -99,7 +99,7 @@ describe("provisioning list instance-info", () => {
                         zOSMF.host,
                         zOSMF.port,
                         zOSMF.user,
-                        zOSMF.pass,
+                        zOSMF.pass
                     ]
                 );
                 expect(response.stderr.toString()).toBe("");
