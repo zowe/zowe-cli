ef3874c04cf21545328c8281413b914c64b89018
Andrew <u1858490@unimail.hud.ac.uk>
2020-08-21 11:44:48 +0100

    Revert "Added tsconfig files"
    This reverts commit 50ff1c53e97aae56dafd5d544088368dd52b0b20.

--- packages/cli/zosuss/src/issue/ssh/Ssh.handler.ts
+++ packages/cli/zosuss/src/issue/ssh/Ssh.handler.ts
@@ -11,6 +11,7 @@
 
 import { IHandlerParameters, IHandlerResponseConsoleApi } from "@zowe/imperative";
 import { Shell } from "../../../../../../packages/zosuss/src/Shell";
+import { ClientChannel } from "ssh2";
 import { SshBaseHandler } from "../../../../../../packages/zosuss/src/SshBaseHandler";
 
 /**
