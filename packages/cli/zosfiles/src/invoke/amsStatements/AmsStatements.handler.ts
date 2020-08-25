cf34c26ffff61267d8a98c25c61b3a40aaf93285
Andrew <u1858490@unimail.hud.ac.uk>
2020-08-25 13:44:34 +0100

    Added sdk path to imports zosfile invoke
    Signed-off-by: Andrew <u1858490@unimail.hud.ac.uk>

--- packages/cli/zosfiles/src/invoke/amsStatements/AmsStatements.handler.ts
+++ packages/cli/zosfiles/src/invoke/amsStatements/AmsStatements.handler.ts
@@ -10,11 +10,11 @@
 */
 
 import { AbstractSession, IHandlerParameters, TextUtils } from "@zowe/imperative";
-import { ZosFilesConstants } from "../../../../../../packages/zosfiles/src/constants/ZosFiles.constants";
-import { IZosFilesResponse } from "../../../../../../packages/zosfiles/src/doc/IZosFilesResponse";
-import { Invoke } from "../../../../../../packages/zosfiles/src/methods/invoke";
+import { ZosFilesConstants } from "@zowe/zos-files-for-zowe-sdk";
+import { IZosFilesResponse } from "@zowe/zos-files-for-zowe-sdk";
+import { Invoke } from "@zowe/zos-files-for-zowe-sdk";
 import { ZosFilesBaseHandler } from "../../ZosFilesBase.handler";
-import { IZosFilesOptions } from "../../../../../../packages/zosfiles/src/doc/IZosFilesOptions";
+import { IZosFilesOptions } from "@zowe/zos-files-for-zowe-sdk";
 
 /**
  * Handler to create a PDS data set
