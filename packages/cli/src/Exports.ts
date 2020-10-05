d850d215cbab603dba1bdf72fe09f13676304ceb
Andrew W. Harn <andrew.harn@broadcom.com>
2020-10-05 13:15:18 +0000

    Add typedoc
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

---
+++ packages/cli/src/Exports.ts
@@ -0,0 +1,19 @@
+import * as core from "@zowe/core-for-zowe-sdk";
+import * as provisioning from "@zowe/provisioning-for-zowe-sdk";
+import * as workflows from "@zowe/zos-workflows-for-zowe-sdk";
+import * as zosconsole from "@zowe/zos-console-for-zowe-sdk";
+import * as zosfiles from "@zowe/zos-files-for-zowe-sdk";
+import * as zosjobs from "@zowe/zos-jobs-for-zowe-sdk";
+import * as zosmf from "@zowe/zosmf-for-zowe-sdk";
+import * as zostso from "@zowe/zos-tso-for-zowe-sdk";
+import * as zosuss from "@zowe/zos-uss-for-zowe-sdk";
+
+export * from "@zowe/core-for-zowe-sdk";
+export * from "@zowe/provisioning-for-zowe-sdk";
+export * from "@zowe/zos-workflows-for-zowe-sdk";
+export * from "@zowe/zos-console-for-zowe-sdk";
+export * from "@zowe/zos-files-for-zowe-sdk";
+export * from "@zowe/zos-jobs-for-zowe-sdk";
+export * from "@zowe/zosmf-for-zowe-sdk";
+export * from "@zowe/zos-tso-for-zowe-sdk";
+export * from "@zowe/zos-uss-for-zowe-sdk";
