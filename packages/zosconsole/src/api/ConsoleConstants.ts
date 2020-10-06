7be520eeba65f23e5a5be75669ff600c4dfe5722
Timothy Johnson <timothy.johnson@broadcom.com>
2020-10-06 09:20:28 -0400

    Fix API error header in message definitions
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    Signed-off-by: nikunj goyal <nikunjgoyal31@gmail.com>

---
+++ packages/zosconsole/src/api/ConsoleConstants.ts
@@ -0,0 +1,62 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { apiErrorHeader, IMessageDefinition } from "@zowe/imperative";
+
+export class ConsoleConstants {
+    public static readonly CLASS: string = "Consoles";
+    public static readonly RESOURCE: string = "/zosmf/restconsoles/consoles";
+    public static readonly RES_DEF_CN: string = "defcn";
+    public static readonly SOL_MSGS: string = "/solmsgs";
+
+    public static readonly DEFAULT_FOLLOWUP_ATTEMPTS: number = 1;
+    public static readonly DEFAULT_TIMEOUT: number = 0;
+}
+
+export const displayResponse: IMessageDefinition = {
+    message: `Populating console response: {{data}}`
+};
+export const displayError: IMessageDefinition = {
+    message: apiErrorHeader.message + ` Populating console error: {{data}}`
+};
+export const displayCollectResponse: IMessageDefinition = {
+    message: `Collect response: {{data}}`
+};
+export const displayCollectError: IMessageDefinition = {
+    message: apiErrorHeader.message + ` Collect error: {{data}}`
+};
+export const decreaseCounter: IMessageDefinition = {
+    message: `Decreasing follow up counter`
+};
+export const resetCounter: IMessageDefinition = {
+    message: `Reset follow up counter`
+};
+export const noSession: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No session was supplied.`
+};
+export const noZosmfInput: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No zosmf console input parameters were supplied.`
+};
+export const noConsoleInput: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No console issue parameters were supplied.`
+};
+export const noConsoleName: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No console name was supplied.`
+};
+export const noCommandKey: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No command response key was supplied.`
+};
+export const noCollectParameters: IMessageDefinition = {
+    message: apiErrorHeader.message + ` No console collect parameters were supplied.`
+};
+export const collectProcessingDetails: IMessageDefinition = {
+    message: `Collect processing - timeout: {{timer}}, followUpCounter: {{counter}}`
+};
