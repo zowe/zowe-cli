e0e2e7b7004786557bf2527d81f408fea536e5f2
Timothy Johnson <timothy.johnson@broadcom.com>
2020-10-28 07:44:56 -0400

    Add missing import in unit test
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    Signed-off-by: nikunj goyal <nikunjgoyal31@gmail.com>

--- packages/zostso/__tests__/__unit__/IssueTso.unit.test.ts
+++ packages/zostso/__tests__/__unit__/IssueTso.unit.test.ts
@@ -10,7 +10,8 @@
 */
 
 import { ImperativeError, Session } from "@zowe/imperative";
-import { IIssueTsoParms, ISendResponse, IssueTso, IStartTsoParms, SendTso, StartTso, StopTso, IZosmfTsoResponse } from "../../src";
+import { IIssueTsoParms, ISendResponse, IssueTso, IStartStopResponse, IStartTsoParms, IZosmfTsoResponse, SendTso,
+         StartTso, StopTso } from "../../src";
 
 const PRETEND_SESSION = new Session({
     user: "user",
