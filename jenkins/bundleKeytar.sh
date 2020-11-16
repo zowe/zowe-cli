319dc4f96b4245b55ea2d6b49ebcc29c9d3ebf9a
Timothy Johnson <timothy.johnson@broadcom.com>
2020-11-16 12:23:46 -0500

    Don't expect dos2unix command to exist
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- jenkins/bundleKeytar.sh
+++ jenkins/bundleKeytar.sh
@@ -9,11 +9,12 @@ githubAuthHeader=$1
 rm -rf prebuilds
 mkdir prebuilds && cd prebuilds
 
+dos2unix="tr -d '\r'"
 jqBin=`which jq || echo "npx jq"`
 keytarVersion=`node -e "console.log(require('../package.json').dependencies.keytar)"`
 
 curl -fs https://$githubAuthHeader@api.github.com/repos/atom/node-keytar/releases/tags/v$keytarVersion |
     $jqBin -c '.assets[] | select (.name | contains("node"))' |
     $jqBin -cr 'select (.browser_download_url) | .browser_download_url' |
-    dos2unix |
+    $dos2unix |
     while read -r bdu; do curl -fsL -o `echo -n $(echo -n $bdu | md5sum | cut -c1-6)'-'$(basename $bdu)` $bdu; done
