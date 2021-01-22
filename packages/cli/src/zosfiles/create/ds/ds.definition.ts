5153c7450022f88cf86ec6c7300198827c3b407a
nikunj goyal <nikunjgoyal31@gmail.com>
2021-01-23 02:24:10 +0530

    another push trying to make lint happy
    Signed-off-by: nikunj goyal <nikunjgoyal31@gmail.com>

--- packages/cli/src/zosfiles/create/ds/ds.definition.ts
+++ packages/cli/src/zosfiles/create/ds/ds.definition.ts
@@ -1,25 +1,21 @@
 /*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Copyright Contributors to the Zowe Project.
- *
- */
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
 
 import { ICommandDefinition } from "@zowe/imperative";
 import { CreateDefaults } from "@zowe/zos-files-for-zowe-sdk";
-import {
-    ZosFilesCreateExtraOptions,
-    ZosFilesCreateOptions,
-} from "../Create.options";
+import { ZosFilesCreateExtraOptions, ZosFilesCreateOptions } from "../Create.options";
 import i18nTypings from "../../-strings-/en";
 
 // Does not use the import in anticipation of some internationalization work to be done later.
-const strings = (require("../../-strings-/en").default as typeof i18nTypings)
-    .CREATE;
+const strings = (require("../../-strings-/en").default as typeof i18nTypings).CREATE;
 
 /**
  * Create dataSet command definition containing its description, examples and/or options
@@ -32,52 +28,38 @@ export const DsDefinition: ICommandDefinition = {
     type: "command",
     handler: __dirname + "/ds.handler",
     profile: {
-        optional: ["zosmf"],
+        optional: ["zosmf"]
     },
     positionals: [
         {
             name: "dataSetName",
             type: "string",
             description: strings.POSITIONALS.DATASETNAME,
-            required: true,
-        },
+            required: true
+        }
     ],
     options: [
         ZosFilesCreateExtraOptions.size,
         ZosFilesCreateOptions.volser,
-        {
-            ...ZosFilesCreateOptions.primary,
-            defaultValue: CreateDefaults.DATA_SET.C.primary,
-        },
+        {...ZosFilesCreateOptions.primary, defaultValue: CreateDefaults.DATA_SET.C.primary},
         ZosFilesCreateOptions.secondary,
-        {
-            ...ZosFilesCreateOptions.dirblk,
-            defaultValue: CreateDefaults.DATA_SET.C.dirblk,
-        },
-        {
-            ...ZosFilesCreateOptions.recfm,
-            defaultValue: CreateDefaults.DATA_SET.C.recfm,
-        },
-        {
-            ...ZosFilesCreateOptions.blksize,
-            defaultValue: CreateDefaults.DATA_SET.C.blksize,
-        },
-        {
-            ...ZosFilesCreateOptions.lrecl,
-            defaultValue: CreateDefaults.DATA_SET.C.lrecl,
-        },
+        {...ZosFilesCreateOptions.dirblk, defaultValue: CreateDefaults.DATA_SET.C.dirblk},
+        {...ZosFilesCreateOptions.recfm, defaultValue: CreateDefaults.DATA_SET.C.recfm},
+        {...ZosFilesCreateOptions.blksize, defaultValue: CreateDefaults.DATA_SET.C.blksize},
+        {...ZosFilesCreateOptions.lrecl, defaultValue: CreateDefaults.DATA_SET.C.lrecl},
         ZosFilesCreateOptions.storclass,
         ZosFilesCreateOptions.mgntclass,
         ZosFilesCreateOptions.dataclass,
         ZosFilesCreateOptions.unit,
         ZosFilesCreateOptions.dsntype,
         ZosFilesCreateExtraOptions.showAttributes,
-        ZosFilesCreateExtraOptions.like,
+        ZosFilesCreateExtraOptions.like
     ].sort((a, b) => a.name.localeCompare(b.name)),
     examples: [
         {
             description: strings.ACTIONS.DATA_SET_LIKE.EXAMPLES.EX1,
-            options: "NEW.DATASET --like EXISTING.DATASET",
-        },
-    ],
+            options: "NEW.DATASET --like EXISTING.DATASET"
+        }
+
+    ]
 };
