f98e48ab6968cc6f70c0ff02a298d92f0c99737e
Timothy Johnson <timothy.johnson@broadcom.com>
2021-04-27 15:31:33 -0400

    Add preinstall script to @zowe/cli package
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ packages/cli/scripts/preinstall.js
@@ -0,0 +1,21 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+const fs = require("fs");
+const path = require("path");
+const cliLibDir = path.join(__dirname, "..", "lib");
+if (!fs.existsSync(cliLibDir)) {
+    fs.mkdirSync(cliLibDir);
+}
+const indexJsFile = path.join(cliLibDir, "index.js");
+if (!fs.existsSync(indexJsFile)) {
+    fs.writeFileSync(indexJsFile, "");
+}
