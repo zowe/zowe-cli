2b5284a7095c7f2ebe0ee242d27b99b9ce1c0d95
Gene Johnston <eugene.johnston@broadcom.com>
2021-06-14 16:15:51 -0400

    Add comments.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/core/src/apiml/doc/IApimlSvcAttrsLoaded.ts
+++ packages/core/src/apiml/doc/IApimlSvcAttrsLoaded.ts
@@ -8,12 +8,23 @@
 * Copyright Contributors to the Zowe Project.
 *
 */
+
 import { IApimlSvcAttrs } from "@zowe/imperative";
 
-/* Once getPluginApimlConfigs() has processed the loaded ImperativeConfig,
- * the connProfType will be a required property in the resulting object.
+/**
+ * Once getPluginApimlConfigs() has processed the loaded ImperativeConfig,
+ * and plugin properties, it returns an array of these structures.
  */
 export interface IApimlSvcAttrsLoaded extends IApimlSvcAttrs {
+    /**
+     * Once getPluginApimlConfigs() has processed the loaded ImperativeConfig,
+     * the connProfType will be a required property in the resulting object.
+     */
     connProfType: string;
+
+    /**
+     * The plugin name from which these attributes were retrieved.
+     * If retrieved from the core CLI, then the CLI name is used.
+     */
     pluginName: string;
 }
