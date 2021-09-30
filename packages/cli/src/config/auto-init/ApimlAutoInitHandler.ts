914f1a5acd770f0bdee0c7d643c437d7c60f96a9
Gene Johnston <eugene.johnston@broadcom.com>
2021-09-30 15:56:01 -0400

    Change services structures into  IAutoInitRpt.
    Display selected profiles and alternate profiles.
    Add placeholders in recordProfileUpdates().
    
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/cli/src/config/auto-init/ApimlAutoInitHandler.ts
+++ packages/cli/src/config/auto-init/ApimlAutoInitHandler.ts
@@ -9,12 +9,15 @@
 *
 */
 
+import * as path from "path";
 import { ZosmfSession } from "@zowe/zosmf-for-zowe-sdk";
-import { BaseAutoInitHandler, AbstractSession, ICommandArguments, ISession, IHandlerResponseApi,
+import { BaseAutoInitHandler, AbstractSession, Config, ICommandArguments, ISession, IHandlerResponseApi,
     IHandlerParameters, SessConstants, IConfig, ImperativeConfig, ImperativeError, RestClientError
 } from "@zowe/imperative";
 import { IApimlProfileInfo, IApimlSvcAttrsLoaded, Login, Services } from "@zowe/core-for-zowe-sdk";
 import { IAutoInitRpt } from "@zowe/core-for-zowe-sdk/lib/apiml/doc/IAutoInitRpt";
+import { IProfileRpt } from "@zowe/core-for-zowe-sdk/lib/apiml/doc/IProfileRpt";
+import { IAltProfile } from "@zowe/core-for-zowe-sdk/lib/apiml/doc/IAltProfile";
 
 /**
  * This class is used by the auth command handlers as the base class for their implementation.
@@ -39,6 +42,8 @@ export default class ApimlAutoInitHandler extends BaseAutoInitHandler {
     private mAutoInitReport: IAutoInitRpt = {
         configFileNm: "",
         typeOfChange: "",
+        startingConfig: null,
+        endingConfig: null,
         profileRpts: []
     };
 
@@ -115,66 +120,135 @@ export default class ApimlAutoInitHandler extends BaseAutoInitHandler {
         // all profile updates have been made. Now we can record those updates.
         this.recordProfileUpdates();
 
-        // display a report of the profile updates
-        // todo: display a real report
-        response.console.log("configFileNm = " + this.mAutoInitReport.configFileNm);
+        // information about the config file
+        response.console.log(this.mAutoInitReport.typeOfChange +
+            " the following Zowe configuration file: " + path.basename(this.mAutoInitReport.configFileNm)
+        );
+
+        // information about each selected profile
+        let editMsg = "change your Zowe configuration"
+        for (let profRpt of this.mAutoInitReport.profileRpts) {
+            response.console.log("\n" + profRpt.typeOfChange +
+                " profile '" + profRpt.selProfNm + "' of type '" + profRpt.selProfType +
+                "' with basePath '" + profRpt.selBasePath + "'"
+            );
+
+            let msg = "    Plugins that use this profile: ";
+            let loopCount = 1;
+            for (let pluginNm of profRpt.pluginNms) {
+                if (loopCount > 1) {
+                    msg += ", ";
+                }
+                msg += pluginNm;
+                loopCount++;
+            }
+            response.console.log(msg);
+
+            // display the alternate profiles
+            if (profRpt.altProfiles.length > 0) {
+                response.console.log("    Alternate profiles of type '" + profRpt.selProfType + "' are:");
+                for (let altProf of profRpt.altProfiles) {
+                    response.console.log("        Profile '" + altProf.altProfName +
+                        "' with basePath '" + altProf.altBasePath + "'"
+                    );
+                }
+                editMsg = "select an alternate profile"
+            }
+        }
+        response.console.log("\nYou can edit this configuration file to " + editMsg + ":\n    " +
+            this.mAutoInitReport.configFileNm);
+
+        // todo: display the rest of the report
     }
 
     /**
      * Record the set of profiles found by our interrogation of plugins and APIML.
      * The information is re-arranged to enable easy reporting of the desired information.
-     * This function assumes that the 'services' module continues to use its "first-found" algorithm.
+     * This function assumes that the 'services' module continues to use its algorithm
+     * in which the first profile of a given type is the profile that we select..
      * If that changes, this function must also change.
      *
-     * @param {IApimlProfileInfo} profileInfos
+     * @param {IApimlProfileInfo} apimlProfInfos
      *        The profileInfo array returned by services.getServicesByConfig().
      */
-    private recordProfilesFound(profileInfos: IApimlProfileInfo[]): void {
-        // todo: before auto-init makes any changes, record the starting config
+    private recordProfilesFound(apimlProfInfos: IApimlProfileInfo[]): void {
+        // record the starting config
+        if (ImperativeConfig.instance.config.exists) {
+            this.mAutoInitReport.typeOfChange = "Updated";
+            this.mAutoInitReport.startingConfig = ImperativeConfig.instance.config;
+
+        } else {
+            this.mAutoInitReport.typeOfChange = "Created";
+            this.mAutoInitReport.startingConfig = null;
+        }
+
+        // Record the first profile found by APIML for each profile type as a selected profile
+        let alreadySelProfTypes: string[] = [];
+        apimlProfsLoop:
+        for (let currProfInx = 0; currProfInx < apimlProfInfos.length; currProfInx++) {
+            // has this profile type already been recorded as selected?
+            for (let alreadySelType of alreadySelProfTypes) {
+                if (apimlProfInfos[currProfInx].profType === alreadySelType) {
+                    continue apimlProfsLoop;
+                }
+            }
+
+            // this is our first encounter with a profile of this type, so we select it
+            let newProfRpt: IProfileRpt = {
+                typeOfChange: "Using",  // placeholder until we determine the right value
+                selProfNm: apimlProfInfos[currProfInx].profName,
+                selProfType: apimlProfInfos[currProfInx].profType,
+                selBasePath: apimlProfInfos[currProfInx].basePaths[0],
+                pluginNms: [],
+                altProfiles: []
+            };
+
+            // add all of the plugins using this profile
+            for (let pluginObj of apimlProfInfos[currProfInx].pluginConfigs) {
+                newProfRpt.pluginNms.push(pluginObj.pluginName);
+            }
+
+            // check for the same profile type among our remaining profiles - they are alternates
+            for (let remainProfInx = currProfInx + 1; remainProfInx < apimlProfInfos.length; remainProfInx++) {
+                if (apimlProfInfos[currProfInx].profType === apimlProfInfos[remainProfInx].profType) {
+                    for (let apimlPluginCfg of apimlProfInfos[remainProfInx].pluginConfigs) {
+                        let newAltProf: IAltProfile = {
+                            altProfName: apimlProfInfos[remainProfInx].profName,
+                            altProfType: apimlPluginCfg.connProfType,
+                            altBasePath: "" // placeholder until we determine the right value
+                        }
+
+                        // each basePath found by apiml for this profile is another alternate
+                        for (let nextBasePath of apimlProfInfos[remainProfInx].basePaths) {
+                            newAltProf.altBasePath = nextBasePath;
+                            newProfRpt.altProfiles.push(newAltProf);
+                        }
+                    }
+                }
+            }
+
+            alreadySelProfTypes.push(newProfRpt.selProfType);
+            this.mAutoInitReport.profileRpts.push(newProfRpt);
+        }
+    }
+
+    /**
+     * Record how the profiles have been updated by auto-init.
+     */
+    private recordProfileUpdates(): void {
+        // get our current (ending) config
+        this.mAutoInitReport.endingConfig = ImperativeConfig.instance.config;
 
         // record the config file name path
         this.mAutoInitReport.configFileNm = ImperativeConfig.instance.config.api.layers.get().path;
 
-        /* zzz
-        private m_autoInitReport: IAutoInitRpt = {
-            configFileNm: "",
-            typeOfChange: "",
-            profileRpts: [{
-                selProfNm: "",
-                typeOfChange: "",
-                selProfType: "",
-                selBasePath: "",
-                pluginNms: [],
+        // todo: compare starting config with the ending config and set values for 'typeOfChange' fields
 
-            }],
-        };
-
-
-        for (const nextProfInfo of profileInfos) {
-            params.response.console.log("_________________________________");
-            params.response.console.log("Created profile '" + nextProfInfo.profName +
-                "'of type '" + nextProfInfo.profType + "'"
-            );
-            for (const nextPluginConfig of nextProfInfo.pluginConfigs) {
-                params.response.console.log("    CLI name = " + nextPluginConfig.pluginName);
-            }
-            for (const nextBasePath of nextProfInfo.basePaths) {
-                params.response.console.log("    Base path = " + nextBasePath);
-            }
-        }
-
-        zzz */
-
-
-        // todo: Record the first profile found for each profile type
-    }
-
-    /**
-     * Record How the profiles have been updated by auto-init.
-     */
-    private recordProfileUpdates(): void {
-        // todo: get our current (ending) config
-
-        // todo: compare starting config with the ending config
+        /* todo: Detect previous direct-to-service profile that has a port and has the same
+         * profile name as retrieved from APIML (like "zosmf"). Now the profile named "zosmf"
+         * will have a basePath for APIML, but the old port number form before..
+         * The old port will break connections that will now attempt to connect to APIML.
+         * This detection might reside in displayAutoInitChanges?
+         */
     }
 }
