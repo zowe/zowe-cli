33d2110dae29b79a670e922a1aff0882fb2a21cd
Gene Johnston <eugene.johnston@broadcom.com>
2021-09-30 15:50:46 -0400

    Add startingConfig and endingConfig.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/core/src/apiml/doc/IAutoInitRpt.ts
+++ packages/core/src/apiml/doc/IAutoInitRpt.ts
@@ -9,13 +9,16 @@
 *
 */
 
+import { Config } from "@zowe/imperative";
 import { IProfileRpt } from "./IProfileRpt";
 
 /**
  * This structure is used to form the auto-init output report.
  */
 export interface IAutoInitRpt {
-    configFileNm: string;       // the effected config file name
-    typeOfChange: string;       // created or modified
-    profileRpts: IProfileRpt[]; // report about each profile we touch
+    configFileNm: string;       // the affected config file name
+    typeOfChange: string;       // Was the config file created or modified
+    startingConfig: Config;     // the configuration that we started with
+    endingConfig: Config;       // the configuration that we ended with
+    profileRpts: IProfileRpt[]; // report about each profile we touched
 }
