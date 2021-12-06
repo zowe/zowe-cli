ebee8b967c40b76ff605d758b1fe0e608e00f3ec
Gene Johnston <eugene.johnston@broadcom.com>
2021-12-06 17:02:40 -0500

    Mock enableDaemon( ) to make this a unit test.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/cli/__tests__/daemon/__unit__/enable/Enable.handler.unit.test.ts
+++ packages/cli/__tests__/daemon/__unit__/enable/Enable.handler.unit.test.ts
@@ -9,48 +9,65 @@
 *
 */
 import EnableDaemonHandler from "../../../../src/daemon/enable/Enable.handler";
+import { IDaemonCmdResult } from "../../../../src/daemon/doc/IDaemonCmdResult";
 
-describe("Enable daemon handler", () => {
+describe("Handler for daemon enable", () => {
     let enableHandler: any; // use "any" so we can call private functions
     let enableDaemonSpy: any;
+
+    // command parms passed to process() by multiple tests
+    let apiMessage = "";
+    let jsonObj;
+    let logMessage = "";
+    const cmdParms = {
+        arguments: {
+            $0: "fake",
+            _: ["fake"]
+        },
+        response: {
+            data: {
+                setMessage: jest.fn((setMsgArgs) => {
+                    apiMessage = setMsgArgs;
+                }),
+                setObj: jest.fn((setObjArgs) => {
+                    jsonObj = setObjArgs;
+                })
+            },
+            console: {
+                log: jest.fn((logArgs) => {
+                    logMessage += "\n" + logArgs;
+                })
+            },
+            progress: {}
+        },
+        profiles: {}
+    } as any;
 
     beforeAll(() => {
         // instantiate our handler and spy on its private enableDaemon() function
         enableHandler = new EnableDaemonHandler();
-        enableDaemonSpy = jest.spyOn(enableHandler, "enableDaemon");
+        enableDaemonSpy = jest.spyOn(EnableDaemonHandler.prototype as any, "enableDaemon");
+    });
+
+    beforeEach(() => {
+        // ensure that toHaveBeenCalledTimes does not accumulate for enableDaemonSpy between tests
+        jest.clearAllMocks();
     });
 
     describe("process method", () => {
-        it("should enable the daemon", async () => {
-            let apiMessage = "";
+        it("should succeed when the enableDaemon function succeeds", async () => {
             let error;
-            let jsonObj;
-            let logMessage = "";
+            const allOkMsg = "Everything worked ok";
+
+            const enableResultOk: IDaemonCmdResult = {
+                success: true,
+                msgText: allOkMsg
+            };
+            enableDaemonSpy.mockImplementation(() => {return enableResultOk});
+
             try {
                 // Invoke the handler with a full set of mocked arguments and response functions
-                await enableHandler.process({
-                    arguments: {
-                        $0: "fake",
-                        _: ["fake"]
-                    },
-                    response: {
-                        data: {
-                            setMessage: jest.fn((setMsgArgs) => {
-                                apiMessage = setMsgArgs;
-                            }),
-                            setObj: jest.fn((setObjArgs) => {
-                                jsonObj = setObjArgs;
-                            })
-                        },
-                        console: {
-                            log: jest.fn((logArgs) => {
-                                logMessage += "\n" + logArgs;
-                            })
-                        },
-                        progress: {}
-                    },
-                    profiles: {}
-                } as any);
+                await enableHandler.process(cmdParms);
             } catch (e) {
                 error = e;
             }
@@ -58,13 +75,30 @@ describe("Enable daemon handler", () => {
             expect(error).toBeUndefined();
             expect(enableDaemonSpy).toHaveBeenCalledTimes(1);
             expect(logMessage).toContain("Daemon mode enabled");
+            expect(logMessage).toContain(allOkMsg);
         });
-    });
 
-    describe("enableDaemon method", () => {
-        it("should return true upon success", async () => {
-            const cmdResult = enableHandler.enableDaemon();
-            expect(cmdResult.success).toBe(true);
+        it("should fail when the enableDaemon function fails", async () => {
+            let error;
+            const badStuffMsg = "Some bad stuff happened";
+
+            const enableResultBad: IDaemonCmdResult = {
+                success: false,
+                msgText: badStuffMsg
+            };
+            enableDaemonSpy.mockImplementation(() => {return enableResultBad});
+
+            try {
+                // Invoke the handler with a full set of mocked arguments and response functions
+                await enableHandler.process(cmdParms);
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error).toBeUndefined();
+            expect(enableDaemonSpy).toHaveBeenCalledTimes(1);
+            expect(logMessage).toContain("Failed to enable daemon mode");
+            expect(logMessage).toContain(badStuffMsg);
         });
     });
 });
