78073155f3862fb3da9f5b6df34e90fe634dc215
Gene Johnston <eugene.johnston@broadcom.com>
2021-12-29 16:55:10 -0500

    Complete the implementation of disable.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/cli/src/daemon/disable/Disable.handler.ts
+++ packages/cli/src/daemon/disable/Disable.handler.ts
@@ -9,7 +9,12 @@
 *
 */
 
-import { ICommandHandler, IHandlerParameters, ImperativeError } from "@zowe/imperative";
+import * as nodeJsPath from "path";
+
+import {
+    ICommandHandler, IHandlerParameters, ImperativeConfig, ImperativeError,
+    IO, ISystemInfo, ProcessUtils
+} from "@zowe/imperative";
 
 /**
  * Handler to disable daemon mode.
@@ -26,30 +31,91 @@ export default class DisableDaemonHandler implements ICommandHandler {
      *
      * @throws {ImperativeError}
      */
-    public process(cmdParams: IHandlerParameters): Promise<void> {
-        let userMsg: string;
+    public async process(cmdParams: IHandlerParameters): Promise<void> {
         try {
-            userMsg = this.disableDaemon();
+            await this.disableDaemon();
         } catch(impErr) {
             cmdParams.response.console.log("Failed to disable Zowe CLI daemon mode.\n" + (impErr as ImperativeError).message);
             cmdParams.response.data.setExitCode(1);
             return;
         }
 
-        cmdParams.response.console.log("Zowe CLI daemon mode disabled.\n" + userMsg);
+        cmdParams.response.console.log("Zowe CLI daemon mode is disabled.");
         cmdParams.response.data.setExitCode(0);
         return;
     }
 
     /**
-     * Enable daemon mode.
+     * Disable daemon mode.
      *
      * @throws {ImperativeError}
      *
      * @returns {string} An informational message to display to the user after
      *          successful completion of the operation.
           */
-    private disableDaemon(): string {
-        return "Pretend that we disabled daemon-mode.";
+    private async disableDaemon(): Promise<void> {
+        // form the path to the bin directory in ZOWE_CLI_HOME
+        let zoweExePath = nodeJsPath.normalize(ImperativeConfig.instance.cliHome + "/bin/");
+
+        // determine our current OS
+        const sysInfo: ISystemInfo = ProcessUtils.getBasicSystemInfo();
+
+        // add the right EXE file name for the current OS
+        switch (sysInfo.platform) {
+            case "darwin" || "linux": {
+                zoweExePath += "zowe";
+                break;
+            }
+            case "win32": {
+                zoweExePath += "zowe.exe";
+                break;
+            }
+            default: {
+                throw new ImperativeError({
+                    msg: `Daemon mode is not supported on the '${sysInfo.platform}' operating system.`
+                });
+            }
+        }
+
+        // remove the EXE if it exists
+        if (IO.existsSync(zoweExePath)) {
+            IO.deleteFile(zoweExePath);
+        }
+
+        // find processes running "node"
+        let procArray;
+        try {
+            const findProc = require('find-process');
+            procArray = await findProc('name', 'node', true);
+        } catch(err) {
+            /* This catch is not invoked, because the find-process.finders[].win32.utils.spawn function
+             * does not add an on("error") event handler for the child_process.spawn() function it calls.
+             * Thus, ENOENT is emitted as an uncatchable event. Thanks find-process!
+             * If a user gets an error like this, just tell him that powershell is not on his PATH.
+             * Typically it should be. The same type of error might occur on Linux if 'ps' were not
+             * on the user's PATH.
+             */
+            let powerShellHint = "";
+            if (err.includes("powershell.exe ENOENT")) {
+                powerShellHint = "\nPowershell.exe may not be on your PATH.";
+            }
+            throw new ImperativeError({
+                msg: "Failed while searching for the Zowe-CLI daemon process.\n"
+                     + `Reason: ${err}`
+                     + powerShellHint
+            });
+        }
+
+        /* Paths in proc list on Windows sometimes have forward slash
+         * and sometimes backslash, so allow either.
+         */
+        const zoweCmdRegEx = "@zowe[/|\\\\]cli[/|\\\\]lib[/|\\\\]main.js";
+
+        // match and kill any running Zowe daemon
+        for (const nextProc of procArray) {
+            if (nextProc.cmd.match(zoweCmdRegEx) && nextProc.cmd.includes("--daemon")) {
+                process.kill(nextProc.pid, "SIGINT");
+            }
+        }
     }
 }
