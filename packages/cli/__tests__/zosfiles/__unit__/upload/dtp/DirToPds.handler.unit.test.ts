9bb638c8cef5ef18012fea80dd3777ba758cd775
Andrew W. Harn <andrew.harn@broadcom.com>
2022-03-22 15:48:22 -0400

    Update CLI unit tests
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/cli/__tests__/zosfiles/__unit__/upload/dtp/DirToPds.handler.unit.test.ts
+++ packages/cli/__tests__/zosfiles/__unit__/upload/dtp/DirToPds.handler.unit.test.ts
@@ -109,5 +109,203 @@ describe("Upload dir-to-pds handler", () => {
             expect(apiMessage).toMatchSnapshot();
             expect(logMessage).toMatchSnapshot();
         });
+
+        it("should upload a directory to a PDS in binary format if requested", async () => {
+            // Require the handler and create a new instance
+            const handlerReq = require("../../../../../src/zosfiles/upload/dtp/DirToPds.handler");
+            const handler = new handlerReq.default();
+            const inputdir = "test-dir";
+            const dataSetName = "testing";
+            const binary = true;
+
+            // Vars populated by the mocked function
+            let error;
+            let apiMessage = "";
+            let jsonObj;
+            let logMessage = "";
+            let fakeSession = null;
+
+            // Mock the submit JCL function
+            Upload.dirToPds = jest.fn((session) => {
+                fakeSession = session;
+                return {
+                    success: false,
+                    commandResponse: "uploaded",
+                    apiResponse: [
+                        {success: true, from: inputdir, to: dataSetName},
+                        {success: false, from: "testfrom", to: "testto"},
+                        {success: undefined, from: "dummy", to: "nowhere"}
+                    ]
+                };
+            });
+
+            // Mocked function references
+            const profFunc = jest.fn((args) => {
+                return {
+                    host: "fake",
+                    port: "fake",
+                    user: "fake",
+                    password: "fake",
+                    auth: "fake",
+                    rejectUnauthorized: "fake"
+                };
+            });
+
+            try {
+                // Invoke the handler with a full set of mocked arguments and response functions
+                await handler.process({
+                    arguments: {
+                        $0: "fake",
+                        _: ["fake"],
+                        inputdir,
+                        dataSetName,
+                        binary,
+                        ...UNIT_TEST_ZOSMF_PROF_OPTS
+                    },
+                    response: {
+                        data: {
+                            setMessage: jest.fn((setMsgArgs) => {
+                                apiMessage = setMsgArgs;
+                            }),
+                            setObj: jest.fn((setObjArgs) => {
+                                jsonObj = setObjArgs;
+                            })
+                        },
+                        console: {
+                            log: jest.fn((logArgs) => {
+                                logMessage += "\n" + logArgs;
+                            })
+                        },
+                        progress: {
+                            startBar: jest.fn((parms) => {
+                                // do nothing
+                            }),
+                            endBar: jest.fn(() => {
+                                // do nothing
+                            })
+                        }
+                    },
+                    profiles: {
+                        get: profFunc
+                    }
+                } as any);
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error).toBeDefined();
+            expect(profFunc).toHaveBeenCalledWith("zosmf", false);
+            expect(Upload.dirToPds).toHaveBeenCalledTimes(1);
+            expect(Upload.dirToPds).toHaveBeenCalledWith(fakeSession, inputdir, dataSetName, {
+                binary,
+                task: {
+                    percentComplete: 0,
+                    stageName: 0,
+                    statusMessage: "Uploading directory to PDS"
+                }
+            });
+            expect(jsonObj).toMatchSnapshot();
+            expect(apiMessage).toMatchSnapshot();
+            expect(logMessage).toMatchSnapshot();
+        });
+
+        it("should upload a directory to a PDS in record format if requested", async () => {
+            // Require the handler and create a new instance
+            const handlerReq = require("../../../../../src/zosfiles/upload/dtp/DirToPds.handler");
+            const handler = new handlerReq.default();
+            const inputdir = "test-dir";
+            const dataSetName = "testing";
+            const record = true;
+
+            // Vars populated by the mocked function
+            let error;
+            let apiMessage = "";
+            let jsonObj;
+            let logMessage = "";
+            let fakeSession = null;
+
+            // Mock the submit JCL function
+            Upload.dirToPds = jest.fn((session) => {
+                fakeSession = session;
+                return {
+                    success: false,
+                    commandResponse: "uploaded",
+                    apiResponse: [
+                        {success: true, from: inputdir, to: dataSetName},
+                        {success: false, from: "testfrom", to: "testto"},
+                        {success: undefined, from: "dummy", to: "nowhere"}
+                    ]
+                };
+            });
+
+            // Mocked function references
+            const profFunc = jest.fn((args) => {
+                return {
+                    host: "fake",
+                    port: "fake",
+                    user: "fake",
+                    password: "fake",
+                    auth: "fake",
+                    rejectUnauthorized: "fake"
+                };
+            });
+
+            try {
+                // Invoke the handler with a full set of mocked arguments and response functions
+                await handler.process({
+                    arguments: {
+                        $0: "fake",
+                        _: ["fake"],
+                        inputdir,
+                        dataSetName,
+                        record,
+                        ...UNIT_TEST_ZOSMF_PROF_OPTS
+                    },
+                    response: {
+                        data: {
+                            setMessage: jest.fn((setMsgArgs) => {
+                                apiMessage = setMsgArgs;
+                            }),
+                            setObj: jest.fn((setObjArgs) => {
+                                jsonObj = setObjArgs;
+                            })
+                        },
+                        console: {
+                            log: jest.fn((logArgs) => {
+                                logMessage += "\n" + logArgs;
+                            })
+                        },
+                        progress: {
+                            startBar: jest.fn((parms) => {
+                                // do nothing
+                            }),
+                            endBar: jest.fn(() => {
+                                // do nothing
+                            })
+                        }
+                    },
+                    profiles: {
+                        get: profFunc
+                    }
+                } as any);
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error).toBeDefined();
+            expect(profFunc).toHaveBeenCalledWith("zosmf", false);
+            expect(Upload.dirToPds).toHaveBeenCalledTimes(1);
+            expect(Upload.dirToPds).toHaveBeenCalledWith(fakeSession, inputdir, dataSetName, {
+                record,
+                task: {
+                    percentComplete: 0,
+                    stageName: 0,
+                    statusMessage: "Uploading directory to PDS"
+                }
+            });
+            expect(jsonObj).toMatchSnapshot();
+            expect(apiMessage).toMatchSnapshot();
+            expect(logMessage).toMatchSnapshot();
+        });
     });
 });
--- packages/cli/__tests__/zosfiles/__unit__/upload/stds/StdinToDataSet.handler.unit.test.ts
+++ packages/cli/__tests__/zosfiles/__unit__/upload/stds/StdinToDataSet.handler.unit.test.ts
@@ -99,5 +99,197 @@ describe("Upload stdin-to-data-set handler", () => {
             expect(profFunc).toHaveBeenCalledWith("zosmf", false);
             expect(Upload.streamToDataSet).toHaveBeenCalledTimes(1);
         });
+
+        it("should upload a file to a data set in binary format if requested", async () => {
+            // Require the handler and create a new instance
+            const handlerReq = require("../../../../../src/zosfiles/upload/stds/StdinToDataSet.handler");
+            const handler = new handlerReq.default();
+            const inputBuffer = Buffer.from("test-data");
+            const dataSetName = "testing";
+            const binary = true;
+
+            // Vars populated by the mocked function
+            let error;
+            let apiMessage = "";
+            let jsonObj;
+            let logMessage = "";
+            let fakeSession = null;
+
+            // Mock the submit JCL function
+            Upload.streamToDataSet = jest.fn((session) => {
+                fakeSession = session;
+                return {
+                    success: true,
+                    commandResponse: "uploaded"
+                };
+            });
+
+            // Mocked function references
+            const profFunc = jest.fn((args) => {
+                return {
+                    host: "fake",
+                    port: "fake",
+                    user: "fake",
+                    password: "fake",
+                    auth: "fake",
+                    rejectUnauthorized: "fake"
+                };
+            });
+
+            process.nextTick(() => {
+                process.stdin.emit("data", Buffer.from("test-data"));
+                process.stdin.emit("end");
+            });
+
+            try {
+                // Invoke the handler with a full set of mocked arguments and response functions
+                await handler.process({
+                    arguments: {
+                        $0: "fake",
+                        _: ["fake"],
+                        inputBuffer,
+                        dataSetName,
+                        binary,
+                        ...UNIT_TEST_ZOSMF_PROF_OPTS
+                    },
+                    response: {
+                        data: {
+                            setMessage: jest.fn((setMsgArgs) => {
+                                apiMessage = setMsgArgs;
+                            }),
+                            setObj: jest.fn((setObjArgs) => {
+                                jsonObj = setObjArgs;
+                            })
+                        },
+                        console: {
+                            log: jest.fn((logArgs) => {
+                                logMessage += "\n" + logArgs;
+                            })
+                        },
+                        progress: {
+                            startBar: jest.fn((parms) => {
+                                // do nothing
+                            }),
+                            endBar: jest.fn(() => {
+                                // do nothing
+                            })
+                        }
+                    },
+                    profiles: {
+                        get: profFunc
+                    }
+                } as any);
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error).toBeUndefined();
+            expect(Upload.streamToDataSet).toHaveBeenCalledWith(fakeSession, undefined, dataSetName, {
+                binary,
+                task: {
+                    percentComplete: 0,
+                    stageName: 0,
+                    statusMessage: "Uploading stdin to data set"
+                }
+            });
+            expect(profFunc).toHaveBeenCalledWith("zosmf", false);
+            expect(Upload.streamToDataSet).toHaveBeenCalledTimes(1);
+        });
+
+        it("should upload a file to a data set in record format if requested", async () => {
+            // Require the handler and create a new instance
+            const handlerReq = require("../../../../../src/zosfiles/upload/stds/StdinToDataSet.handler");
+            const handler = new handlerReq.default();
+            const inputBuffer = Buffer.from("test-data");
+            const dataSetName = "testing";
+            const record = true;
+
+            // Vars populated by the mocked function
+            let error;
+            let apiMessage = "";
+            let jsonObj;
+            let logMessage = "";
+            let fakeSession = null;
+
+            // Mock the submit JCL function
+            Upload.streamToDataSet = jest.fn((session) => {
+                fakeSession = session;
+                return {
+                    success: true,
+                    commandResponse: "uploaded"
+                };
+            });
+
+            // Mocked function references
+            const profFunc = jest.fn((args) => {
+                return {
+                    host: "fake",
+                    port: "fake",
+                    user: "fake",
+                    password: "fake",
+                    auth: "fake",
+                    rejectUnauthorized: "fake"
+                };
+            });
+
+            process.nextTick(() => {
+                process.stdin.emit("data", Buffer.from("test-data"));
+                process.stdin.emit("end");
+            });
+
+            try {
+                // Invoke the handler with a full set of mocked arguments and response functions
+                await handler.process({
+                    arguments: {
+                        $0: "fake",
+                        _: ["fake"],
+                        inputBuffer,
+                        dataSetName,
+                        record,
+                        ...UNIT_TEST_ZOSMF_PROF_OPTS
+                    },
+                    response: {
+                        data: {
+                            setMessage: jest.fn((setMsgArgs) => {
+                                apiMessage = setMsgArgs;
+                            }),
+                            setObj: jest.fn((setObjArgs) => {
+                                jsonObj = setObjArgs;
+                            })
+                        },
+                        console: {
+                            log: jest.fn((logArgs) => {
+                                logMessage += "\n" + logArgs;
+                            })
+                        },
+                        progress: {
+                            startBar: jest.fn((parms) => {
+                                // do nothing
+                            }),
+                            endBar: jest.fn(() => {
+                                // do nothing
+                            })
+                        }
+                    },
+                    profiles: {
+                        get: profFunc
+                    }
+                } as any);
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error).toBeUndefined();
+            expect(Upload.streamToDataSet).toHaveBeenCalledWith(fakeSession, undefined, dataSetName, {
+                record,
+                task: {
+                    percentComplete: 0,
+                    stageName: 0,
+                    statusMessage: "Uploading stdin to data set"
+                }
+            });
+            expect(profFunc).toHaveBeenCalledWith("zosmf", false);
+            expect(Upload.streamToDataSet).toHaveBeenCalledTimes(1);
+        });
     });
 });
