9a981a5b6f92519dfab45e7baedd1d950ca35b9c
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-04-28 13:54:06 +0000

    deprecate individual constants instead of removing them
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- packages/zostso/src/constants/ZosTso.constants.ts
+++ packages/zostso/src/constants/ZosTso.constants.ts
@@ -91,5 +91,54 @@ export class TsoProfileConstants {
         TsoProfileConstants.TSO_OPTION_REGION_SIZE,
         TsoProfileConstants.TSO_OPTION_ROWS
     ];
-
 }
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_GROUP = TsoProfileConstants.TSO_OPTION_GROUP;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_ACCOUNT = TsoProfileConstants.TSO_OPTION_ACCOUNT;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_ACCOUNT_PROFILE = TsoProfileConstants.TSO_OPTION_ACCOUNT_PROFILE;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_CHAR_SET = TsoProfileConstants.TSO_OPTION_CHAR_SET;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_CODE_PAGE = TsoProfileConstants.TSO_OPTION_CODE_PAGE;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_COLUMNS = TsoProfileConstants.TSO_OPTION_COLUMNS;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_LOGON_PROCEDURE = TsoProfileConstants.TSO_OPTION_LOGON_PROCEDURE;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_REGION_SIZE = TsoProfileConstants.TSO_OPTION_REGION_SIZE;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_OPTION_ROWS = TsoProfileConstants.TSO_OPTION_ROWS;
+
+/**
+ * @deprecated Please use this constant from the TsoProfileConstants class
+ */
+export const TSO_PROFILE_OPTIONS = TsoProfileConstants.TSO_PROFILE_OPTIONS;
