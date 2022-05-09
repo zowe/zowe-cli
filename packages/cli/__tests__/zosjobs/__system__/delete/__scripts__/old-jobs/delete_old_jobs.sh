a620afba65de384e09d8a910bcda6af119d06740
Timothy Johnson <timothy.johnson@broadcom.com>
2022-05-09 18:13:41 -0400

    Add CLI system tests
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ packages/cli/__tests__/zosjobs/__system__/delete/__scripts__/old-jobs/delete_old_jobs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Max attempt count
+ATTEMPTS=10
+
+# Wait time between attempts
+WAIT=10
+
+# Submit a job and ensure the RC is 0
+JOBID=`zowe jobs submit ds "$1" --rff jobid --rft string`
+RC=$?
+if [ $RC -gt 0 ]
+then
+    echo $JOBID 1>&2
+    echo "Submit returned a non-zero return code" 1>&2
+    exit $RC
+fi
+
+# Loop until the job goes to the output queue
+until [ $ATTEMPTS -gt 0 ]
+do
+    STATUS=`zowe jobs view job-status-by-jobid $JOBID --rff status --rft string`
+    RC=$?
+    if [ $RC -gt 0 ] ; then
+        echo $STATUS 1>&2
+        echo "The view job status command returned a non-zero rc: $RC" 1>&2
+        exit $RC
+    fi
+    if [ "$STATUS" = "OUTPUT" ] ; then
+        break
+    else
+        ((ATTEMPTS--))
+        sleep $WAIT
+    fi
+done
+
+# Check status
+if [ $ATTEMPTS -eq 0 -a "$STATUS" != "OUTPUT" ]; then
+    echo "Wait time limit reached" 1>&2
+    exit 1
+fi
+
+# Purge the jobs
+zowe jobs delete old-jobs
+RC=$?
+
+if [ $RC -gt 0 ]
+then
+    echo "The delete old-jobs command returned a non-zero rc: $RC" 1>&2
+    exit $RC
+fi
---
+++ packages/cli/__tests__/zosjobs/__system__/delete/cli.zos-jobs.delete.old-jobs.system.test.ts
@@ -0,0 +1,48 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ITestEnvironment, runCliScript } from "@zowe/cli-test-utils";
+import { TestEnvironment } from "../../../../../../__tests__/__src__/environment/TestEnvironment";
+import { ITestPropertiesSchema } from "../../../../../../__tests__/__src__/properties/ITestPropertiesSchema";
+
+// Test Environment populated in the beforeAll();
+let TEST_ENVIRONMENT: ITestEnvironment<ITestPropertiesSchema>;
+let IEFBR14_JCL: string;
+
+describe("zos-jobs delete old-jobs command", () => {
+    // Create the unique test environment
+    beforeAll(async () => {
+        TEST_ENVIRONMENT = await TestEnvironment.setUp({
+            testName: "zos_jobs_delete_old_jobs_command",
+            tempProfileTypes: ["zosmf"]
+        });
+        IEFBR14_JCL = TEST_ENVIRONMENT.systemTestProperties.zosjobs.iefbr14Member;
+    });
+
+    describe("error handling", () => {
+        it("should surface an error from z/OSMF if the jobid doesn't exist", () => {
+            const response = runCliScript(__dirname + "/__scripts__/old-jobs/not_found.sh", TEST_ENVIRONMENT);
+            expect(response.status).toBe(0);
+            expect(response.stderr.toString()).toBe("");
+            expect(response.stdout.toString()).toContain("No jobs found with prefix");
+        });
+    });
+
+    describe("successful scenario", () => {
+        it("should delete all old jobs", () => {
+            const response = runCliScript(__dirname + "/__scripts__/old-jobs/delete_old_jobs.sh",
+                TEST_ENVIRONMENT, [IEFBR14_JCL]);
+            expect(response.status).toBe(0);
+            expect(response.stderr.toString()).toBe("");
+            expect(response.stdout.toString()).toContain("Successfully deleted");
+        });
+    });
+});
