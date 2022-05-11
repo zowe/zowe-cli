48507018a81edcd94ffab1125a0508ae3145c62e
Andrew W. Harn <andrew.harn@broadcom.com>
2022-05-11 11:26:35 -0400

    Try disabling daemon for this test
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/cli/__tests__/zosjobs/__integration__/submit/uss-file/__scripts__/submit_syntax_invalid_parm.sh
+++ packages/cli/__tests__/zosjobs/__integration__/submit/uss-file/__scripts__/submit_syntax_invalid_parm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-MSYS_NO_PATHCONV=1; zowe zos-jobs submit uss-file "/a/ibmuser/ussfile.txt" blah
+MSYS_NO_PATHCONV=1; ZOWE_USE_DAEMON=0; zowe zos-jobs submit uss-file "/a/ibmuser/ussfile.txt" blah
 exit $?
