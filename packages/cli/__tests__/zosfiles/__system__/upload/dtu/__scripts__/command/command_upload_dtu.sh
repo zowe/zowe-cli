e8b9ffac78dd8f78fbef638e276bd9d327db6d21
Timothy Johnson <timothy.johnson@broadcom.com>
2022-08-30 12:56:45 -0400

    Fix some failing system tests
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cli/__tests__/zosfiles/__system__/upload/dtu/__scripts__/command/command_upload_dtu.sh
+++ packages/cli/__tests__/zosfiles/__system__/upload/dtu/__scripts__/command/command_upload_dtu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "================ Z/OS FILES UPLOAD LOCAL DIRECTORY TO USS DIRECTORY==============="
-zowe zos-files upload dir-to-uss $*
+zowe zos-files upload dir-to-uss "$1" "$2" ${@:3}
 if [ $? -gt 0 ]
 then
     exit $?
