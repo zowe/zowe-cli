9c078d9fbb34302b1790264038245be22f0da06f
Timothy Johnson <timothy.johnson@broadcom.com>
2022-08-30 13:43:21 -0400

    Handle spaces in paths for system tests
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cli/__tests__/zosfiles/__system__/upload/dtp/__scripts__/command/command_upload_dtp.sh
+++ packages/cli/__tests__/zosfiles/__system__/upload/dtp/__scripts__/command/command_upload_dtp.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "================ Z/OS FILES UPLOAD LOCAL DIRECTORY ==============="
-zowe zos-files upload dir-to-pds $*
+zowe zos-files upload dir-to-pds "$1" "$2" ${@:3}
 if [ $? -gt 0 ]
 then
     exit $?
--- packages/cli/__tests__/zosjobs/__system__/submit/local-file/__scripts__/submit_valid_local_file_fully_qualified.sh
+++ packages/cli/__tests__/zosjobs/__system__/submit/local-file/__scripts__/submit_valid_local_file_fully_qualified.sh
@@ -7,5 +7,5 @@ USER=$4
 PASS=$5
 
 # pass the data set name as an argument to the script
-zowe zos-jobs submit local-file $JCL --host $HOST --port $PORT --user $USER --password $PASS --ru=false
+zowe zos-jobs submit local-file "$JCL" --host $HOST --port $PORT --user $USER --password $PASS --ru=false
 exit $?
