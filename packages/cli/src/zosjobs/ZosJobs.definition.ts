c5754edd0d2f461f7c1fccdc00c45419dbd38ad3
Amber Torrise <amber.torrise@broadcom.com>
2022-09-30 14:48:43 -0400

    initial commit containing all files that need to be modified. many changes to come and just recording files to modify
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- packages/cli/src/zosjobs/ZosJobs.definition.ts
+++ packages/cli/src/zosjobs/ZosJobs.definition.ts
@@ -17,6 +17,7 @@ import { CancelDefinition } from "./cancel/Cancel.definition";
 import { DeleteDefinition } from "./delete/Delete.definition";
 import { DownloadDefinition } from "./download/Download.definition";
 import { ZosmfSession } from "@zowe/zosmf-for-zowe-sdk";
+import { ChangeDefinition } from "./change/Change.definition";
 
 export const definition: ICommandDefinition = {
     name: "zos-jobs",
@@ -30,6 +31,7 @@ export const definition: ICommandDefinition = {
         ViewDefinition,
         ListDefinition,
         DeleteDefinition,
+        ChangeDefinition,
         CancelDefinition
     ],
     passOn: [
