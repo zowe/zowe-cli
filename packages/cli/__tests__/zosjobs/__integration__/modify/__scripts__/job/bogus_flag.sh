06066ff198a4524464c686f9010e173dbf146c6e
Timothy Johnson <timothy.johnson@broadcom.com>
2022-11-18 09:44:12 -0500

    Fix failing integration tests
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cli/__tests__/zosjobs/__integration__/modify/__scripts__/job/bogus_flag.sh
+++ packages/cli/__tests__/zosjobs/__integration__/modify/__scripts__/job/bogus_flag.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-# Max attempt count
-ATTEMPTS=10
-
-# Wait time between attempts
-WAIT=10
-
-# Submit the job and ensure the RC is 0
-JOBID=`zowe jobs submit lf "$1" --rff jobid --rft string`
-RC=$?
-if [ $RC -gt 0 ]
-then
-    echo $JOBID 1>&2
-    echo "Submit returned a non-zero return code" 1>&2
-    exit $RC
-fi
-
-# Use modify command with a real jobid
 zowe zos-jobs modify job $JOBID --bogus-flag
-RC=$?
-
 exit $?
