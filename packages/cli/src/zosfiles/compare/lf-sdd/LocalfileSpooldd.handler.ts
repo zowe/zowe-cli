6666e9c527425150906f079baefc5c3b4a252d70
Amber Torrise <amber.torrise@broadcom.com>
2023-01-31 12:05:13 -0500

    fix in variable declaration
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- packages/cli/src/zosfiles/compare/lf-sdd/LocalfileSpooldd.handler.ts
+++ packages/cli/src/zosfiles/compare/lf-sdd/LocalfileSpooldd.handler.ts
@@ -64,7 +64,7 @@ export default class LocalfileSpoolddHandler extends ZosFilesBaseHandler {
             fs.closeSync(localFileHandle);
         }
 
-        const lfContentBuf = fs.readFileSync(localFile);
+        lfContentBuf = fs.readFileSync(localFile);
         let lfContentString: string = "";
         const seqnumlen = 8;
 
