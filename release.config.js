86a36a5104d0221a6c8788b8acc1629e754d8567
Timothy Johnson <timothy.johnson@broadcom.com>
2023-02-03 10:42:25 -0500

    Try again to publish 7.9.8
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- release.config.js
+++ release.config.js
@@ -31,7 +31,7 @@ module.exports = {
             smokeTest: true
         }],
         ["@octorelease/github", {
-            checkPrLabels: true
+            // checkPrLabels: true
         }],
         "@octorelease/git"
     ]
