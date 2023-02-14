0df26163e0365571a537c9e09979ad6e62d1bd0e
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-02-14 13:41:52 +0000

    update tests to reflect the new statusMessage on thee progress bar
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- packages/cli/__tests__/zosfiles/__unit__/compare/ds/Dataset.handler.unit.test.ts
+++ packages/cli/__tests__/zosfiles/__unit__/compare/ds/Dataset.handler.unit.test.ts
@@ -82,7 +82,7 @@ describe("Compare data set handler", () => {
         const dsTask = {
             percentComplete: 0,
             stageName: 0,
-            statusMessage: "Retrieving second dataset"
+            statusMessage: "Retrieving content for the second file/dataset"
         };
 
         beforeEach(() => {
