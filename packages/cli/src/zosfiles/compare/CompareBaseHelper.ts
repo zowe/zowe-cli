c392d5a1c30dc2298a4b92516a995fc00da67aac
Amber Torrise <amber.torrise@broadcom.com>
2023-04-06 14:45:03 -0400

    possibly fixing a build error with IDiffOptions
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- packages/cli/src/zosfiles/compare/CompareBaseHelper.ts
+++ packages/cli/src/zosfiles/compare/CompareBaseHelper.ts
@@ -190,12 +190,20 @@ export class CompareBaseHelper {
             };
         }
 
-        const jsonDiff = await DiffUtils.getDiffString(string1, string2, {
-            outputFormat: 'terminal',
-            contextLinesArg: this.contextLines,
-            name1: options.name1,
-            name2: options.name2
-        });
+        let jsonDiff;
+        if (options.name1 && options.name2){
+            jsonDiff = await DiffUtils.getDiffString(string1, string2, {
+                outputFormat: 'terminal',
+                contextLinesArg: this.contextLines,
+                name1: options.name1,
+                name2: options.name2
+            });
+        }else{
+            jsonDiff = await DiffUtils.getDiffString(string1, string2, {
+                outputFormat: 'terminal',
+                contextLinesArg: this.contextLines
+            });
+        }
 
         return {
             success: true,
