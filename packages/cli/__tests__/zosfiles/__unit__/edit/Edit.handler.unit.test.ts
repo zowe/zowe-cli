2d002141a8e00320beb82c95cd90a625897e82a0
Amber Torrise <amber.torrise@broadcom.com>
2023-04-26 16:26:57 -0400

    mocking out the handler
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- packages/cli/__tests__/zosfiles/__unit__/edit/Edit.handler.unit.test.ts
+++ packages/cli/__tests__/zosfiles/__unit__/edit/Edit.handler.unit.test.ts
@@ -9,85 +9,96 @@
 *
 */
 
-import { Get, Upload } from "@zowe/zos-files-for-zowe-sdk";
-import { GuiResult } from "@zowe/imperative";
+import { GuiResult, IHandlerParameters, ImperativeError, ProcessUtils, RestConstants, TextUtils } from "@zowe/imperative";
 import {ILocalFile,
 EditUtilities} from "../../../../src/zosfiles/edit/Edit.utils"
+import { mockHandlerParameters } from "@zowe/cli-test-utils";
+import { EditDefinition } from "../../../../src/zosfiles/edit/Edit.definition";
+import EditHandler from "../../../../src/zosfiles/edit/Edit.handler";
+import { UNIT_TEST_PROFILES_ZOSMF, UNIT_TEST_ZOSMF_PROF_OPTS } from "../../../../../../__tests__/__src__/mocks/ZosmfProfileMock";
+import * as path from 'path';
 
 describe("Files Edit Group Handler", () => {
     describe("process method", () => {
-        // Require the handler and create a new instance
-        const handlerDsReq = require("../../../../../src/zosfiles/edit/ds/Dataset.handler");
-        const handlerUssReq = require("../../../../../src/zosfiles/edit/ds/Dataset.handler");
-        const handlerDs = new handlerDsReq.default();
-        const handlerUss = new handlerUssReq.default();
-        const dataSetName = "testingDataset";
-        const ussFileName = "testingUssFile";
+        //Variable instantiation
+        const dataSetName = "dataset";
+        const dataSetPath = path.join(process.cwd(), "packages\\cli\\src\\zosfiles\\edit\\Edit.handler.ts")
 
-        let dslf: ILocalFile = {
-            tempPath: null,
+        let commandParameters: IHandlerParameters = mockHandlerParameters({
+            arguments: UNIT_TEST_ZOSMF_PROF_OPTS,
+            positionals: ["zos-files", "edit", "ds"],
+            definition: EditDefinition,
+            profiles: UNIT_TEST_PROFILES_ZOSMF
+        });
+
+        let localFile: ILocalFile = {
+            tempPath: dataSetPath,
             fileName: dataSetName,
             fileType: "ds",
             guiAvail: true,
-            zosResp: null
+            zosResp: {
+                apiResponse:{etag: 'remote etag'},
+                success: true,
+                commandResponse: ``
+            }
         };
 
-        let usslf: ILocalFile = {
-            tempPath: null,
-            fileName: ussFileName,
-            fileType: "uss",
-            guiAvail: true,
-            zosResp: null
-        };
+        //Mocks Setup
+        const buildTempPathSpy = jest.spyOn(EditUtilities, "buildTempPath");
+        const checkForStashSpy = jest.spyOn(EditUtilities, "checkForStash");
+        const promptUserSpy = jest.spyOn(EditUtilities, "promptUser");
+        const localDownloadSpy = jest.spyOn(EditUtilities, "localDownload");
+        const uploadEditsSpy = jest.spyOn(EditUtilities, "uploadEdits");
+        const guiAvailSpy = jest.spyOn(ProcessUtils, "isGuiAvailable");
+        jest.spyOn(EditUtilities, "fileComparison").mockImplementation(jest.fn());
+        jest.spyOn(EditUtilities, "makeEdits").mockImplementation(jest.fn());        guiAvailSpy.mockImplementation(jest.fn(() => {
+            return GuiResult.GUI_AVAILABLE;
+        }));
+        buildTempPathSpy.mockImplementation(jest.fn(async () => {
+            return dataSetPath;
+        }));
+        checkForStashSpy.mockImplementation(jest.fn(async () => {
+            return true;
+        }));
+        promptUserSpy.mockImplementation(jest.fn(async () => {
+            return true;
+        }));
+        localDownloadSpy.mockImplementation(jest.fn(async () => {
+            return localFile;
+        }));
+        uploadEditsSpy.mockImplementation(jest.fn(async () => {
+            return true;
+        }));
 
-        const zosErrorResp_etagMismatch = {
-            apiResponse:[],
-            success: false,
-            commandResponse: 'z/OSMF REST API Error:\nRest API failure with HTTP(S) status 412\n',
-        }
+        //Handler Setup
+        const handler = new EditHandler;
+        commandParameters.arguments.dataSetName = dataSetName;
+        const params = Object.assign({}, ...[commandParameters]);
+        params.arguments = Object.assign({}, ...[commandParameters.arguments]);
 
-        it("should update lfFile etag without overwriting file contents (keeping stash)", () => {
-            //TEST SETUP
-            // set lfFile to some pre-made ILocalFile from this file (lfFile.guiAvail = true)
-            // mock Utils.buildTempPath()
-            // mock Utils.checkForStash() to return true
-            // mock Utils.promptUser(Prompt.useStash) to return true
-            // mock Utils.fileComparison()
-            // mock Utils.localDownload() to return the same lfFile with an updated etag
-            //TEST CONFIRMATION
-            // check that pre-made lfFile doesn't have the same etag as remote but remote has same etag as updated lfFile
-            // check that file contents haven't been overwritten to be same as remote
-        })
-        it("should catch any error during setup, stash and etag retrieval", () => {
-            //TEST SETUP
-            // set lfFile to some pre-made ILocalFile from this file (lfFile.guiAvail = false)
-            // mock Utils.buildTempPath()
-            // mock Utils.checkForStash() to return false
-            // have Utils.localDownload() throw generic error
-            //TEST CONFIRMATION
-            //check that processWithSession throws error
-        })
-        it("should catch remote 404 error specifically ", () => {
-            //TEST SETUP
-            // set lfFile to some pre-made ILocalFile from this file (lfFile.guiAvail = false)
-            // mock Utils.buildTempPath()
-            // mock Utils.checkForStash() to return false
-            // have Utils.localDownload() throw 404
-            //TEST CONFIRMATION
-            //check that processWithSession throws 404 error
-        })
-        it("should make and upload edits successfully", () => {
-            //TEST SETUP
-            // set lfFile to some pre-made ILocalFile from this file (lfFile.guiAvail = true)
-            // mock Utils.buildTempPath()
-            // mock Utils.checkForStash() to return true
-            // mock Utils.promptUser(Prompt.useStash) to return true
-            // mock Utils.fileComparison()
-            // mock Utils.localDownload() to return the same lfFile with an updated etag
-            // mock Utils.makeEdits() to return
-            // mock Utils.uploadEdits() to return true
-            //TEST CONFIRMATION
-            // check that processWithSession returns IZosFilesResponse where IZosFilesResponse.commandResponse contains "uploaded edited file"
+        it("should make and upload edits successfully", async () => {
+            await handler.process(params);
+            expect(params.response.data.setObj).toHaveBeenCalledWith({
+                success: true,
+                commandResponse: TextUtils.chalk.green(
+                    "Successfully uploaded edited file to mainframe"
+                )});
         });
+        it("should catch remote 404 error", async () => {
+            localDownloadSpy.mockImplementation(jest.fn(async () => {
+                throw new ImperativeError({
+                    msg: "File not found on mainframe. Command terminated",
+                    errorCode: String(RestConstants.HTTP_STATUS_404)
+                });
+            }));
+
+            let caughtError;
+            try {
+                await handler.process(params)
+            } catch(e) {
+                caughtError = e;
+            }
+            expect(caughtError).toBeInstanceOf(ImperativeError)
+        })
     })
 })
