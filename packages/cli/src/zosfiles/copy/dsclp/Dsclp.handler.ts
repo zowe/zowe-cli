f2c74376cfd996b1438843447039e4e87c11b720
KevinLoesch1 <kevin.loesch@broadcom.com>
2023-04-28 11:07:02 -0400

    Change in copy overwrite prompt
    Signed-off-by: KevinLoesch1 <kevin.loesch@broadcom.com>

--- packages/cli/src/zosfiles/copy/dsclp/Dsclp.handler.ts
+++ packages/cli/src/zosfiles/copy/dsclp/Dsclp.handler.ts
@@ -58,7 +58,7 @@ export default class DsclpHandler extends ZosFilesBaseHandler {
     private promptForOverwrite(console: IHandlerResponseConsoleApi) {
         return async (targetDSN: string) => {
             const answer: string = await console.prompt(
-                `The dataset '${targetDSN}' already exists on the target system. Do you wish to overwrite it? [y/N]: `);
+                `The dataset '${targetDSN}' already exists on the target system. Do you want to overwrite it? [y/N]: `);
             return (answer != null && (answer.toLowerCase() === "y" || answer.toLowerCase() === "yes"));
         };
     }
