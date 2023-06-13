73026a812c68f96558d821ab14deeac93daaeab5
Gene Johnston <eugene.johnston@broadcom.com>
2023-06-13 11:52:47 -0400

    User errors in msg. Server response in causeErrors
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/core/src/rest/ZosmfRestClient.ts
+++ packages/core/src/rest/ZosmfRestClient.ts
@@ -9,7 +9,10 @@
 *
 */
 
-import { IImperativeError, Logger, RestClient, TextUtils, RestConstants, SessConstants } from "@zowe/imperative";
+import {
+    EnvironmentalVariableSettings, IImperativeError, Logger, RestClient, TextUtils,
+    RestConstants, SessConstants
+} from "@zowe/imperative";
 import { ZosmfHeaders } from "./ZosmfHeaders";
 
 /**
@@ -51,50 +54,98 @@ export class ZosmfRestClient extends RestClient {
      * @memberof ZosmfRestClient
      */
     protected processError(original: IImperativeError): IImperativeError {
-        original.msg = "z/OSMF REST API Error:\n" + original.msg;
+        // TODO:V3_ERR_FORMAT - Remove block in V3
+        if (!EnvironmentalVariableSettings.useV3ErrFormat()) {
+            original.msg = "z/OSMF REST API Error:\n" + original.msg;
+        }
 
-        let details = original.causeErrors;
+        let causeErrorsString = original.causeErrors; // TODO:V3_ERR_FORMAT - Remove in V3
+        let causeErrorsJson;
         try {
-            const json = JSON.parse(details);
-            // if we didn't get an error trying to parse json, check if there is a stack
-            // on the JSON error and delete it
-            if (json.stack != null) {
-                this.log.error("An error was encountered in z/OSMF with a stack." +
-                    " Here is the full error before deleting the stack:\n%s", JSON.stringify(json));
-                this.log.error("The stack has been deleted from the error before displaying the error to the user");
-                delete json.stack; // remove the stack field
-            }
+            // don't try to parse an empty string
+            if (original.causeErrors !== "") {
+                causeErrorsJson = JSON.parse(causeErrorsString);
+                // if we didn't get an error trying to parse causeErrorsString, check if there is a stack
+                // on the JSON error and delete it
+                if (causeErrorsJson.stack != null) {
+                    this.log.error("An error was encountered in z/OSMF with a stack." +
+                        " Here is the full error before deleting the stack:\n%s", JSON.stringify(causeErrorsJson));
+                    this.log.error("The stack has been deleted from the error before displaying the error to the user");
+                    delete causeErrorsJson.stack; // remove the stack field
+                }
 
-            // if we didn't get an error, make the parsed details part of the error
-            details = TextUtils.prettyJson(json, undefined, false);
+                // if we didn't get an error, make the parsed causeErrorsString part of the error
+                causeErrorsString = TextUtils.prettyJson(causeErrorsJson, undefined, false);
+            }
         } catch (e) {
-            // if there's an error, the causeErrors text is not json
+            // if there's an error, the causeErrors text is not JSON
             this.log.debug("Encountered an error trying to parse causeErrors as JSON  - causeErrors is likely not JSON format");
         }
-        original.msg += "\n" + details; // add the data string which is the original error
 
+        const origMsgFor401 = original.msg;
+        // TODO:V3_ERR_FORMAT - Don't test for env variable in V3
+        if (EnvironmentalVariableSettings.useV3ErrFormat()) {
+            // extract properties from causeErrors and place them into 'msg' as user-focused messages
+            if (causeErrorsJson?.details?.length > 0) {
+                for (const detail of causeErrorsJson.details) {
+                    original.msg += "\n" + detail;
+                }
+            }
+            if (causeErrorsJson?.messages?.length > 0) {
+                for (const message of causeErrorsJson.messages) {
+                    original.msg += "\n" + message.messageContent;
+                }
+            }
+        } else { // TODO:V3_ERR_FORMAT - Remove in V3
+            original.msg += "\n" + causeErrorsString; // add the data string which is the original error
+        }
+
+        // add further clarification on authentication errors
         if (this.response && this.response.statusCode === RestConstants.HTTP_STATUS_401) {
-            original.msg = "This operation requires authentication.\n\n" + original.msg +
-                "\nHost:      " + this.session.ISession.hostname +
-                "\nPort:      " + this.session.ISession.port +
-                "\nBase Path: " + this.session.ISession.basePath +
-                "\nResource:  " + this.mResource +
-                "\nRequest:   " + this.mRequest +
-                "\nHeaders:   " + JSON.stringify(this.mReqHeaders) +
-                "\nPayload:   " + this.mRequest +
-                "\n"
-            ;
+            // TODO:V3_ERR_FORMAT - Don't test for env variable in V3
+            if (EnvironmentalVariableSettings.useV3ErrFormat()) {
+                // For 401, the original msg belongs in the 'response from service' part of our error.
+                original.causeErrors = `{"Error": "${origMsgFor401}"}`;
+                original.msg  += "\nThis operation requires authentication.";
+            } else { // TODO:V3_ERR_FORMAT - Remove in V3
+                original.additionalDetails = "This operation requires authentication.\n\n" + original.msg +
+                    "\nHost:      " + this.session.ISession.hostname +
+                    "\nPort:      " + this.session.ISession.port +
+                    "\nBase Path: " + this.session.ISession.basePath +
+                    "\nResource:  " + this.mResource +
+                    "\nRequest:   " + this.mRequest +
+                    "\nHeaders:   " + JSON.stringify(this.mReqHeaders) +
+                    "\nPayload:   " + this.mRequest +
+                    "\n"
+                ;
+            }
 
             if (this.session.ISession.type === SessConstants.AUTH_TYPE_BASIC) {
-                original.additionalDetails = "Username or password are not valid or expired.\n\n";
+                // TODO:V3_ERR_FORMAT - Don't test for env variable in V3
+                if (EnvironmentalVariableSettings.useV3ErrFormat()) {
+                    original.msg += "\nUsername or password are not valid or expired.";
+                } else { // TODO:V3_ERR_FORMAT - Remove in V3
+                    original.additionalDetails = "Username or password are not valid or expired.\n\n";
+                }
             }
             if (this.session.ISession.type === SessConstants.AUTH_TYPE_TOKEN) {
-                original.additionalDetails = "Token is not valid or expired.\n\n" +
+                // TODO:V3_ERR_FORMAT - Don't test for env variable in V3
+                if (EnvironmentalVariableSettings.useV3ErrFormat()) {
+                    original.msg += "\nToken is not valid or expired." +
                     "For CLI usage, see `zowe auth login apiml --help`";
+                } else { // TODO:V3_ERR_FORMAT - Remove in V3
+                    original.additionalDetails = "Token is not valid or expired.\n\n" +
+                    "For CLI usage, see `zowe auth login apiml --help`";
+                }
             }
             // TODO: Add PFX support in the future
             if (this.session.ISession.type === SessConstants.AUTH_TYPE_CERT_PEM) {
-                original.additionalDetails = "Certificate is not valid or expired.\n\n";
+                // TODO:V3_ERR_FORMAT - Don't test for env variable in V3
+                if (EnvironmentalVariableSettings.useV3ErrFormat()) {
+                    original.msg += "\nCertificate is not valid or expired.";
+                } else { // TODO:V3_ERR_FORMAT - Remove in V3
+                    original.additionalDetails = "Certificate is not valid or expired.\n\n";
+                }
             }
         }
 
