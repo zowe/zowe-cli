7b32f2464fd971f40e4426d39eac8025d0ab9b55
Timothy Johnson <timothy.johnson@broadcom.com>
2023-09-18 14:26:57 -0400

    Try to fix TextUtils path in postinstall script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cli/scripts/printSuccessMessage.js
+++ packages/cli/scripts/printSuccessMessage.js
@@ -15,7 +15,7 @@ function getImperative() {
     } catch (err) {
         if (err.code === "ERR_MODULE_NOT_FOUND" || err.code === "MODULE_NOT_FOUND") {
             require("ts-node/register");
-            return require(require("path").resolve(__dirname, "../../../../imperative/packages/utilities/src/TextUtils"));
+            return require(require("path").resolve(__dirname, "../../imperative/src/utilities/src/TextUtils"));
         } else {
             throw err;
         }
@@ -33,26 +33,4 @@ function printSuccessMessage() {
     console.log("\n" + table + "\n");
 }
 
-// printSuccessMessage();
-const installSuccessMessage =
-    "Zowe CLI has been successfully installed. " +
-    "You can safely ignore all non-plug-in related errors and warnings. " +
-    "Please check above for any plug-in related issues.";
-const Table = require("cli-table3");
-const table = new Table({
-    chars: {
-        top: "═",
-        "top-left": "╔",
-        "top-right": "╗",
-        bottom: "═",
-        "bottom-left": "╚",
-        "bottom-right": "╝",
-        left: "║",
-        right: "║",
-    },
-    colWidths: [require("yargs").terminalWidth() - 8],
-    style: { "padding-left": 0, "padding-right": 0, head: [], border: [] },
-    wordWrap: true,
-});
-table.push([installSuccessMessage]);
-console.log("\n" + table.toString() + "\n");
+printSuccessMessage();
