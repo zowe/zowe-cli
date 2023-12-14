f79a0a4e5222ca0b33599b607551f8cb9f88f548
Gene Johnston <eugene.johnston@broadcom.com>
2023-12-14 11:51:47 -0500

    Remove profile creation from a profile read test
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/__tests__/__integration__/cmd/__tests__/integration/cli/read/__scripts__/profile/create_and_read.sh
+++ packages/imperative/__tests__/__integration__/cmd/__tests__/integration/cli/read/__scripts__/profile/create_and_read.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
-set -e
-OUTPUT=$(cmd-cli profiles create insecure "test_insecure" --info "some info" --secret "not so secret info")
+#!/bin/sh
+
+# include exitOnFailure function
+myScriptDir=`dirname $0`
+. $myScriptDir/exitOnFailure.sh
+
+# set -e
+# OUTPUT=$(cmd-cli profiles create insecure "test_insecure" --info "some info" --secret "not so secret info")
+
+# copy pre-existing profile to test directory
+cp -r $myScriptDir/../__resources__/profiles profiles
+exitOnFailure "Failed to copy test profile." $?
+
+# read the profile and display its information
 cmd-cli read profile
+exitOnFailure "Failed display profile." $?
