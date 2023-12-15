acbb5ef8344d21c705f4f95dac336d7a248781f5
Gene Johnston <eugene.johnston@broadcom.com>
2023-12-15 16:06:15 -0500

    Use pre-existing profile files not v1 profile cmds
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/core/__tests__/utils/__integration__/__scripts__/copy_profiles.sh
+++ packages/core/__tests__/utils/__integration__/__scripts__/copy_profiles.sh
@@ -7,7 +7,3 @@ myScriptDir=`dirname $0`
 # copy pre-existing profiles to test directory
 cp -r $myScriptDir/../__resources__/profiles profiles
 exitOnFailure "Failed to copy test profile." $?
-
-# read the profile and display its information
-cmd-cli read profile
-exitOnFailure "Failed display profile." $?
