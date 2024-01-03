536a5455ab1efa00e6a8c4ed2aca0cc796e5d160
Amber Torrise <at895452@broadcom.net>
2024-01-03 14:42:01 -0500

    reverting additions
    Signed-off-by: Amber Torrise <at895452@broadcom.net>

--- packages/cli/src/zosfiles/create/ds/ds.definition.ts
+++ packages/cli/src/zosfiles/create/ds/ds.definition.ts
@@ -52,7 +52,6 @@ export const DsDefinition: ICommandDefinition = {
         ZosFilesCreateOptions.unit,
         ZosFilesCreateOptions.dsntype,
         ZosFilesCreateExtraOptions.attributes,
-        ZosFilesCreateExtraOptions.showAttributes,
         ZosFilesCreateExtraOptions.like
     ].sort((a, b) => a.name.localeCompare(b.name)),
     examples: [
