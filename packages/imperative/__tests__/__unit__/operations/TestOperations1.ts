f8e22e011591496d4d8f332972ccfbe22931ffb3
Amber Torrise <at895452@broadcom.net>
2024-01-17 16:38:45 -0500

    adding back in operations tests
    Signed-off-by: Amber Torrise <at895452@broadcom.net>

---
+++ packages/imperative/__tests__/__unit__/operations/TestOperations1.ts
@@ -0,0 +1,28 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { TestSubOp1 } from "./subops/TestSubOp1";
+import { TestSubOp2 } from "./subops/TestSubOp2";
+import { TestSubOpNoUndo } from "./subops/TestSubOpNoUndo";
+import { Operations } from "../../../index";
+export class TestOperations1 extends Operations<any> {
+
+    constructor() {
+        super("Test Operations 1", true);
+        this.defineOperations();
+    }
+
+    protected defineOperations(): void {
+        this.addNextOperation(new TestSubOp1());
+        this.addNextOperation(new TestSubOp2());
+        this.addNextOperation(new TestSubOpNoUndo());
+    }
+}
---
+++ packages/imperative/__tests__/__unit__/operations/subops/TestSubOp4.ts
@@ -0,0 +1,41 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { IOperationCompleted, IOperationUndoCompleted, Operation } from "../../../../src/operations";
+import { TestLogger } from "../../../../__tests__/src/TestLogger";
+
+const logger = TestLogger.getTestLogger();
+
+export class TestSubOp4 extends Operation<any> {
+
+    constructor() {
+        super("Initialize Test Sub Op 4", true);
+    }
+
+    public logOperationResults(): void {
+        logger.info("Test sub operation 4 has ended.");
+    }
+
+    protected execute(inputParameters: any, operationCompletedCallback: IOperationCompleted<any>) {
+        this.operationResultMessage = "The test sub op 4 was executed.";
+        this.setOperationUndoable();
+        operationCompletedCallback(Operation.NO_OUTPUT);
+    }
+
+    protected undo(undoCompletedCallback: IOperationUndoCompleted): void {
+        logger.info("Performing undo action for test sub op 4.");
+        undoCompletedCallback();
+    }
+
+    protected logOperationBeginMessages(): void {
+        logger.info("Test sub operation 4 is beginning.");
+    }
+}
