bae96fdbfdf6ac7a283c06243bd7c7eb009072ee
Amber Torrise <amber.torrise@broadcom.com>
2024-02-22 16:30:37 -0500

    removing custom handling and using standard error handling
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- packages/cli/src/zosfiles/upload/ftds/FileToDataSet.handler.ts
+++ packages/cli/src/zosfiles/upload/ftds/FileToDataSet.handler.ts
@@ -9,10 +9,10 @@
 *
 */
 
-import { AbstractSession, IHandlerParameters, ITaskWithStatus, TaskStage, TextUtils } from "@zowe/imperative";
-import { IZosFilesResponse, Upload, IUploadResult } from "@zowe/zos-files-for-zowe-sdk";
+import { AbstractSession, IHandlerParameters, ITaskWithStatus, TaskStage } from "@zowe/imperative";
+import { IZosFilesResponse, Upload } from "@zowe/zos-files-for-zowe-sdk";
 import { ZosFilesBaseHandler } from "../../ZosFilesBase.handler";
-
+import { promises as fs } from 'fs';
 /**
  * Handler to upload content from a file to a data set
  * @export
@@ -21,49 +21,26 @@ export default class FileToDataSetHandler extends ZosFilesBaseHandler {
     public async processWithSession(commandParameters: IHandlerParameters,
         session: AbstractSession): Promise<IZosFilesResponse> {
 
+        const inputFile = commandParameters.arguments.inputfile;
+        const dataSetName = commandParameters.arguments.dataSetName;
+
+        // Check for file's existence
+        try {
+            await fs.access(inputFile, fs.constants.F_OK | fs.constants.R_OK);
+        } catch (error) {
+            throw new Error(`Failed to access the input file: ${inputFile}. Error: ${error.message}`);
+        }
+
+        // Then upload existing file
         const task: ITaskWithStatus = {
             percentComplete: 0,
             statusMessage: "Uploading to data set",
             stageName: TaskStage.IN_PROGRESS
         };
         commandParameters.response.progress.startBar({task});
-        const response = await Upload.fileToDataset(session, commandParameters.arguments.inputfile,
-            commandParameters.arguments.dataSetName,
-            {
-                volume: commandParameters.arguments.volumeSerial,
-                binary: commandParameters.arguments.binary,
-                record: commandParameters.arguments.record,
-                encoding: commandParameters.arguments.encoding,
-                task,
-                responseTimeout: commandParameters.arguments.responseTimeout
-            });
 
-        if (response.apiResponse) {
-            let skipCount: number = 0;
-            let successCount: number = 0;
-            let errorCount: number = 0;
-            response.apiResponse.forEach((element: IUploadResult) => {
-                if (element.success === true) {
-                    const formatMessage = TextUtils.prettyJson(element);
-                    commandParameters.response.console.log(formatMessage);
-                    successCount++;
-                } else if (element.success === false) {
-
-                    const formatMessage = TextUtils.prettyJson(element);
-                    commandParameters.response.console.error(TextUtils.chalk.red(formatMessage));
-                    errorCount++;
-                } else {
-                    skipCount++;
-                }
-            });
-
-            commandParameters.response.console.log(TextUtils.prettyJson({
-                file_to_upload: response.apiResponse.length,
-                success: successCount,
-                error: errorCount,
-                skipped: skipCount
-            }));
-        }
+        const response = await Upload.fileToDataset(session, inputFile, dataSetName, {
+        });
 
         return response;
     }
