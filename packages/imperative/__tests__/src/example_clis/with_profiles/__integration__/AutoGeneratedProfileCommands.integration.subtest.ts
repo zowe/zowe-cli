3fcffd13f8bf29418538eb33645e0e7fbf9a5f00
Gene Johnston <eugene.johnston@broadcom.com>
2024-02-23 10:59:08 -0500

    Example CLI integration tests no longer expect values to be read from V1 profile files
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/__tests__/src/example_clis/with_profiles/__integration__/AutoGeneratedProfileCommands.integration.subtest.ts
+++ packages/imperative/__tests__/src/example_clis/with_profiles/__integration__/AutoGeneratedProfileCommands.integration.subtest.ts
@@ -9,99 +9,23 @@
 *
 */
 
-import { IImperativeConfig } from "../../../../../src/imperative";
 import * as T from "../../../TestUtil";
-import * as path from "path";
-import * as fs from "fs";
 
 describe("We should provide auto-generated profile commands for convenience, " +
     "so that Imperative-based CLIs can let users manage configuration profiles", () => {
     const cliBin = __dirname + "/../ProfileExampleCLI.ts";
-    const config: IImperativeConfig = require(__dirname + "/../ProfileExampleConfiguration");
 
-    const profileTypeA = "profile-a";
-    const profileTypeB = "profile-b";
-
-    const manyFieldProfile = "many-field-profile";
-    const env = JSON.parse(JSON.stringify(process.env));
-    const home = config.defaultHome;
-
-    beforeAll(() => {
-        // ensure a clean CLI home directory exists before running our copy_profile script
-        T.rimraf(home);
-        fs.mkdirSync(home);
-
-        // copy existing profiles into test directory
-        const result = T.runCliScript(path.join(__dirname, "__scripts__/copy_auto_gen_profiles.sh"), home);
-        expect(result.stderr.toString()).toBe("");
-        expect(result.status).toBe(0);
-    });
-
-    afterAll(() => {
-        T.rimraf(home);
-    });
-
-    it("should use a profile with a valid dependent profile", () => {
+    it("should fail to load a V1 dependent profile", () => {
         const result = T.executeTestCLICommand(cliBin, this, ["use-dependent-profile"]);
-        expect(result.stderr).toBe("");
-        expect(result.stdout).toContain("Loaded profile dependency of type profile-a");
-        expect(result.stdout).toContain("Loaded main profile of type profile-with-dependency");
-        expect(result.status).toBe(0);
-    });
 
-    it("should use a profile with a dependent profile that is not the default for its type", () => {
-        // set the default profile for type profile-a
-        let result: any = T.runCliScript(path.join(__dirname, "__scripts__/set_default_profile.sh"), home,
-            [profileTypeA, "non_existent_default_a_profile"]
+        /* Since we no longer read V1 profiles from disk, such an operation will always return an error.
+           Note that Zowe client code no longer attempts to do such an operation.
+         */
+        expect(result.stderr).toContain(
+            'Profile of type "profile-with-dependency" does not exist ' +
+            'within the loaded profiles for the command and it is marked as required'
         );
-        expect(result.stderr.toString()).toBe("");
-        expect(result.status).toBe(0);
-
-        // use a profile that has a dependency which is now NOT the default profile for that dependency
-        result = T.executeTestCLICommand(cliBin, this, ["use-dependent-profile"]);
-        expect(result.stderr).toBe("");
-        expect(result.stdout).toContain(`Loaded profile dependency of type ${profileTypeA}`);
-        expect(result.stdout).toContain("Loaded main profile of type profile-with-dependency");
-        expect(result.status).toBe(0);
-    });
-
-    it("should be able to use two types of profiles", () => {
-        const firstProfile = "first";
-        const secondProfile = "second";
-
-        // set the default profile for type profile-a
-        let result: any = T.runCliScript(path.join(__dirname, "__scripts__/set_default_profile.sh"), home,
-            [profileTypeA, firstProfile]
-        );
-        expect(result.stderr.toString()).toBe("");
-        expect(result.status).toBe(0);
-
-        // set the default profile for type profile-b
-        result = T.runCliScript(path.join(__dirname, "__scripts__/set_default_profile.sh"), home,
-            [profileTypeB, firstProfile]
-        );
-        expect(result.stderr.toString()).toBe("");
-        expect(result.status).toBe(0);
-
-        // use both A profiles
-        T.findExpectedOutputInCommand(cliBin, ["use-profile-a"],
-            [], // default A profile
-            "stdout", true, this, T.CMD_TYPE.JSON, { ignoreCase: true }
-        );
-        T.findExpectedOutputInCommand(cliBin, ["use-profile-a", "--profile-a-profile", secondProfile],
-            [],
-            "stdout", true, this, T.CMD_TYPE.JSON, { ignoreCase: true }
-        );
-
-        // use both B profiles
-        T.findExpectedOutputInCommand(cliBin, ["use-profile-b"], // default B profile
-            [],
-            "stdout", true, this, T.CMD_TYPE.JSON, { ignoreCase: true }
-        );
-        T.findExpectedOutputInCommand(cliBin, ["use-profile-b", "--profile-b-profile", secondProfile],
-            [],
-            "stdout", true, this, T.CMD_TYPE.JSON, { ignoreCase: true }
-        );
+        expect(result.status).toBe(1);
     });
 
     it("should not fail a command where the profile is listed as optional and not specified", () => {
