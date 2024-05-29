c98ad7adca1d4acb068b749a8869e30807f50606
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2024-05-29 09:21:25 -0700

    Updated unit tests for Proxy.ts
    Signed-off-by: Peter Haumer <4391934+phaumer@users.noreply.github.com>

--- packages/imperative/src/rest/__tests__/client/Proxy.unit.test.ts
+++ packages/imperative/src/rest/__tests__/client/Proxy.unit.test.ts
@@ -8,37 +8,37 @@
 * Copyright Contributors to the Zowe Project.
 *
 */
-import { Proxy } from '../../src/client/Proxy';
-import { HTTPS_PROTOCOL, HTTP_PROTOCOL } from '../../src/session/SessConstants';
-import { ZosmfSession } from '../../../../../zosmf/src/ZosmfSession';
-import { HttpsProxyAgent } from 'https-proxy-agent';
-import { HttpProxyAgent } from 'http-proxy-agent';
-import { env } from 'process';
 
-describe('Proxy tests', () => {
-    const session = ZosmfSession.createSessCfgFromArgs({
-        $0: "zowe",
-        _: [""],
-        host: "fake",
-        port: "fake",
-        rejectUnauthorized: false,
-        basePath: "fakeBasePath",
-        tokenValue: "fake",
-        tokenType: "fake"
-    });
+import * as process from "process";
+
+import { Proxy } from "../../src/client/Proxy";
+
+import { HTTPS_PROTOCOL, HTTP_PROTOCOL } from "../../src/session/SessConstants";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import { HttpProxyAgent } from "http-proxy-agent";
+
+import { ISession } from "../../src/session/doc/ISession";
+
+describe("Proxy tests", () => {
+    const session = {
+        hostname: "fake.com",
+        port: 443,
+        rejectUnauthorized: false
+    } as ISession;
     const privateProxy = Proxy as any;
-    const httpUrl = 'http://www.zowe.com';
-    const httpsUrl = 'https://www.zowe.com';
+    const httpUrl = "http://www.zowe.com";
+    const httpsUrl = "https://www.zowe.com";
+    const noProxyList = "www.zowe.com, fake.com,ibm.com,broadcom.com   ";
     let getProxySettingsSpy: jest.SpyInstance;
     let checkUrlSpy: jest.SpyInstance;
 
-    describe('getProxyAgent', () => {
+    describe("getProxyAgent", () => {
         beforeEach(() => {
             jest.clearAllMocks();
             getProxySettingsSpy = jest.spyOn(privateProxy, "getProxySettings");
         });
 
-        it('Should retrieve the HTTP proxy agent', () => {
+        it("Should retrieve the HTTP proxy agent", () => {
             const expected = new HttpProxyAgent(httpUrl);
             getProxySettingsSpy.mockReturnValue({
                 proxyUrl: httpUrl,
@@ -47,7 +47,7 @@ describe('Proxy tests', () => {
             expect(JSON.stringify(Proxy.getProxyAgent(session))).toEqual(JSON.stringify(expected));
         });
 
-        it('Should retrieve the HTTPS proxy agent', () => {
+        it("Should retrieve the HTTPS proxy agent", () => {
             const expected = new HttpsProxyAgent(httpsUrl, { rejectUnauthorized: false });
             getProxySettingsSpy.mockReturnValue({
                 proxyUrl: httpsUrl,
@@ -57,13 +57,13 @@ describe('Proxy tests', () => {
         });
     });
 
-    describe('getSystemProxyUrl', () => {
+    describe("getSystemProxyUrl", () => {
         beforeEach(() => {
             jest.clearAllMocks();
             getProxySettingsSpy = jest.spyOn(privateProxy, "getProxySettings");
         });
 
-        it('Should retrieve the system proxy URL', () => {
+        it("Should retrieve the system proxy URL", () => {
             getProxySettingsSpy.mockReturnValue({
                 proxyUrl: httpsUrl,
                 protocol: HTTPS_PROTOCOL
@@ -72,13 +72,13 @@ describe('Proxy tests', () => {
         });
     });
 
-    describe('getProxySettings', () => {
+    describe("getProxySettings", () => {
         beforeEach(() => {
             jest.clearAllMocks();
             checkUrlSpy = jest.spyOn(privateProxy, "checkUrl");
         });
 
-        it('Should return proxy settings from session', () => {
+        it("Should return proxy settings from session", () => {
             const expected = {
                 proxyUrl: httpsUrl,
                 protocol: HTTPS_PROTOCOL
@@ -88,20 +88,33 @@ describe('Proxy tests', () => {
         });
     });
 
-    describe('getHttpEnvVariables', () => {
+    describe("getHttpEnvVariables", () => {
         beforeEach(() => {
             jest.clearAllMocks();
         });
 
-        it('Should return the HTTP environment variables if they exist', () => {
-            const expected = {
-                test: "test"
-            };
-            Object.defineProperty(env, "HTTP_PROXY", {
-                value: expected,
-                configurable: true
-            });
+        it("Should return the HTTP environment variables if they exist", () => {
+            const expected = httpUrl;
+            process.env["HTTP_PROXY"] = expected;
             expect(Proxy["getHttpEnvVariables"]()).toEqual(expected);
+            process.env["HTTP_PROXY"] = undefined;
+        });
+    });
+
+    describe("matchesNoProxySettings", () => {
+        it("Should match session hostname with no_proxy",  () => {
+            const expected = true;
+            process.env["NO_PROXY"] = noProxyList;
+            expect(Proxy["matchesNoProxySettings"](session)).toEqual(expected);
+            process.env["NO_PROXY"] = undefined;
+        });
+
+        it("Should not match session hostname with no_proxy",  () => {
+            const expected = false;
+            process.env["NO_PROXY"] = noProxyList;
+            session.hostname = "microsoft.com";
+            expect(Proxy["matchesNoProxySettings"](session)).toEqual(expected);
+            process.env["NO_PROXY"] = undefined;
         });
     });
 });
