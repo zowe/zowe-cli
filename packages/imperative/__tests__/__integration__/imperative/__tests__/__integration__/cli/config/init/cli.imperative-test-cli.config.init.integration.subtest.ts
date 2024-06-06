321d40c63b07348f267ac41762aaa21da320797b
Gene Johnston <eugene.johnston@broadcom.com>
2024-06-06 16:36:18 -0400

    Fix config-init integ tests for project_base
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/__tests__/__integration__/imperative/__tests__/__integration__/cli/config/init/cli.imperative-test-cli.config.init.integration.subtest.ts
+++ packages/imperative/__tests__/__integration__/imperative/__tests__/__integration__/cli/config/init/cli.imperative-test-cli.config.init.integration.subtest.ts
@@ -12,7 +12,11 @@
 import { ITestEnvironment } from "../../../../../../../__src__/environment/doc/response/ITestEnvironment";
 import { SetupTestEnvironment } from "../../../../../../../__src__/environment/SetupTestEnvironment";
 import { runCliScript } from "../../../../../../../src/TestUtil";
-import { expectedSchemaObject, expectedConfigObject, expectedUserConfigObject } from "../__resources__/expectedObjects";
+import {
+    expectedSchemaObject,
+    expectedGlobalConfigObject, expectedGlobalUserConfigObject,
+    expectedProjectConfigObject, expectedProjectUserConfigObject
+} from "../__resources__/expectedObjects";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -51,132 +55,102 @@ describe("imperative-test-cli config init", () => {
     it("should initialize a project config", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config.sh",
             TEST_ENVIRONMENT.workingDir, ["--prompt false"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json");
+        const expectedProjectConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.schema.json");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
-        expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedConfigObject);
+        expect(response.output.toString()).toContain(expectedProjectConfigLocation);
+        expect(fs.existsSync(expectedProjectConfigLocation)).toEqual(true);
+        expect(fs.existsSync(expectedProjectConfigLocation)).toEqual(true);
+        expect(JSON.parse(fs.readFileSync(expectedProjectConfigLocation).toString())).toEqual(expectedProjectConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
 
     });
     it("should initialize a user project config", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config.sh",
             TEST_ENVIRONMENT.workingDir, ["--user-config --prompt false"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.user.json");
+        const expectedProjectUserConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.user.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.schema.json");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedProjectUserConfigLocation);
+        expect(fs.existsSync(expectedProjectUserConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedUserConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedProjectUserConfigLocation).toString())).toEqual(expectedProjectUserConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a global config", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config.sh",
             TEST_ENVIRONMENT.workingDir, ["--global-config --prompt false"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.json");
+        const expectedGlobalConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.schema.json");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedGlobalConfigLocation);
+        expect(fs.existsSync(expectedGlobalConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedGlobalConfigLocation).toString())).toEqual(expectedGlobalConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a user global config", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config.sh",
             TEST_ENVIRONMENT.workingDir, ["--global-config --user-config --prompt false"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.user.json");
+        const expectedGlobalUserConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.user.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.schema.json");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
-        expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedUserConfigObject);
+        expect(response.output.toString()).toContain(expectedGlobalUserConfigLocation);
+        expect(fs.existsSync(expectedGlobalUserConfigLocation)).toEqual(true);
+        expect(fs.existsSync(expectedGlobalUserConfigLocation)).toEqual(true);
+        expect(JSON.parse(fs.readFileSync(expectedGlobalUserConfigLocation).toString())).toEqual(expectedGlobalUserConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a project config with prompting", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config_prompt.sh",
             TEST_ENVIRONMENT.workingDir, [""]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json");
+        const expectedProjectConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.schema.json");
         expect(response.output.toString()).not.toContain("Unable to securely save credentials");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedProjectConfigLocation);
+        expect(fs.existsSync(expectedProjectConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedProjectConfigLocation).toString())).toEqual(expectedProjectConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a user project config with prompting", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config_prompt.sh",
             TEST_ENVIRONMENT.workingDir, ["--user-config"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.user.json");
+        const expectedProjectUserConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.user.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.schema.json");
         expect(response.output.toString()).not.toContain("Unable to securely save credentials");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedProjectUserConfigLocation);
+        expect(fs.existsSync(expectedProjectUserConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedUserConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedProjectUserConfigLocation).toString())).toEqual(expectedProjectUserConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a global config with prompting", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config_prompt.sh",
             TEST_ENVIRONMENT.workingDir, ["--global-config"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.json");
+        const expectedGlobalConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.schema.json");
         expect(response.output.toString()).not.toContain("Unable to securely save credentials");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedGlobalConfigLocation);
+        expect(fs.existsSync(expectedGlobalConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedGlobalConfigLocation).toString())).toEqual(expectedGlobalConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
     it("should initialize a user global config with prompting", () => {
         const response = runCliScript(__dirname + "/__scripts__/init_config_prompt.sh",
             TEST_ENVIRONMENT.workingDir, ["--global-config --user-config"]);
-        const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.user.json");
+        const expectedGlobalUserConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.user.json");
         const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.schema.json");
         expect(response.output.toString()).not.toContain("Unable to securely save credentials");
         expect(response.output.toString()).toContain(`Saved config template to`);
-        expect(response.output.toString()).toContain(expectedConfigLocation);
-        expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
+        expect(response.output.toString()).toContain(expectedGlobalUserConfigLocation);
+        expect(fs.existsSync(expectedGlobalUserConfigLocation)).toEqual(true);
         expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedUserConfigObject);
+        expect(JSON.parse(fs.readFileSync(expectedGlobalUserConfigLocation).toString())).toEqual(expectedGlobalUserConfigObject);
         expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
     });
-    // eslint-disable-next-line jest/no-commented-out-tests
-    // it("should create a profile of a specified name", () => {
-    //     const response = runCliScript(__dirname + "/__scripts__/init_config.sh",
-    //         TEST_ENVIRONMENT.workingDir, ["--profile lpar.service --prompt false"]);
-    //     const expectedConfigLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.config.json");
-    //     const expectedSchemaLocation = path.join(TEST_ENVIRONMENT.workingDir, "imperative-test-cli.schema.json");
-    //     const expectedConfigObject: IConfig = {
-    //         $schema: "./imperative-test-cli.schema.json",
-    //         profiles: {
-    //             lpar: {
-    //                 properties: {},
-    //                 profiles: {
-    //                     service: {
-    //                         properties: {}
-    //                     }
-    //                 }
-    //             }
-    //         },
-    //         defaults: {},
-    //         secure: []
-    //     };
-    //     expect(response.output.toString()).toContain(`Saved config template to`);
-    //     expect(response.output.toString()).toContain(expectedConfigLocation);
-    //     expect(fs.existsSync(expectedConfigLocation)).toEqual(true);
-    //     expect(fs.existsSync(expectedSchemaLocation)).toEqual(true);
-    //     expect(JSON.parse(fs.readFileSync(expectedConfigLocation).toString())).toEqual(expectedConfigObject);
-    //     expect(JSON.parse(fs.readFileSync(expectedSchemaLocation).toString())).toEqual(expectedSchemaObject);
-    //     runCliScript(__dirname + "/../__scripts__/delete_configs.sh", TEST_ENVIRONMENT.workingDir,
-    //         ["imperative-test-cli.config.json imperative-test-cli.schema.json"]);
-    // });
 });
