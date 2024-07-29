7b3baa36f0afeb65721f37b7ccf3129f6a83d855
Amber Torrise <amber.torrise@broadcom.com>
2024-07-29 10:17:17 -0400

    keeping work towards table of cli squad but removing it from needing to be done in this workflow
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- .github/updateReleaseMarkdown.js
+++ .github/updateReleaseMarkdown.js
@@ -1,37 +1,65 @@
 const fs = require('fs');
 const path = require('path');
 
+const mdFilePath = path.join(__dirname, '../RELEASE_HISTORY.md');
+
 // Build the new row to be added
 const newRow = `|  v${process.env.NEW_VERSION}  | ${new Date().toISOString().split('T')[0].slice(0, 7)} | **Active** | [Release Notes](https://docs.zowe.org/stable/whats-new/release-notes/v${process.env.NEW_VERSION.replace(/\./g, '_')}) |`;
 
-const mdFilePath = path.join(__dirname, '../RELEASE_HISTORY.md');
-
 // Read, Update and Write to Markdown File
-fs.readFile(mdFilePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
+function updateReleaseHistory(newRow) {
+    fs.readFile(mdFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
 
-  // Find the table and insert the new row after the second row
-  const lines = data.split('\n');
-  let tableLineCount = 0;
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].startsWith('|') && lines[i].endsWith('|')) {
-      tableLineCount++;
-      if (tableLineCount === 2) {
-        // Insert the new row after the second row
-        lines.splice(i + 1, 0, newRow);
-        break;
-      }
-    }
-  }
+        // Find the table and insert the new row after the second row
+        const lines = data.split('\n');
+        let tableLineCount = 0;
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].startsWith('|') && lines[i].endsWith('|')) {
+                tableLineCount++;
+                if (tableLineCount === 2) {
+                    // Insert the new row after the second row
+                    lines.splice(i + 1, 0, newRow);
+                    break;
+                }
+            }
+        }
 
-  fs.writeFile(mdFilePath, lines.join('\n'), 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing the file:', err);
-      return;
-    }
-    console.log('Markdown file updated successfully.');
-  });
-});
+        fs.writeFile(mdFilePath, lines.join('\n'), 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing the file:', err);
+                return;
+            }
+            console.log('Markdown file updated successfully.');
+        });
+    });
+}
+
+// Update the zoweVersion in package.json
+function updatePackageJsonVersion(newVersion) {
+    const packageJsonPath = path.join(__dirname, '../packages/cli/package.json');
+    fs.readFile(packageJsonPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading package.json:', err);
+            return;
+        }
+
+        let packageJson = JSON.parse(data);
+        packageJson.zoweVersion = `v${newVersion}`;
+
+        fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing to package.json:', err);
+                return;
+            }
+            console.log('package.json updated successfully.');
+        });
+    });
+}
+
+// Execute the functions
+updatePackageJsonVersion(process.env.NEW_VERSION);
+updateReleaseHistory(newRow);
--- RELEASE_HISTORY.md
+++ RELEASE_HISTORY.md
@@ -7,7 +7,7 @@ Zowe follows a regular release schedule with major versions released every two y
 ### Zowe v3.x LTS Releases
 |  Version | Release Date |  Status    | Release Notes |
 |:--------:|:------------:|:----------:|:-------------:|
-|  v3.0.0  | 2024-09      | **Active** | [Link](#)     |
+|  v3.0.0-prerelease  | 2024-08      | **Under Development** |     |
 
 ### Major Release Timeline
 
@@ -17,7 +17,7 @@ Zowe follows a regular release schedule with major versions released every two y
 
 - **Active**: Each major version remains in this phase for 2 years, receiving regular updates and new features.
 - **Maintenance**: Following the Active phase, each major version remains in this phase for an additional 2.5 years, receiving only critical fixes and security patches.
-- **Under Development**: The pre-Active phase when the next major version is prepared. This phase varies in length and the version is not available for general consumption.
+- **Under Development**: The pre-Active phase where the next major version is prepared. This phase varies in length and is not available for general consumption.
 
 ### Guarantees
 
@@ -63,22 +63,7 @@ The following [components of the release](https://github.com/zowe/community/blob
 
 
 ## Zowe CLI Squad
-
-| Name | GitHub ID | Email | Roles |
-|-|-|-|-|
-| Adam Wolfe | @adam-wolfe | adam.wolfe@broadcom.com | Lead, TSC Rep, Security Squad Rep |
-| Amanda D'Errico | @AmandaDErrico | amanda.derrico@ibm.com | Developer |
-| Amber Torrise | @ATorrise | amber.torrise@broadcom.com | Developer |
-| Ana X. Ceron | @anaxceron | ana.ceron@broadcom.com | Tech Writer |
-| Andrew Harn | @awharn | andrew.harn@broadcom.com | Developer |
-| Eugene Johnston | @gejohnston | eugene.johnston@broadcom.com | Developer |
-| Fernando Rijo Cedeno | @zFernand0 | fernando.rijocedeno@broadcom.com | Developer |
-| Jessica Tonda | @JTonda | jessica.tonda@broadcom.com | Scrum Master |
-| Kevin Loesch | @KevinLoesch1 | kevin.loesch@broadcom.com | Developer |
-| Timothy Johnson | @t1m0thyj | timothy.johnson@broadcom.com | Developer |
-| Trae Yelovich | @traeok | trae.yelovich@broadcom.com | Developer |
-
-See the full lists below:
+- https://github.com/zowe/community/blob/master/COMMITTERS.md#zowe-cli-squad
 - https://github.com/orgs/zowe/teams/zowe-cli-administrators
 - https://github.com/orgs/zowe/teams/zowe-cli-committers
 - https://github.com/orgs/zowe/teams/zowe-cli-contributors
