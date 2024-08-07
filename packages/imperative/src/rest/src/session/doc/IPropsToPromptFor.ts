feac0a8089eb5b43f6f4ef285dc54cc6f36f3895
jace-roell <jace.roell@hotmail.com>
2024-08-07 09:46:28 -0400

    explicitly import ISession and add license header
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/imperative/src/rest/src/session/doc/IPropsToPromptFor.ts
+++ packages/imperative/src/rest/src/session/doc/IPropsToPromptFor.ts
@@ -1,4 +1,15 @@
-import { ISession } from "../../..";
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ISession } from './ISession';
 
 export interface IPropsToPromptFor <SessCfgType extends ISession=ISession> {
     name: keyof SessCfgType & string,
