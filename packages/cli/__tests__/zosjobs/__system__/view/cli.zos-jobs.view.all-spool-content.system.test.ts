70cb5221816afb99244ba248123522d025a976f4
ATorrise <amber.torrise@broadcom.com>
2024-08-26 11:14:02 -0400

    whoops
    Signed-off-by: ATorrise <amber.torrise@broadcom.com>

--- packages/cli/__tests__/zosjobs/__system__/view/cli.zos-jobs.view.all-spool-content.system.test.ts
+++ packages/cli/__tests__/zosjobs/__system__/view/cli.zos-jobs.view.all-spool-content.system.test.ts
@@ -1,3 +1,14 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
 import { ITestEnvironment, runCliScript } from "@zowe/cli-test-utils";
 import { TestEnvironment } from "../../../../../../__tests__/__src__/environment/TestEnvironment";
 import { ITestPropertiesSchema } from "../../../../../../__tests__/__src__/properties/ITestPropertiesSchema";
