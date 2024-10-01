b1b8130f5737d772f1e8bfef76ef5f3a8c41cd35
jace-roell <jace.roell@hotmail.com>
2024-10-01 11:43:43 -0400

    start command POC
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zostso/src/StartTsoApp.ts
+++ packages/zostso/src/StartTsoApp.ts
@@ -1,13 +1,13 @@
 /*
-* This program and the accompanying materials are made available under the terms of the
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Copyright Contributors to the Zowe Project.
-*
-*/
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
 
 import { AbstractSession, Headers } from "@zowe/imperative";
 import { ZosmfRestClient } from "@zowe/core-for-zowe-sdk";
@@ -20,6 +20,8 @@ import { noAccountNumber, TsoConstants } from "./TsoConstants";
 import { TsoResponseService } from "./TsoResponseService";
 import { IStartASAppResponse } from "./doc/IStartASAppResponse";
 import { IStartTsoAppParms } from "./doc/input/IStartTsoAppParms";
+import { StartTso } from "./StartTso";
+import { IIssueResponse } from "../lib";
 /**
  * Start TSO address space and receive servlet key
  * @export
@@ -35,29 +37,48 @@ export class StartTsoApp {
      * @returns {Promise<IStartASAppResponse>} command response on resolve, @see {IStartASAppResponse}
      * @memberof StartTso
      */
-    public static async start(session: AbstractSession, accountNumber: string, params: IStartTsoAppParms, parms?: IStartTsoParms): Promise<IStartASAppResponse> {
+    public static async start(
+        session: AbstractSession,
+        accountNumber: string,
+        params: IStartTsoAppParms,
+        startParms: IStartTsoParms
+    ): Promise<IStartASAppResponse> {
         // Address space is not known
         TsoValidator.validateSession(session);
-        TsoValidator.validateNotEmptyString(accountNumber, noAccountNumber.message);
-        if(!params.queueID || !params.servletKey)
-        {
-            console.log("NEEDS IMPLEMENTATION");
-            return undefined;
+        TsoValidator.validateNotEmptyString(
+            accountNumber,
+            noAccountNumber.message
+        );
+        if (!params.queueID || !params.servletKey) {
+            const response: IIssueResponse = {
+                success: false,
+                startResponse: await StartTso.start(
+                    session,
+                    accountNumber,
+                    startParms
+                ),
+                startReady: false,
+                zosmfResponse: null,
+                commandResponse: null,
+                stopResponse: null,
+            };
+            params.servletKey =
+                response.startResponse.zosmfTsoResponse.servletKey;
+            params.queueID = response.startResponse.zosmfTsoResponse.queueID;
         }
         // Address space is known
-        else
-        {
-            const endpoint = `${TsoConstants.RESOURCE}/app/${params.servletKey}/${params.appKey}`;
-            const apiResponse = await ZosmfRestClient.postExpectJSON<IStartASAppResponse>(
+        const endpoint = `${TsoConstants.RESOURCE}/app/${params.servletKey}/${params.appKey}`;
+        const apiResponse =
+            await ZosmfRestClient.postExpectJSON<IStartASAppResponse>(
                 session,
                 endpoint,
                 [Headers.APPLICATION_JSON],
                 {
-                    "startcmd": `${params.startupCommand} '&1 &2 ${params.queueID}'`
+                    startcmd: `${params.startupCommand} '&1 &2 ${params.queueID}'`,
                 }
             );
-            return apiResponse;
-        }
+        apiResponse.queueID = params.queueID;
+        apiResponse.servletKey = params.servletKey;
+        return apiResponse;
     }
 }
-
