3a9b6a551c4c81c3c3cf7d2e5a25e21e6a5486f1
jace-roell <jace.roell@hotmail.com>
2024-10-10 13:40:40 -0400

    format
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zostso/src/SendTsoApp.ts
+++ packages/zostso/src/SendTsoApp.ts
@@ -44,25 +44,24 @@ export class SendTsoApp {
         );
 
         const endpoint = `${TsoConstants.RESOURCE}/app/${params.servletKey}/${params.appKey}`;
-        const apiResponse = await ZosmfRestClient.putExpectJSON<
-        IASAppResponse & { ver: string }
-        >(
-            session,
-            endpoint,
-            [Headers.CONTENT_TYPE, "text/plain"],
-            params.message
-        );
-        const formattedApiResponse: IASAppResponse = {
-            version: apiResponse.ver,
-            reused: apiResponse.reused,
-            timeout: apiResponse.timeout,
-            servletKey: apiResponse.servletKey ?? null,
-            queueID: apiResponse.queueID ?? null,
-            tsoData: apiResponse.tsoData.map((message: any) => ({
-                VERSION: message["TSO MESSAGE"].VERSION,
-                DATA: message["TSO MESSAGE"].DATA,
-            })),
-        };
+        const apiResponse =
+            await ZosmfRestClient.putExpectJSON<IASAppResponse&{ver: string}>(
+                session,
+                endpoint,
+                [Headers.CONTENT_TYPE, "text/plain"],
+                params.message
+            );
+            const formattedApiResponse: IASAppResponse = {
+                version: apiResponse.ver,
+                reused: apiResponse.reused,
+                timeout: apiResponse.timeout,
+                servletKey: apiResponse.servletKey ?? null,
+                queueID: apiResponse.queueID ?? null,
+                tsoData: apiResponse.tsoData.map((message: any) => ({
+                    VERSION: message["TSO MESSAGE"].VERSION,
+                    DATA: message["TSO MESSAGE"].DATA,
+                })),
+            };
         return formattedApiResponse;
     }
 }
