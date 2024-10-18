3af43f170b7ab84321d1fd98fce0d56a87c7edd7
jace-roell <jace.roell@hotmail.com>
2024-10-18 12:30:18 -0400

    remove echo from shell
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zostso/__tests__/__system__/__scripts__/send/send_tso_app.sh
+++ packages/zostso/__tests__/__system__/__scripts__/send/send_tso_app.sh
@@ -9,14 +9,5 @@ servletKey=$7
 message=$8
 appKey=$9
 
-echo "Account: $account"
-echo "Host: $host"
-echo "Port: $port"
-echo "User: $user"
-echo "Password: $password"
-echo "RU: $ru"
-echo "ServletKey: $servletKey"
-echo "Message: $message"
-
 zowe zos-tso send app --ak "$appKey" --sk "$servletKey" --message "$message" --account $account --host $host --port $port --user $user --password $password --ru $ru
 exit $?
