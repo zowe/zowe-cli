4b291d68fe292d05dbd3672948c23a82643cc4ee
jace-roell <jace.roell@hotmail.com>
2024-10-24 09:37:56 -0400

    unit test and snapshot
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/cli/__tests__/zosfiles/__unit__/upload/ftu/FileToUSS.handler.unit.test.ts
+++ packages/cli/__tests__/zosfiles/__unit__/upload/ftu/FileToUSS.handler.unit.test.ts
@@ -11,6 +11,7 @@
 
 import { Upload } from "@zowe/zos-files-for-zowe-sdk";
 import { UNIT_TEST_ZOSMF_PROF_OPTS } from "../../../../../../../__tests__/__src__/TestConstants";
+import { ZosFilesAttributes } from "@zowe/zos-files-for-zowe-sdk";
 
 describe("Upload file-to-uss handler", () => {
     describe("process method", () => {
@@ -92,6 +93,96 @@ describe("Upload file-to-uss handler", () => {
                 includeHidden: undefined,
                 maxConcurrentRequests: undefined,
                 responseTimeout: undefined
+            });
+            expect(jsonObj).toMatchSnapshot();
+            expect(apiMessage).toMatchSnapshot();
+            expect(logMessage).toMatchSnapshot();
+        });
+        it("should upload a file to a USS if requested - zosattributes file", async () => {
+            // Require the handler and create a new instance
+            const handlerReq = require("../../../../../src/zosfiles/upload/ftu/FileToUSS.handler");
+            const handler = new handlerReq.default();
+            const inputfile = "test-file";
+            const USSFileName = "testing";
+            let zosAttributes: any;
+
+            let error;
+            let apiMessage = "";
+            let jsonObj;
+            let logMessage = "";
+            let fakeSession = null;
+
+            jest.spyOn(ZosFilesAttributes, "loadFromFile").mockImplementation(() => {
+                zosAttributes = Object.create(ZosFilesAttributes.prototype);
+                zosAttributes.attributes = new Map([
+                    ['*.json', { ignore: true }],
+                    ['*.bin', { ignore: false, localEncoding: 'binary', remoteEncoding: 'binary' }],
+                    ['*.jcl', { ignore: false, localEncoding: 'IBM-1047', remoteEncoding: 'IBM-1047' }],
+                    ['*.md', { ignore: false, localEncoding: 'UTF-8', remoteEncoding: 'UTF-8' }],
+                    ['*.txt', { ignore: false, localEncoding: 'UTF-8', remoteEncoding: 'IBM-1047' }]
+                ]);
+                zosAttributes.basePath = undefined;
+                return zosAttributes;
+            });
+            Upload.uploadFile = jest.fn(async (session, file, name, options = {}) => {
+                fakeSession = session;
+                return {
+                    success: true,
+                    commandResponse: "uploaded",
+                    apiResponse: [
+                        { success: true, from: inputfile, to: USSFileName }
+                    ]
+                };
+            });
+            try {
+                await handler.process({
+                    arguments: {
+                        $0: "fake",
+                        _: ["fake"],
+                        inputfile,
+                        USSFileName,
+                        ...UNIT_TEST_ZOSMF_PROF_OPTS
+                    },
+                    response: {
+                        data: {
+                            setMessage: jest.fn((setMsgArgs) => {
+                                apiMessage = setMsgArgs;
+                            }),
+                            setObj: jest.fn((setObjArgs) => {
+                                jsonObj = setObjArgs;
+                            })
+                        },
+                        console: {
+                            log: jest.fn((logArgs) => {
+                                logMessage += "\n" + logArgs;
+                            })
+                        },
+                        progress: {
+                            startBar: jest.fn(() => {
+                                // do nothing
+                            }),
+                            endBar: jest.fn(() => {
+                                // do nothing
+                            })
+                        }
+                    }
+                } as any);
+            } catch (e) {
+                error = e;
+            }
+            expect(error).toBeUndefined();
+            expect(Upload.uploadFile).toHaveBeenCalledTimes(1);
+            expect(Upload.uploadFile).toHaveBeenCalledWith(fakeSession, inputfile, USSFileName, {
+                attributes: zosAttributes,
+                binary: undefined,
+                includeHidden: undefined,
+                maxConcurrentRequests: undefined,
+                responseTimeout: undefined,
+                task: {
+                    percentComplete: 0,
+                    stageName: 0,
+                    statusMessage: "Uploading USS file"
+                }
             });
             expect(jsonObj).toMatchSnapshot();
             expect(apiMessage).toMatchSnapshot();
