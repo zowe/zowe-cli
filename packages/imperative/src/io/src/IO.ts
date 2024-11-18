fcb259a377a283c2e861bec1a1bedbf9506c3edc
jace-roell <jace.roell@hotmail.com>
2024-11-18 08:50:39 -0500

    pass processed buffer 'as T'
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/imperative/src/io/src/IO.ts
+++ packages/imperative/src/io/src/IO.ts
@@ -293,7 +293,7 @@ export class IO {
                 bufferList.push(currentByte);
                 prevByte = currentByte;
             }
-            processed = Buffer.from(bufferList);
+            processed = Buffer.from(bufferList) as T;
             return processed;
         }
     }
