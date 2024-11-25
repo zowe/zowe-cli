2646eda0b36604f97e34c588ea6f6edc0083c185
jace-roell <jace.roell@hotmail.com>
2024-11-25 10:13:28 -0500

    removed comment from shell
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/cli/__tests__/zostso/__system__/stop/__scripts__/address-space/as_success_stop.sh
+++ packages/cli/__tests__/zostso/__system__/stop/__scripts__/address-space/as_success_stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# SERVLET_KEY=`zowe tso start as | grep -oP "(?<=: ).*"`
 SERVLET_KEY=`zowe tso start as | awk -F': ' '{print $2}' | sed '1p;d'`
 
 zowe zos-tso stop address-space ${SERVLET_KEY}
