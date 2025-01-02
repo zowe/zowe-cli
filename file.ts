f0ff57302b469f53242e91a63785de85a448015c
jace-roell <jace.roell@hotmail.com>
2025-01-02 09:48:30 -0500

    remove test file
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- file.ts
+++
@@ -1,8 +0,0 @@
-const { ProfileInfo } = require("@zowe/imperative");
-(async () => {
-    const profInfo = new ProfileInfo("zowe");
-    await profInfo.readProfilesFromDisk();
-    const upd = { profileName: "lpar1.zosmf", profileType: "zosmf" };
-    await profInfo.updateProperty({ ...upd, property: "user", value: "abc", setSecure: true });
-    await profInfo.updateProperty({ ...upd, property: "password", value: "daaaef", setSecure: true });
-})();
