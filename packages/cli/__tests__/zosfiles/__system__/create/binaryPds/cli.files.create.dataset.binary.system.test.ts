1b0f808303276469a35050bc439554d8d6da20f1
Andrew W. Harn <andrew.harn@broadcom.com>
2025-03-19 14:45:57 -0400

    Update tests again
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/cli/__tests__/zosfiles/__system__/create/binaryPds/cli.files.create.dataset.binary.system.test.ts
+++ packages/cli/__tests__/zosfiles/__system__/create/binaryPds/cli.files.create.dataset.binary.system.test.ts
@@ -43,7 +43,7 @@ describe("Create Binary Data Set", () => {
 
         user = defaultSystem.zosmf.user.trim().toUpperCase();
         hlq = user + ".ZOSTEST";
-        dsname = `${hlq}.TEST.DATA.SET`;
+        dsname = `${hlq}.DATA.SET`;
 
     });
 
