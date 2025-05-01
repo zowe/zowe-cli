f199edaf8531b2b72c5da530b6ab3a51e61372a3
Andrew W. Harn <andrew.harn@broadcom.com>
2025-04-30 22:08:17 -0400

    Fix test properly this time
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/zosmf/__tests__/__system__/methods/CheckStatus.system.test.ts
+++ packages/zosmf/__tests__/__system__/methods/CheckStatus.system.test.ts
@@ -121,9 +121,8 @@ describe("Check Status Api", () => {
 
             expect(error).toBeTruthy();
             expect(response).toBeFalsy();
-            const jsonCauseErrors = error.causeErrors;
-            expect(jsonCauseErrors.message).toContain("EPROTO");
-            expect(error.port).toContain(badPort);
+            expect(error.causeErrors.message).toMatch(/EPROTO|ECONNRESET/);
+            expect(error.mDetails.port).toEqual(badPort);
         }, 300000);
     });
 });
