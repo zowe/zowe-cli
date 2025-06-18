232ef26822ebf6513628b1f676c4db01d4b4e701
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2025-06-18 11:12:09 -0400

    windows: try os.EOL to fix integration tests on windows :yum:
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- packages/imperative/__tests__/__integration__/imperative/__tests__/__integration__/cli/config/secure/__scripts__/secure_prompt.sh
+++ packages/imperative/__tests__/__integration__/imperative/__tests__/__integration__/cli/config/secure/__scripts__/secure_prompt.sh
@@ -9,13 +9,14 @@ command="imperative-test-cli config secure $1"
 
 cat > node_script.js <<EOF
 const cp = require('child_process');
+const os = require('os');
 const command = '$command'.trim().split(' ');
 const child = cp.spawn(command.shift(), command, { stdio: "pipe" });
 const values = "$values".split(',');
 child.stdout.on('data', (data) => {
   console.log(data.toString());
   if (data.toString().includes("Press ENTER to skip:") || data.toString().includes("(will be hidden):")) {
-    child.stdin.write(values.shift() + '\n');
+    child.stdin.write(values.shift() + os.EOL);
     if (values.length === 0) {
       child.stdin.end();
     }
