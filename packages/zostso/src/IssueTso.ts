86d1bc1d69bd9452fcd78934e4a1e9b67d71ba76
Pujal Gandhi <71276682+pujal0909@users.noreply.github.com>
2025-07-02 11:09:32 -0400

    Update packages/zostso/src/IssueTso.ts
    Co-authored-by: Timothy Johnson <timothy.johnson@broadcom.com>
    Signed-off-by: Pujal Gandhi <71276682+pujal0909@users.noreply.github.com>

--- packages/zostso/src/IssueTso.ts
+++ packages/zostso/src/IssueTso.ts
@@ -113,7 +113,7 @@ export class IssueTso {
         if(response.startResponse?.messages?.includes("IKJ56482I")) {
             throw new ImperativeError({
                 msg: `Invalid logon procedure.`,
-                additionalDetails: response.startResponse?.messages
+                additionalDetails: response.startResponse.messages
             });
         }
 
