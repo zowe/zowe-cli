cbe888abb83e6097d6f28856706020b775042c68
Gene Johnston <eugene.johnston@broadcom.com>
2025-12-01 14:41:12 -0500

    Create a CLI ProfileInfo test program
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -16,6 +16,13 @@ import { inspect } from "util";
 import { DaemonDecider } from "./daemon/DaemonDecider";
 import { join } from "path";
 
+// Hack: special imports done to enable our hack below.
+import { ProfileInfo } from "@zowe/imperative";
+import {
+    ConvertMsg, ConvertMsgFmt, ConvertV1Profiles, IConvertV1ProfOpts, IConvertV1ProfResult
+} from "@zowe/imperative";
+// end Hack
+
 // TODO(Kelosky): if we remove this, imperative fails to find config in package.json & we must debug this.
 const config: IImperativeConfig = {
     configurationModule: join(__dirname, "imperative")
@@ -27,6 +34,34 @@ const config: IImperativeConfig = {
         if(process.argv.includes("--daemon") || process.env.npm_lifecycle_event === "postinstall") {
             config.daemonMode = true;
         }
+
+        /* Hack:
+         * These are items that you can temporarily do in packages/cli/src/main.ts to test an API
+         * initialized like ZE.
+         * By switching which of the following 2 lines is commented, you can call an API as
+         * the CLI would or as ZE would.
+         */
+        // await Imperative.init(config); // This initializes like the CLI does.
+        const profInfo = new ProfileInfo("zowe"); // This initializes ConfigUtils.initImpUtils() like ZE
+
+        // Now call the API function that you are interested in.
+        await profInfo.readProfilesFromDisk();
+        const zosmfProfile = profInfo.getDefaultProfile("zosmf");
+        const baseProfile = profInfo.getDefaultProfile("base");
+        const getSecureValsVal = true;
+        const zosmfMergedArgs = profInfo.mergeArgsForProfile(zosmfProfile,
+            { getSecureVals: getSecureValsVal }
+        );
+        /* eslint-disable no-console */
+        console.log("baseProfile:\n " + JSON.stringify(baseProfile, null, 2));
+        console.log("zosmfProfile:\n " + JSON.stringify(zosmfProfile, null, 2));
+        console.log("zosmfMergedArgs with getSecureVals = " + getSecureValsVal + ":\n " +
+            JSON.stringify(zosmfMergedArgs, null, 2)
+        );
+        console.log("Gene's hack got to end");
+        /* eslint-disable no-console */
+        process.exit(999);
+        // end Hack
 
         await Imperative.init(config);
         const daemonDecider = new DaemonDecider(process.argv);
