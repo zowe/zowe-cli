80108cd9cf732b79088a9f13e97d7f9b8ad4f72f
Gene Johnston <eugene.johnston@broadcom.com>
2026-04-30 17:57:55 -0400

    Truncate displayed message. Log detailed message.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/src/utilities/src/ExecUtils.ts
+++ packages/imperative/src/utilities/src/ExecUtils.ts
@@ -11,6 +11,10 @@
 
 import { SpawnSyncOptions, SpawnSyncReturns } from "child_process";
 import * as spawn from "cross-spawn";
+import { normalize as pathNormalize } from "path";
+
+import { ImperativeConfig } from "./ImperativeConfig";
+import { Logger } from "../../logger";
 
 /**
  * A collection of utilities related to executing a sub-process.
@@ -48,15 +52,43 @@ export class ExecUtils {
         if (result.error != null) {
             throw result.error;
         } else if (result.status !== 0) {
-            let msg = `Command failed: ${argv.join(" ")}`;
-            msg += `\nExit code = ${result.status.toString()}`;
+            // log a detailed message
+            let errMsg = `Command that failed: ${argv.join(" ")}`;
+            if (result.status) {
+                errMsg += `\nExit code = ${result.status.toString()}`;
+            }
             if (result.stderr?.length > 0) {
-                msg += `\nStdErr:\n${result.stderr.toString()}`;
+                errMsg += `\n\nCommand's standard error stream:\n${result.stderr.toString()}`;
             }
             if (result.stdout?.length > 0) {
-                msg += `\nStdOut:\n${result.stdout.toString()}`;
+                errMsg += `\nCommand's standard output stream:\n${result.stdout.toString()}`;
             }
-            throw new Error(msg);
+
+            // give additional tips to debug npm commands
+            if (argv[0].toLowerCase().includes("npm")) {
+                errMsg += "\nIf you are using an NPM registry, " +
+                    "confirm that NPM can access the registry with the following command:\n" +
+                    "    npm view <your_desired_package_name>\n" +
+                    "Confirm that NPM can download your package from your registry:\n" +
+                    "    npm pack <your_desired_package_name> -registry <your_npm_registry_name> "
+            }
+            Logger.getImperativeLogger().error(errMsg);
+
+            // throw a truncated message to be displayed
+            const truncLen = 100;
+            errMsg = "";
+            if (result.stderr?.length > 0) {
+                errMsg += `${result.stderr.toString()}`;
+            }
+            if (result.stdout?.length > 0) {
+                errMsg += `\n${result.stdout.toString()}`;
+            }
+            if (errMsg.length > truncLen) {
+                errMsg = errMsg.slice(0, truncLen) + " ... <message truncated>";
+            }
+            const impLogFileNm = pathNormalize(ImperativeConfig.instance.cliHome + "/logs/imperative.log");
+            errMsg += `\nFor error details, search for 'Command that failed' in ${impLogFileNm}`;
+            throw new Error(errMsg);
         }
         return result.stdout as T;
     }
