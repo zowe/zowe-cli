ce15a6eead76faa5a6e31c03acd521a70d866006
Gene Johnston <eugene.johnston@broadcom.com>
2026-06-12 17:23:20 -0400

    Increase code coverage for EncodeUri
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/src/rest/__tests__/client/EncodeUri.unit.test.ts
+++ packages/imperative/src/rest/__tests__/client/EncodeUri.unit.test.ts
@@ -9,9 +9,11 @@
 *
 */
 
+import { AbstractSession } from "../../src/session/AbstractSession";
 import { EncodeUri } from "../../src/client/EncodeUri";
-import { ZosFilesConstants } from "../../../../../zosfiles/src/constants/ZosFiles.constants";
+import { ImperativeError } from "../../../error/src/ImperativeError";
 import { Session } from "../../src/session/Session";
+import { ZosFilesConstants } from "../../../../../zosfiles/src/constants/ZosFiles.constants";
 
 describe("EncodeUri tests", () => {
     const ussBaseUri = ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES;
@@ -26,59 +28,109 @@ describe("EncodeUri tests", () => {
         basePath: "basePath_means_apiml"
     });
 
-    it("should encode no special characters for a zos URI path with zosmf", () => {
-        const zosBaseUri = ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES;
-        const zosSpecialChars = "@$-#";
-        const suppliedUri = zosBaseUri + `/-(VOL99)/ALPHA.12345.JCL(TEST${zosSpecialChars})`;
+    describe("encUriPathForZos tests", () => {
+        it("should encode no special characters for a zos URI path with zosmf", () => {
+            const zosBaseUri = ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES;
+            const zosSpecialChars = "@$-#";
+            const suppliedUri = zosBaseUri + `/-(VOL99)/ALPHA.12345.JCL(TEST${zosSpecialChars})`;
 
-        const encodedUri = EncodeUri.encUriPathForZos(zosmfSession, suppliedUri);
-        expect(encodedUri).toEqual(suppliedUri);
+            const encodedUri = EncodeUri.encUriPathForZos(zosmfSession, suppliedUri);
+            expect(encodedUri).toEqual(suppliedUri);
+        });
+
+        it("should encode only # for a zos URI path with apiml", () => {
+            const zosBaseUri = ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES;
+            const zosSpecialChars = "@$-#";
+            const suppliedUri = zosBaseUri + `/-(VOL99)/ALPHA.12345.JCL(TEST${zosSpecialChars})`;
+            const expectedUri = zosBaseUri + "/-(VOL99)/ALPHA.12345.JCL(TEST@$-%23)";
+
+            const encodedUri = EncodeUri.encUriPathForZos(apimlSession, suppliedUri);
+            expect(encodedUri).toEqual(expectedUri);
+        });
+    }); // end encUriPathForZos
+
+    describe("encUriPathForUss tests", () => {
+        it("should encode a small subset of special characters for a uss URI path with zosmf", () => {
+            const normalUriChars = ussBaseUri + "/some/dir/file";
+            const suppliedUri = normalUriChars + ussSpecialChars;
+            const expectedUri = normalUriChars + "-=~,.!:'&$@*_%25%20%2B%3F#;<>[]^{|}";
+
+            const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
+            expect(encodedUri).toEqual(expectedUri);
+        });
+
+        it("should encode a larger subset of special characters for a uss URI path with apiml", () => {
+            const normalUriChars = ussBaseUri + "/some/dir/file";
+            const suppliedUri = normalUriChars + ussSpecialChars;
+            const expectedUri = normalUriChars + "-=~,.!:'&$@*_%25%20%2B%3F%23%3B%3C%3E%5B%5D%5E%7B%7C%7D";
+
+            const encodedUri = EncodeUri.encUriPathForUss(apimlSession, suppliedUri);
+            expect(encodedUri).toEqual(expectedUri);
+        });
+
+        it("should eliminate multiple forward slash characters for a uss URI path", () => {
+            const suppliedUri = ussBaseUri + "/" + "/some//dir/with////multiple//slashes/subDir/../test.txt";
+            const expectedUri = ussBaseUri + "/some/dir/with/multiple/slashes/test.txt";
+
+            const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
+            expect(encodedUri).toEqual(expectedUri);
+        });
+
+        it("should throw an error if a backslash is in a uss URI path", () => {
+            const suppliedUri = ussBaseUri + "/some//dir/with\\a/backslash/test.txt";
+            let error: ImperativeError = {} as ImperativeError;
+
+            try {
+                EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
+            } catch (e) {
+                error = e as ImperativeError;
+            }
+
+            expect(error).toBeDefined();
+            expect(error.message).toEqual("The supplied USS path = " +
+                "'/zosmf/restfiles/fs/some//dir/with\\a/backslash/test.txt' contains a backslash \\ character. " +
+                "This request will not be processed. In both z/OSMF and API-ML the backslash is either ignored, " +
+                "or the request fails with an HTTP 400 or 500 error code."
+            );
+        });
+
+        it("should throw an error if a doublequote is in a uss URI path", () => {
+            const suppliedUri = ussBaseUri + "/some//dir/with\"a/doublequote/test.txt";
+            let error: ImperativeError = {} as ImperativeError;
+
+            try {
+                EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
+            } catch (e) {
+                error = e as ImperativeError;
+            }
+
+            expect(error).toBeDefined();
+            expect(error.message).toEqual("The supplied USS path = " +
+                "'/zosmf/restfiles/fs/some//dir/with\"a/doublequote/test.txt' contains a double-quote \" character. " +
+                "This request will not be processed. In both z/OSMF and API-ML the double-quote " +
+                "fails with an HTTP 400 or 500 error code."
+            );
+        });
+    }); // end encUriPathForUss
+
+    describe("encUriQueryForUss tests", () => {
+        it("should encode more special characters for a uss query string", () => {
+            const suppliedUriPath = ussBaseUri + "/some/dir/";
+            const expectedUriPath = suppliedUriPath;
+            const suppliedQuery = "test" + ussSpecialChars;
+            const expectedQuery = "test" + "-%3D~%2C.!%3A'%26%24%40*_%25%20%2B%3F%23%3B%3C%3E%5B%5D%5E%7B%7C%7D";
+
+            const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUriPath) + "?" +
+                EncodeUri.encUriQueryForUss(suppliedQuery);
+            expect(encodedUri).toEqual(`${expectedUriPath}?${expectedQuery}`);
+        });
     });
 
-    it("should encode only # for a zos URI path with apiml", () => {
-        const zosBaseUri = ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES;
-        const zosSpecialChars = "@$-#";
-        const suppliedUri = zosBaseUri + `/-(VOL99)/ALPHA.12345.JCL(TEST${zosSpecialChars})`;
-        const expectedUri = zosBaseUri + "/-(VOL99)/ALPHA.12345.JCL(TEST@$-%23)";
-
-        const encodedUri = EncodeUri.encUriPathForZos(apimlSession, suppliedUri);
-        expect(encodedUri).toEqual(expectedUri);
-    });
-
-    it("should encode a small subset of special characters for a uss URI path with zosmf", () => {
-        const normalUriChars = ussBaseUri + "/some/dir/file";
-        const suppliedUri = normalUriChars + ussSpecialChars;
-        const expectedUri = normalUriChars + "-=~,.!:'&$@*_%25%20%2B%3F#;<>[]^{|}";
-
-        const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
-        expect(encodedUri).toEqual(expectedUri);
-    });
-
-    it("should encode a larger subset of special characters for a uss URI path with apiml", () => {
-        const normalUriChars = ussBaseUri + "/some/dir/file";
-        const suppliedUri = normalUriChars + ussSpecialChars;
-        const expectedUri = normalUriChars + "-=~,.!:'&$@*_%25%20%2B%3F%23%3B%3C%3E%5B%5D%5E%7B%7C%7D";
-
-        const encodedUri = EncodeUri.encUriPathForUss(apimlSession, suppliedUri);
-        expect(encodedUri).toEqual(expectedUri);
-    });
-
-    it("should eliminate multiple forward slash characters for a uss URI path", () => {
-        const suppliedUri = ussBaseUri + "/" + "/some//dir/with////multiple//slashes/subDir/../test.txt";
-        const expectedUri = ussBaseUri + "/some/dir/with/multiple/slashes/test.txt";
-
-        const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUri);
-        expect(encodedUri).toEqual(expectedUri);
-    });
-
-    it("should encode more special characters for a uss query string", () => {
-        const suppliedUriPath = ussBaseUri + "/some/dir/";
-        const expectedUriPath = suppliedUriPath;
-        const suppliedQuery = "test" + ussSpecialChars;
-        const expectedQuery = "test" + "-%3D~%2C.!%3A'%26%24%40*_%25%20%2B%3F%23%3B%3C%3E%5B%5D%5E%7B%7C%7D";
-
-        const encodedUri = EncodeUri.encUriPathForUss(zosmfSession, suppliedUriPath) + "?" +
-            EncodeUri.encUriQueryForUss(suppliedQuery);
-        expect(encodedUri).toEqual(`${expectedUriPath}?${expectedQuery}`);
+    describe("shouldEncodeForApiml tests", () => {
+        it("should return false if a null session object is supplied", () => {
+            // using class["name"] notation because it is a private static function
+            const isApiml = EncodeUri["shouldEncodeForApiml"](null as unknown as AbstractSession);
+            expect(isApiml).toBe(false);
+        });
     });
 });
