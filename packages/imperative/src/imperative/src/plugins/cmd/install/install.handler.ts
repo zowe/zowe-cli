f8880546626a6edbc5d2e1b68136abe0132be268
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2026-08-15 09:20:29 -0400

    chore: fix quoting
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- packages/imperative/src/imperative/src/plugins/cmd/install/install.handler.ts
+++ packages/imperative/src/imperative/src/plugins/cmd/install/install.handler.ts
@@ -90,7 +90,7 @@ export default class InstallHandler implements ICommandHandler {
 
                 params.response.console.log(
                     "Plug-ins within the Imperative CLI Framework can legitimately gain\n" +
-                    `control of the \`${ImperativeConfig.instance.rootCommandName}\` CLI application ` +
+                    "control of the `" + ImperativeConfig.instance.rootCommandName + "` CLI application " +
                     "during the execution of every command.\n" +
                     "Install third-party plug-ins at your own risk."
                 );
