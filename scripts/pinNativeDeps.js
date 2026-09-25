644b1533e1751171649362218e7757938cfc5ff5
Timothy Johnson <timothy.johnson@broadcom.com>
2026-09-25 10:56:23 -0400

    Try installing optional dep at top level
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/pinNativeDeps.js
+++ scripts/pinNativeDeps.js
@@ -26,6 +26,10 @@ for (const dep of nativeDeps) {
     const pkgVersion = lockfile.packages[`node_modules/${dep.name}`]?.version;
     if (pkgVersion) {
         childProcess.execSync(`npm pkg set ${pkgKey}=${pkgVersion}`, { cwd: path.resolve(dep.workspace) });
-        childProcess.execSync(`npm install ${dep.name}@${pkgVersion} --no-save`, { cwd: path.resolve(dep.workspace) });
+        try {
+            childProcess.execSync(`npm install ${dep.name}@${pkgVersion} --no-save`, { stdio: "pipe" });
+        } catch (error) {
+            console.error(`Error installing optional dependency ${dep.name}: ${error.stderr.toString()}`);
+        }
     }
 }
